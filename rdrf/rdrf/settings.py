e2f891b78656a4e72c76f4d721a88366d2c74843
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-14 14:06:50 +0800

    RDR-249 changes to settings file

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -1,13 +1,24 @@
 # Django settings for rdrf project.
+import os
 
+WEBAPP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# General site config
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEV_SERVER = True
+SITE_ID = 1
+APPEND_SLASH = True
+SSL_ENABLED = False
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
+ROOT_URLCONF = 'rdrf.urls'
 
-MANAGERS = ADMINS
+SECRET_KEY = 'qj#tl@9@7((%^)$i#iyw0gcfzf&#a*pobgb8yr#1%65+*6!@g$'
+
+# Locale
+TIME_ZONE = 'Australia/Perth'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+
 
 DATABASES = {
     'default': {
@@ -20,136 +31,214 @@ DATABASES = {
     }
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '5#1+e-*h6t6r&amp;%t6@5wdo@sho9bklts@nm_4#r_=r6$-=6%l6n'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
+# Django Core stuff
+TEMPLATE_LOADERS = [
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+]
+
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'iprestrict.middleware.IPRestrictMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'ccg.middleware.ssl.SSLRedirect',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'rdrf.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'rdrf.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.messages',
+    'django_extensions',
+    'south',
+    'messages_ui',
+    'userlog',
+    'registry.groups',
+    'registry.patients',
+    'registry.genetic',
+    'registry.common',
+    'registry.configuration',
+    'rdrf.rdrf',
+    'django_qbe',
+    'django_qbe.savedqueries',
+    'django.contrib.admin',
+    'admin_views',
+    'reversion',
+    'iprestrict',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
+# these determine which authentication method to use
+# apps use modelbackend by default, but can be overridden here
+# see: https://docs.djangoproject.com/en/dev/ref/settings/#authentication-backends
+AUTHENTICATION_BACKENDS = [
+ 'django.contrib.auth.backends.ModelBackend'
+]
+
+# email
+EMAIL_USE_TLS = False
+EMAIL_HOST = '127.0.0.1'
+EMAIL_PORT = 25
+
+# default emails
+ADMINS = [
+    ('Tech Alerts', 'alerts@ccg.murdoch.edu.au')
+]
+MANAGERS = ADMINS
+
+
+STATIC_ROOT = os.path.join(WEBAPP_ROOT, 'static')
+STATIC_URL = '{0}/static/'.format(os.environ.get("SCRIPT_NAME", ""))
+
+MEDIA_ROOT = os.path.join(WEBAPP_ROOT, 'media')
+MEDIA_URL = '{0}/static/media/'.format(os.environ.get("SCRIPT_NAME", ""))
+
+# for local development, this is set to the static serving directory. For deployment use Apache Alias
+STATIC_SERVER_PATH = os.path.join(WEBAPP_ROOT, "static")
+
+# a directory that will be writable by the webserver, for storing various files...
+WRITABLE_DIRECTORY = "/tmp"
+TEMPLATE_DEBUG = DEBUG
+
+# session and cookies
+SESSION_COOKIE_AGE = 60 * 60
+SESSION_COOKIE_PATH = '{0}/'.format(os.environ.get("SCRIPT_NAME", ""))
+SESSION_SAVE_EVERY_REQUEST = True
+CSRF_COOKIE_NAME = "csrftoken_registry"
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_NAME = "rdrf"
+
+# see https://docs.djangoproject.com/en/dev/ref/settings/#session-engine
+# https://docs.djangoproject.com/en/1.3/ref/settings/#std:setting-SESSION_FILE_PATH
+# in production we would suggest using memcached for your session engine
+SESSION_ENGINE = 'django.contrib.sessions.backends.file'
+SESSION_FILE_PATH = WRITABLE_DIRECTORY
+
+# Testing settings
+INSTALLED_APPS.extend(['django_nose'])
+#TEST_RUNNER = 'rdrf.rdrf.tests.PatchedNoseTestSuiteRunner'
+SOUTH_TESTS_MIGRATE = True
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-erase',
+    '--cover-html',
+    '--cover-branches',
+    '--cover-package=rdrf',
+]
+
+# APPLICATION SPECIFIC SETTINGS
+AUTH_PROFILE_MODULE = 'groups.User'
+EMAIL_APP_NAME = "Registry "
+
+# #
+# # LOGGING
+# #
+LOG_DIRECTORY = os.path.join(WEBAPP_ROOT, "log")
+try:
+    if not os.path.exists(LOG_DIRECTORY):
+        os.mkdir(LOG_DIRECTORY)
+except:
+    pass
+os.path.exists(LOG_DIRECTORY), "No log directory, please create one: %s" % LOG_DIRECTORY
+
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': 'Registry [%(levelname)s:%(asctime)s:%(filename)s:%(lineno)s:%(funcName)s] %(message)s'
+        },
+        'db': {
+            'format': 'Registry [%(duration)s:%(sql)s:%(params)s] %(message)s'
+        },
+        'simple': {
+            'format': 'Registry %(levelname)s %(message)s'
+        },
+    },
     'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
     },
     'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'errorfile':{
+            'level':'ERROR',
+            'class':'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIRECTORY, 'error.log'),
+            'when':'midnight',
+            'formatter': 'verbose'
+        },
+        'registryfile':{
+            'class':'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIRECTORY, 'registry.log'),
+            'when':'midnight',
+            'formatter': 'verbose'
+        },
+        'db_logfile':{
+            'level':'DEBUG',
+            'class':'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(LOG_DIRECTORY, 'registry_db.log'),
+            'when':'midnight',
+            'formatter': 'db'
+        },
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+            'filters': [],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter':'verbose',
+            'include_html':True
         }
     },
+    'root': {
+            'handlers':['console', 'errorfile', 'mail_admins'],
+            'level':'ERROR',
+    },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+        'django': {
+            'handlers':['null'],
+            'propagate': False,
+            'level':'INFO',
+        },
+        'registry_log': {
+            'handlers': ['registryfile', 'console'],
+            'level': 'DEBUG',
+            'propagate': False,
         },
     }
 }
+
+
+################################################################################
+## Customize settings for each registry below
+################################################################################
+
+INTERNAL_IPS = ('127.0.0.1', '172.16.2.1')
+
+ALLOWED_HOSTS = [
+    'localhost'
+]
+
+INSTALL_NAME = 'rdrf'
+
+QBE_ACCESS_FOR = lambda user: user.is_superuser
+LOGIN_URL = '{0}/admin'.format(os.environ.get("SCRIPT_NAME", ""))
+
+try:
+    print "Attempting to import default settings as appsettings.rdrf"
+    from appsettings.rdrf import *
+    print "Successfully imported appsettings.rdrf"
+except ImportError, e:
+    print "Failed to import appsettings.rdrf"
