3bf62adbee7c0feeecb52f4788ffa083535120f2
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-18 13:56:02 +0800

    RDR-242 Data binding of section form

--- rdrf/rdrf/form_view.py
+++ rdrf/rdrf/form_view.py
@@ -11,7 +11,7 @@ from registry.patients.models import Patient
 from dynamic_forms import create_form_class_for_section
 from dynamic_data import DynamicDataWrapper
 
-logger = logging.getLogger("dmd")
+logger = logging.getLogger("registry_log")
 
 class FormView(View):
 
@@ -21,7 +21,7 @@ class FormView(View):
 
         dynamic_data = dyn_patient.load_dynamic_data(registry_code,"cdes")
 
-        form_obj = RegistryForm.objects.get(name=form_name)
+        form_obj = RegistryForm.objects.get(name=form_name, registry=registry_code)
         
         sections = self._get_sections(form_obj)
         
@@ -30,23 +30,47 @@ class FormView(View):
             form_section[s] =   create_form_class_for_section(s)
         
         context = {
-            'registry': form_obj.registry,
+            'registry': registry_code,
             'form_name': form_name,
             'patient_id': patient_id,
             'sections': sections,
             'forms': form_section
         }
         context.update(csrf(request))
-        
-        print context
-        
         return render_to_response('rdrf_cdes/form.html', context)
 
-    def post(self, request, form_name, patient_id):
+    def post(self, request, registry_code, form_name, patient_id):
         patient = Patient.objects.get(pk=patient_id)
         dyn_patient = DynamicDataWrapper(patient)
+        form_obj = RegistryForm.objects.get(name=form_name, registry=registry_code)
+        sections = self._get_sections(form_obj)
+        form_section = {}
+        for s in sections:
+            form_class = create_form_class_for_section(s)
+            logger.debug("created form class for section %s: %s" % (s, form_class))
+            logger.debug("POST data = %s" % request.POST)
+            form = form_class(request.POST)
+            if form.is_valid():
+                logger.debug("form is valid")
+                dynamic_data = form.cleaned_data
+                dyn_patient.save_dynamic_data(registry_code, "cdes", dynamic_data)
+            else:
 
-        return render_to_response('rdrf_cdes/form.html')
+
+            form_section[s] = form
+
+
+
+        context = {
+            'registry': registry_code,
+            'form_name': form_name,
+            'patient_id': patient_id,
+            'sections': sections,
+            'forms': form_section
+        }
+
+        context.update(csrf(request))
+        return render_to_response('rdrf_cdes/form.html', context)
     
     def _get_sections(self, form):
         section_parts = form.sections.split(",")        
