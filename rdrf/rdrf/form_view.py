61890aebbe6a9b6d8d0cede317c4c92d8eaefd71
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-11-20 15:04:35 +0800

    Missing templatetag and patient name on a form

--- rdrf/rdrf/form_view.py
+++ rdrf/rdrf/form_view.py
@@ -33,6 +33,7 @@ class FormView(View):
             'registry': registry_code,
             'form_name': form_name,
             'patient_id': patient_id,
+            'patient_name': '%s %s' % (patient.given_names, patient.family_name),
             'sections': sections,
             'display_names': display_names,
             'forms': form_section
