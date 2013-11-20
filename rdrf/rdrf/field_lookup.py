4e29daf19e6763b10a31ea36317adf95f297374b
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-20 10:56:19 +0800

    RDR-246 return calculated field if calculation set and valid

--- rdrf/rdrf/field_lookup.py
+++ rdrf/rdrf/field_lookup.py
@@ -103,6 +103,12 @@ class FieldFactory(object):
                     return True
         return False
 
+    def _is_calculated_field(self):
+        if self.cde.calculation:
+            return True
+        else:
+            return False
+
     def _has_field_override(self):
         return hasattr(fields, self.FIELD_OVERRIDE_TEMPLATE % self.cde.code)
 
@@ -194,7 +200,17 @@ class FieldFactory(object):
             elif self._has_field_for_dataype():
                 field = self._get_field_for_datatype()
             else:
-                # we return a plain Django Field defaulting to a character field
+                if self._is_calculated_field():
+                    try:
+                        parser = CalculatedFieldParser(self.cde)
+                        script = parser.get_script()
+                        from widgets import CalculatedFieldWidget
+                        options['widget'] = CalculatedFieldWidget(script)
+                        return django.forms.CharField(**options)
+
+                    except CalculatedFieldParseError, pe:
+                        logger.error("Calculated Field %s Error: %s" % (self.cde, pe))
+
                 field_or_tuple = self.DATATYPE_DICTIONARY.get(self.cde.datatype.lower(), django.forms.CharField)
 
                 if isinstance(field_or_tuple, tuple):
@@ -277,5 +293,71 @@ class ValidatorFactory(object):
 
 
         return validators
+
+class CalculatedFieldParseError(Exception):
+    pass
+
+class CalculatedFieldParser(object):
+
+    def __init__(self, cde):
+        """
+        A calculation is valid javascript like:
+
+        var score = 23;
+        if (context.CDE01 > 5) {
+            score += 100;
+        }
+
+        score += context.CDE02;
+
+        context.result = score;
+
+
+        :param cde:
+        :return:
+        """
+        self.context_indicator = "context"
+        self.pattern =  r"\b%s\.(.+?)\b" % self.context_indicator
+        self.result_name = "result"
+        self.cde = cde
+        self.subjects = []
+        self.calculation = self.cde.calculation.strip()
+        self.observer = self.cde.code
+        self.script = None
+        self._parse_calculation()
+
+
+    def _parse_calculation(self):
+        if self.calculation:
+            self.subjects = self._parse_subjects(self.calculation)
+
+        calculation_result = self.context_indicator + "." + self.result_name
+        if not calculation_result in self.calculation:
+            raise CalculatedFieldParseError("Calculation does not contain %s" % calculation_result)
+        if not self.subjects:
+            raise CalculatedFieldParseError("Calculation does not depend on any fields")
+
+    def _parse_subjects(self, calculation):
+        import re
+        return filter(lambda code: code != self.result_name, re.findall(self.pattern, calculation))
+
+    def get_script(self):
+        observer_code = self.cde.code # e.g. #CDE01
+        subject_codes_string = ",".join(self.subjects)  # e.g. CDE02,CDE05
+        calculation_body = self.calculation  # E.g. context.result = parseInt(context.CDE05) + parseInt(context.CDE02)
+        javascript = """
+            <script>
+            $(document).ready(function(){
+                 $("#id_%s").add_calculation({
+                    subjects: "%s",
+                    calculation: function (context) { %s },
+                    observer: "%s"
+                    });
+                });
+
+            </script>""" % (observer_code, subject_codes_string, calculation_body, observer_code)
+
+        logger.debug("calculated field js: %s" % javascript)
+        return javascript
 
 
