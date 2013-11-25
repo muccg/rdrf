2c2fcd514f5d2747ee910681481408ad70f7d441
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-11-25 11:50:09 +0800

    RegistryForm admin view clenup

--- rdrf/rdrf/admin.py
+++ rdrf/rdrf/admin.py
@@ -4,11 +4,13 @@ from models import *
 class SectionAdmin(admin.ModelAdmin):
     list_display = ('code', 'display_name')
 
+class RegistryFormAdmin(admin.ModelAdmin):
+    list_display = ('registry', 'name', 'sections')
 
 admin.site.register(CDEPermittedValue)
 admin.site.register(CDEPermittedValueGroup)
 admin.site.register(CommonDataElement)
 admin.site.register(Wizard)
-admin.site.register(RegistryForm)
+admin.site.register(RegistryForm, RegistryFormAdmin)
 admin.site.register(Section, SectionAdmin)
 admin.site.register(Registry)
