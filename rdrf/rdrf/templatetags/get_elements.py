1e211dc9c86579e99e15a5693a2c7a4371d5e418
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-26 22:48:35 +0800

    RDR-255 Add form ( of formset section ) now works.

--- rdrf/rdrf/templatetags/get_elements.py
+++ rdrf/rdrf/templatetags/get_elements.py
@@ -3,10 +3,7 @@ import string
 
 register = template.Library()
 
-def js_quote(value):
-    return "'" + value + "'"
-
 @register.filter()
 def get_elements(dictionary, key):
     elements = dictionary.get(key)
-    return ",".join(map(js_quote,elements))
+    return ",".join(elements)
