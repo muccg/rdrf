91a94fdc960f3f2fe4288f141f4ab039332416fa
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2013-11-26 17:00:09 +0800

    Add registry column to patient list

--- rdrf/registry/patients/admin.py
+++ rdrf/registry/patients/admin.py
@@ -50,7 +50,7 @@ class PatientAdmin(AdminViews, admin.ModelAdmin):
 
     inlines = [PatientConsentAdmin, PatientParentAdmin, PatientDoctorAdmin]
     search_fields = ["family_name", "given_names"]
-    list_display = ['__unicode__', 'progress_graph', 'moleculardata_entered', 'freshness', 'working_group']
+    list_display = ['__unicode__', 'rdrf_registry', 'working_group', 'progress_graph', 'moleculardata_entered', 'freshness']
 
     def get_form(self, request, obj=None, **kwargs):
          form = super(PatientAdmin, self).get_form(request, obj, **kwargs)
