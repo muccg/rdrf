49ba1c81291c410c307cb777c7ad21394c24d547
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-26 10:32:03 +0800

    RDR-255 Added get_forms filter to extract forms from formse for multi-enabled section.

---
+++ rdrf/rdrf/templatetags/get_forms.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def get_forms(dictionary, key):
+    formset = dictionary.get(key)
+    for form in formset:
+        yield form
