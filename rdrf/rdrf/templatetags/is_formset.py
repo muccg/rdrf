4ecdb70e756b462a99ac2a6970a65a41276e0e7d
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-26 10:31:13 +0800

    RDR-255 Added is_formset tag

---
+++ rdrf/rdrf/templatetags/is_formset.py
@@ -0,0 +1,9 @@
+from django import template
+from django.forms.formsets import BaseFormSet
+
+register = template.Library()
+
+@register.filter()
+def is_formset(dictionary, key):
+    form_or_formset = dictionary.get(key)
+    return isinstance(form_or_formset, BaseFormSet)
