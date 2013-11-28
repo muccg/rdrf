d999ff81597154e6f3766a12ee988f98793a0ffe
Lee Render <lrender@ccg.murdoch.edu.au>
2013-11-28 15:08:31 +0800

    RDR-242 Made validation messages use cde name not code

--- rdrf/rdrf/validation.py
+++ rdrf/rdrf/validation.py
@@ -24,13 +24,13 @@ class ValidatorFactory(object):
                 def validate_max(value):
                     logger.debug("%s validaton: value = %s max value = %s" % (self.cde, value, self.cde.max_value))
                     if value > self.cde.max_value:
-                        raise ValidationError("Value of %s for %s is more than maximum value %s" % (value, self.cde.code, self.cde.max_value))
+                        raise ValidationError("Value of %s for %s is more than maximum value %s" % (value, self.cde.name, self.cde.max_value))
                 validators.append(validate_max)
 
             if self.cde.min_value:
                 def validate_min(value):
                     if value < self.cde.min_value:
-                        raise ValidationError("Value of %s for %s is less than minimum value %s" % (value, self.cde.code, self.cde.min_value))
+                        raise ValidationError("Value of %s for %s is less than minimum value %s" % (value, self.cde.name, self.cde.min_value))
 
                 validators.append(validate_min)
 
@@ -41,15 +41,15 @@ class ValidatorFactory(object):
                     re_pattern = re.compile(self.cde.pattern)
                     def validate_pattern(value):
                         if not re_pattern.match(value):
-                            raise ValidationError("Value of %s for %s does not match pattern '%s'" % (value, self.cde.code, self.cde.pattern))
+                            raise ValidationError("Value of %s for %s does not match pattern '%s'" % (value, self.cde.name, self.cde.pattern))
                     validators.append(validate_pattern)
                 except Exception,ex:
-                    logger.error("Could not pattern validator for string field of cde %s pattern %s: %s" % (self.cde.code, self.cde.pattern, ex))
+                    logger.error("Could not pattern validator for string field of cde %s pattern %s: %s" % (self.cde.name, self.cde.pattern, ex))
 
             if self.cde.max_length:
                 def validate_length(value):
                     if len(value) > self.cde.length:
-                        raise ValidationError("Value of '%s' for %s is longer than max length of %s" % (value, self.cde.code, self.cde.max_length))
+                        raise ValidationError("Value of '%s' for %s is longer than max length of %s" % (value, self.cde.name, self.cde.max_length))
                 validators.append(validate_length)
 
 
