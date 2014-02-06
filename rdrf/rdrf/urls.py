09cefeff028c11f3dbe2abec2330a231fb3c24d4
Lee Render <lrender@ccg.murdoch.edu.au>
2014-02-06 16:15:19 +0800

    RDR-276 Fixed named urls in next links. Added names for all urls

--- rdrf/rdrf/urls.py
+++ rdrf/rdrf/urls.py
@@ -22,7 +22,6 @@ def handler500(request):
 def handlerApplicationError(request):
     return render_to_response("rdrf_cdes/application_error.html",{"application_error": "Example config Error"})
 
-
 urlpatterns = patterns("",
     url(r'^test404',handler404),
     url(r'^test500',handler500),
@@ -31,14 +30,13 @@ urlpatterns = patterns("",
     (r'', include('django.contrib.auth.urls')),
     (r'', include(common_urls, namespace="registry")),
     url(r"^patient/(\d+)$", views.patient_cdes),
-    url(r"^(?P<registry_code>\w+)/forms/(?P<form_id>\w+)/(?P<patient_id>\d+)$", form_view.FormView.as_view()),
-    url(r"^registry/(?P<registry_code>\w+)/?$", registry_view.RegistryView.as_view()),
-    url(r'^/?$',landing_view.LandingView.as_view()),
-    url(r'^dashboard/?$', dashboard_view.DashboardView.as_view(), name='dashboard' ),
-    url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
-    url(r'^(?P<registry_code>\w+)/questionnaire/?$',form_view.QuestionnaireView.as_view() ),
-    url(r'^(?P<registry_code>\w+)/approval/(?P<questionnaire_response_id>\d+)/?$',form_view.QuestionnaireResponseView.as_view() ),
-    url(r'^(?P<registry_code>\w+)/uploads/(?P<gridfs_file_id>\w+)$',form_view.FileUploadView.as_view()),
+    url(r"^(?P<registry_code>\w+)/forms/(?P<form_id>\w+)/(?P<patient_id>\d+)$", form_view.FormView.as_view(), name='registry_form'),
+    url(r"^registry/(?P<registry_code>\w+)/?$", registry_view.RegistryView.as_view(), name='registry'),
+    url(r'^/?$',landing_view.LandingView.as_view(), name='landing'),
+    url(r'^dashboard/?$', dashboard_view.DashboardView.as_view(), name='dashboard'),
+    url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}, name='login'),
+    url(r'^(?P<registry_code>\w+)/questionnaire/?$',form_view.QuestionnaireView.as_view(), name='questionnaire'),
+    url(r'^(?P<registry_code>\w+)/approval/(?P<questionnaire_response_id>\d+)/?$',form_view.QuestionnaireResponseView.as_view(),
+        name='questionnaire_response'),
+    url(r'^(?P<registry_code>\w+)/uploads/(?P<gridfs_file_id>\w+)$',form_view.FileUploadView.as_view(), name='file_upload'),
 )
-
-
