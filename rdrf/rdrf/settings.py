82100aeb179ef280c84e216f6c65c2843b2dc0dd
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2014-02-28 10:05:44 +0900

    Move Import link to left-hand side menu

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -269,7 +269,8 @@ SUIT_CONFIG = {
 One can add custom menu items to the left hand manu in Django Suit
 '''
 CUSTOM_MENU_ITEMS = [
-    { 'name': 'Patient form', 'url': '{0}/dashboard'.format(os.environ.get("SCRIPT_NAME", "")) },
+    { 'name': 'Patient Forms', 'url': '{0}/dashboard'.format(os.environ.get("SCRIPT_NAME", "")), 'superuser': False },
+    { 'name': 'Import Registry Definition', 'url': '{0}/import'.format(os.environ.get("SCRIPT_NAME", "")), 'superuser': True },
 ]
 
 try:
