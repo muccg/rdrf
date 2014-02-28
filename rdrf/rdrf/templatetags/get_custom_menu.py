b816d7694bb40971a660b8af777b016c2b66f6a9
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2014-02-28 15:28:01 +0900

    Highlight selected custom menu item

--- rdrf/rdrf/templatetags/get_custom_menu.py
+++ rdrf/rdrf/templatetags/get_custom_menu.py
@@ -4,6 +4,12 @@ from django.conf import settings
 register = template.Library()
 
 @register.assignment_tag
-def get_custom_menu():
+def get_custom_menu(request):
     items = getattr(settings, 'CUSTOM_MENU_ITEMS', [])
+    
+    for i in items:
+        i['is_active'] = False
+        if i['url'] == request.path:
+            i['is_active'] = True
+
     return items
