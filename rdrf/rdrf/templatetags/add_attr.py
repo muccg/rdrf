029848f9ea9d65f2784f96c0871aeedb679f6fe7
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2014-03-07 16:13:51 +0900

    RDR-309 template adjustments

---
+++ rdrf/rdrf/templatetags/add_attr.py
@@ -0,0 +1,7 @@
+from django import template
+register = template.Library()
+
+@register.filter(name='add_attr')
+def add_attr(field, param):
+    attr, value = param.split(',')    
+    return field.as_widget(attrs={attr:value})
