7671445d4d20e22d0cbd32a0ca53dd97b54bf7b4
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2014-03-13 15:11:27 +0900

    Remove dashboard files and url

--- rdrf/rdrf/static/js/dashboard.js
+++
@@ -1,7 +0,0 @@
-function check_for_btn(id1, id2, element_id) {
-    if (id1 != -1 && id2 != -1 ) {
-        $(element_id).prop('disabled', false);
-    } else {
-        $(element_id).prop('disabled', true);
-    }
-}
