03ccc6e43eb48c8f0a8d76a7206394e3eec54662
Lee Render <lrender@ccg.murdoch.edu.au>
2014-03-16 21:23:20 +0800

    RDR-318 Made context.CDECode find CDECode on page , even in different sections ( this entails that the input cdes to a calculated field be unique on the page.)

--- rdrf/rdrf/static/js/calculated_field_plugin.js
+++ rdrf/rdrf/static/js/calculated_field_plugin.js
@@ -9,8 +9,14 @@
             observer: ''  // the cde code of the output e,g, CDE03
         }, options );
 
-        var subject_codes_string  = _.map(settings.subjects.split(","), function(code){return "#id_" + settings.prefix + code;}).join()
+        //var subject_codes_string  = _.map(settings.subjects.split(","), function(code){return "#id_" + settings.prefix + code;}).join()
 
+        // locate the codes anywhere on the page ( we assume only one occurance of given cde for now
+        function locate_code(code) {
+            var id = $('[id*=' + code + ']').attr("id");
+            return "#" + id;
+        }
+        var subject_codes_string = _.map(settings.subjects.split(","), function(code) { return locate_code(code);}).join();
         $(subject_codes_string).on("input, change",function () {
             var context = {};
             var subject_codes = settings.subjects.split(",");
@@ -18,14 +24,15 @@
             for(var i=0;i<subject_codes.length;i++) {
                 // Note how we use the prefix to map from the page to the context variable names
                 // and reverse map to update the output
-                context[subject_codes[i]] = $("#id_" + settings.prefix + subject_codes[i]).val();
+                var subject_code_id_on_page = locate_code(subject_codes[i]);
+                context[subject_codes[i]] = $(subject_code_id_on_page).val();
             }
 
             try {
                 settings.calculation(context);
             }
             catch(err) {
-                context.result = "#ERROR";
+                context.result = "ERROR";
             }
 
             $("#id_" + settings.prefix + settings.observer).val(context.result);
