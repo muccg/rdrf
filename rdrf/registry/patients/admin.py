be4c32a6c3f9b3f8dfe50936edd74cdbe06d51b2
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2014-05-22 15:40:14 +0900

    RDR-358 change id names

--- rdrf/registry/patients/admin.py
+++ rdrf/registry/patients/admin.py
@@ -119,7 +119,7 @@ class PatientAdmin(admin.ModelAdmin):
             url = reverse('registry_form', args=(rdrf.code, form.id, obj.id))
             content += "<a href=%s>%s</a><br/>" % (url, form.name)
         
-        return "<button type='button' class='btn btn-info btn-small' data-toggle='popover' data-content='%s' id='phenotype-btn'>Show Forms</button>" % content
+        return "<button type='button' class='btn btn-info btn-small' data-toggle='popover' data-content='%s' id='data-modules-btn'>Show Forms</button>" % content
     
     data_modules_btn.allow_tags = True
     data_modules_btn.short_description = 'Data Modules'
