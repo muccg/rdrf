37620116919721c75ef11b388748a9f7f19f3a16
Maciej Radochonski <mradochonski@ccg.murdoch.edu.au>
2014-06-19 15:36:32 +0900

    RDR-407 Un-allocated Patients section is redundant

--- rdrf/registry/patients/views.py
+++ rdrf/registry/patients/views.py
@@ -11,15 +11,6 @@ import json
 
 from django.contrib.auth import get_user_model
 
-class UnallocatedPatients(View):
-    def get(self, request):
-        user = get_user_model().objects.get(username=request.user)
-        patients = Patient.objects.get_filtered_unallocated(user)
-        results = [obj.as_json() for obj in patients]
-        print results
-        return HttpResponse(json.dumps(results), mimetype='application/json')
-
-
 
 def update_session(request):
     #if not request.is_ajax() or not request.method=='POST':
