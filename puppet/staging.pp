45d2f6a50cd1b87f12f8c4d536cd1effe957147c
Lee Render <lrender@ccg.murdoch.edu.au>
2014-07-15 17:23:41 +0800

    RDR-521 Added staging.pp

---
+++ puppet/staging.pp
@@ -0,0 +1,70 @@
+#
+node default {
+  include ccgcommon
+  include ccgcommon::source
+  include ccgapache
+  include python
+  include repo::epel
+  include repo::ius
+  include repo::pgrpms
+  include globals
+  include ccgdatabase::postgresql::devel
+
+  # server
+  class { 'mongodb': 
+    smallfiles => true,
+    journal  => false,
+  }
+
+  # client
+  package { 'mongodb':
+    ensure => installed
+  }
+
+  $packages = ['python27-psycopg2']
+  package {$packages: ensure => installed}
+
+  # tests need firefox and a virtual X server
+  $testingpackages = ['firefox', 'xorg-x11-server-Xvfb', 'dbus-x11']
+  package {$testingpackages:
+    ensure => installed,
+  }
+
+  # There are some leaked local secrets here we don't care about
+  $django_config = {
+    deployment  => 'staging',
+    dbdriver    => 'django.db.backends.postgresql_psycopg2',
+    dbhost      => '',
+    dbname      => 'rdrf_staging',
+    dbuser      => 'rdrf',
+    dbpass      => 'rdrf',
+    memcache    => $globals::memcache_syd,
+    secret_key  => '*&^*&768768YFYTFYHGGHCgcgfcg',
+    custom_installroot => '/usr/local/webapps/rdrf/lib/python2.7/site-packages',
+
+  }
+
+  # postgressql databases
+   ccgdatabase::postgresql::db { $django_config['dbname']:
+    user     => $django_config['dbuser'],
+    password => $django_config['dbpass'],
+  }
+
+  package {'rdrf': ensure => installed, provider => 'yum_nogpgcheck'}
+
+  django::config { 'rdrf':
+    config_hash => $django_config,
+  }
+
+  webapp::django::syncdbmigrate{'rdrf': dbsync => true}
+
+  django::syncdbmigrate{'rdrf':
+    dbsync  => true,
+    notify  => Service[$ccgapache::params::service_name],
+    require => [
+      Ccgdatabase::Postgresql::Db[$django_config['dbname']],
+      Package['rdrf'],
+      Django::Config['rdrf'] ]
+  }
+
+}
