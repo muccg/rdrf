405cc06a3c30911b9e1dfdab34032bc0be21081d
Lee Render <lrender@ccg.murdoch.edu.au>
2014-07-16 17:25:51 +0800

    RDR-521 Added puppet files for demo, pbcrc  and scratch

---
+++ puppet/demo.pp
@@ -0,0 +1,86 @@
+#
+node default {
+  include ccgcommon
+  include ccgcommon::source
+  include ccgapache
+  include python
+  include repo::epel
+  include repo::ius
+  include repo::pgrpms
+  include globals
+  $release = '0.7.5-1'
+
+  $user = $globals::aws_user
+
+  
+  class {'s3cmd':
+    aws_access_key => $globals::aws_s3backup_access_key,
+    aws_secret_key => $globals::aws_s3backup_secret_key,
+    gpg_passphrase => 'This is our secret passphrase',
+    owner => $user
+  }
+
+  class {'ccgscript::mongodb_backup':
+    dumpdir => "/home/$user/dump/",
+    s3dir   => "s3://$globals::aws_backup_bucket/$globals::aws_backup_bucket_1day/rdrf-demo/"
+  }
+
+  # cronjob to run mongo-backup
+  cron { "mongo-backup":
+     ensure  => present,
+     command => $ccgscript::mongodb_backup::script,
+     user    => $user,
+     minute  => [ 0 ],
+     hour  => [ 7 ],
+  }
+
+  $dbdriver = 'django.db.backends.postgresql_psycopg2'
+  $dbhost = $globals::dbhost_syd_demo
+  $dbuser = $globals::dbuser_syd_demo
+  $dbpass = $globals::dbpass_syd_demo
+  $dbname = 'rdrf_demo'
+
+  class { 'monit::packages':
+    packages => ['rsyslog', 'sshd', 'denyhosts', 'httpd'],
+  }
+
+  # server
+  class { 'mongodb': 
+    smallfiles => true,
+    journal  => false,
+  }
+
+  # client
+  package { 'mongodb':
+    ensure => installed
+  }
+
+  $packages = ['python27-psycopg2']
+  package {$packages: ensure => installed}
+  
+  $django_config = {
+    deployment          => 'prod',
+    release             => '0.7.5-1',
+    dbdriver            => 'django.db.backends.postgresql_psycopg2',
+    dbuser              =>  $globals::dbuser_syd_demo,
+    dbpass              =>  $globals::dbpass_syd_demo,
+    dbname              =>  'rdrf_demo',
+    memcache            =>  $globals::memcache_syd,
+    secretkey           =>  $globals::secretkey_rdrf_demo,
+    admin_email         =>  $global::system_email,
+    custom_installroot  => '/usr/local/webapps/rdrf/lib/python2.7/site-packages',
+  }
+  
+  package {'rdrf': ensure => $release, provider => 'yum_nogpgcheck'} ->
+  django::config { 'rdrf':
+    config_hash => $django_config,
+  } ->
+  django::syncdbmigrate{'rdrf':
+    dbsync  => true,
+    require => [
+      Package[$packages],
+      Ccgdatabase::Postgresql[$django_config['dbname']],
+      Package['rdrf'],
+      Django::Config['rdrf'] ]
+  }
+}
