5d2289eab8761d25994d2a6d55b3e7acbc6fe256
Lee Render <lrender@ccg.murdoch.edu.au>
2014-07-24 11:37:56 +0800

    RDR-521 made rdrf_site_url.txt https://staging.ccgapps.com.au/rdrf and issues nginx restart before running lettuce

--- develop.sh
+++ develop.sh
@@ -101,6 +101,19 @@ function ci_staging_fixture() {
     fi
 }
 
+# restart nginx
+function restart_staging_nginx() {
+    cd /tmp
+    if [ ! -d tempnginxdir ]; then
+        mkdir tempnginxdir
+    fi
+    cd tempnginxdir
+    echo nginx > .projectname
+    ccg --nuke-bootstrap
+    ccg ccg_syd_nginx_staging restart_nginx
+}
+
+
 # staging selenium test
 function ci_staging_selenium() {
     ccg ${AWS_STAGING_INSTANCE} dsudo:"pip2.7 install ${PIP_OPTS} ${TESTING_MODULES}"
@@ -110,9 +123,10 @@ function ci_staging_selenium() {
     ccg ${AWS_STAGING_INSTANCE} dsudo:'yum install rdrf -y'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'killall httpd || true'
     ccg ${AWS_STAGING_INSTANCE} dsudo:'service httpd start'
-    ccg ${AWS_STAGING_INSTANCE} dsudo:'echo https://localhost/rdrf > /tmp/rdrf_site_url'
+    ccg ${AWS_STAGING_INSTANCE} dsudo:'echo https://staging.ccgapps.com.au/rdrf > /tmp/rdrf_site_url'
     ccg ${AWS_STAGING_INSTANCE} drunbg:"Xvfb -ac \:0"
     ccg ${AWS_STAGING_INSTANCE} dsudo:'mkdir -p lettuce && chmod o+w lettuce'
+    restart_staging_nginx
     ccg ${AWS_STAGING_INSTANCE} dsudo:"cd lettuce && env DISPLAY\=\:0 rdrf run_lettuce --with-xunit --xunit-file\=/tmp/tests.xml || true"
     ccg ${AWS_STAGING_INSTANCE} dsudo:'rm /tmp/rdrf_site_url'
     ccg ${AWS_STAGING_INSTANCE} getfile:/tmp/tests.xml,./
@@ -130,6 +144,11 @@ function ci_staging_tests() {
 
     # Run tests
     ccg ${AWS_STAGING_INSTANCE} dsudo:"cd ${REMOTE_TEST_DIR} && ${DJANGO_ADMIN} test rdrf"
+}
+
+
+
+
 }
 
 # lint using flake8
