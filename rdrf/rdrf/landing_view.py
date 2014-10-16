08a56d317e57441141c31d2d133952739b3d20ae
Lee Render <lrender@ccg.murdoch.edu.au>
2014-10-16 15:36:04 +0800

    linting

--- rdrf/rdrf/landing_view.py
+++ rdrf/rdrf/landing_view.py
@@ -2,9 +2,10 @@ from django.shortcuts import render_to_response, RequestContext
 from django.views.generic.base import View
 from models import Registry
 
+
 class LandingView(View):
 
     def get(self, request):
         context = {}
-        context['registrys']  = [ r for r in Registry.objects.all() ]
+        context['registrys'] = [r for r in Registry.objects.all()]
         return render_to_response('rdrf_cdes/landing.html', context, context_instance=RequestContext(request))
