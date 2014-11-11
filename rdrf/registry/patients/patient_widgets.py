1fd4e68ca806f07625e30c6a92fca05ec8b44ef2
Lee Render <lrender@ccg.murdoch.edu.au>
2014-11-11 16:30:02 +0800

    RDR-654 Added hidden field to not lose link to crated patient in PatientRelativeLinkWidget

--- rdrf/registry/patients/patient_widgets.py
+++ rdrf/registry/patients/patient_widgets.py
@@ -1,6 +1,7 @@
 from django.forms import widgets
 import logging
-
+import hashlib
+import random
 logger = logging.getLogger("registry_log")
 
 
@@ -9,10 +10,12 @@ class PatientRelativeLinkWidget(widgets.Widget):
         script = """<script>
                         $(document).ready(function() { $("#" + "%s").siblings(".add-another").hide();});
                     </script>
-                """ % attrs['id']
+                """
         if value is None:
+            control_id = attrs['id']
             html = """<input type="checkbox" id="%s" name="%s">""" % (attrs['id'], name)
         else:
-            #todo fix link
-            html = """<a href='/admin/patients/patient/%s/'>Patient in registry</a>""" % value
-        return html + script
+            control_id = hashlib.sha1(str(random.random())).hexdigest()  # random guid for id ensures we can locate the link to hide the + easily
+            hidden_field = """<input type=hidden id="%s" name="%s" value="%s">""" % (attrs['id'], name, value)  # ensure that the link to patient not lost on submission!
+            html = """<a id="%s" href='/admin/patients/patient/%s/'>Patient in registry</a>%s""" % (control_id, value, hidden_field)
+        return html + script % control_id
