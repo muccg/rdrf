4c762b7443b8235882475be183f925ab3b619446
Lee Render <lrender@ccg.murdoch.edu.au>
2014-11-11 11:39:54 +0800

    RDR-654 Ensured the green plus "add another" icon is hidden in the patient relative list

--- rdrf/registry/patients/patient_widgets.py
+++ rdrf/registry/patients/patient_widgets.py
@@ -6,11 +6,13 @@ logger = logging.getLogger("registry_log")
 
 class PatientRelativeLinkWidget(widgets.Widget):
     def render(self, name, value, attrs=None):
+        script = """<script>
+                        $(document).ready(function() { $("#" + "%s").siblings(".add-another").hide();});
+                    </script>
+                """ % attrs['id']
         if value is None:
-            widget_html = """<input type="checkbox" id="%s" name="%s">""" % (attrs['id'], name)
-            return widget_html
+            html = """<input type="checkbox" id="%s" name="%s">""" % (attrs['id'], name)
         else:
             #todo fix link
-            link = """<a href='/admin/patients/patient/%s/'>Patient in registry</a>""" % value
-            return link
-
+            html = """<a href='/admin/patients/patient/%s/'>Patient in registry</a>""" % value
+        return html + script
