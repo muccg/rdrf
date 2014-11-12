082d7092b24f040f36d305c78331b9512197fc68
Lee Render <lrender@ccg.murdoch.edu.au>
2014-11-12 15:58:43 +0800

    RDR-654 Added mongo_key function to utils

--- rdrf/rdrf/utils.py
+++ rdrf/rdrf/utils.py
@@ -9,8 +9,12 @@ def get_form_section_code(delimited_key):
     return delimited_key.split(settings.FORM_SECTION_DELIMITER)
 
 
+def mongo_key(form_name, section_code, cde_code):
+    return settings.FORM_SECTION_DELIMITER.join([form_name, section_code, cde_code])
+
+
 def id_on_page(registry_form_model, section_model, cde_model):
-    return settings.FORM_SECTION_DELIMITER.join([registry_form_model.name, section_model.code, cde_model.code])
+    return mongo_key(registry_form_model.name, section_model.code, cde_model.code)
 
 
 def de_camelcase(s):
