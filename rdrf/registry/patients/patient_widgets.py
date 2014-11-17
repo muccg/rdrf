1a4715f6acd35a2df1a593da94944dc8b610e95d
Lee Render <lrender@ccg.murdoch.edu.au>
2014-11-17 17:05:53 +0800

    RDR-654 create patient from relative checkbox only enabled when patient relative has been created

--- rdrf/registry/patients/patient_widgets.py
+++ rdrf/registry/patients/patient_widgets.py
@@ -8,7 +8,26 @@ logger = logging.getLogger("registry_log")
 class PatientRelativeLinkWidget(widgets.Widget):
     def render(self, name, value, attrs=None):
         script = """<script>
-                        $(document).ready(function() { $("#" + "%s").siblings(".add-another").hide();});
+                        function setupCreatePatientCheckBox(id) {
+                            var checkBox = $("#" + id);
+                            checkBox.closest("tr").find(":input[type='hidden']").each(function () {
+                                var inputName = $(this).attr("name");
+                                if (inputName.match(/^patientrelative_set.*-id$/)) {
+                                    var value = $(this).attr("value");
+                                    if (value != null) {
+                                        checkBox.prop("disabled", false);
+                                    }
+                                    else {
+                                        checkBox.prop("disabled", true);
+                                    }
+                                }
+                            })
+                        }
+
+                        $(document).ready(function() {
+                            $("#" + "%s").siblings(".add-another").hide();
+                            setupCreatePatientCheckBox("%s");
+                        });
                     </script>
                 """
         if value is None:
@@ -18,4 +37,4 @@ class PatientRelativeLinkWidget(widgets.Widget):
             control_id = hashlib.sha1(str(random.random())).hexdigest()  # random guid for id ensures we can locate the link to hide the + easily
             hidden_field = """<input type=hidden id="%s" name="%s" value="%s">""" % (attrs['id'], name, value)  # ensure that the link to patient not lost on submission!
             html = """<a id="%s" href='/admin/patients/patient/%s/'>Patient in registry</a>%s""" % (control_id, value, hidden_field)
-        return html + script % control_id
+        return html + script % (control_id, control_id)
