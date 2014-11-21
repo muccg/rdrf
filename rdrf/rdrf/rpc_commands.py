7c8abb6b4fbf324bf804358eeec7cfdd71722505
Lee Render <lrender@ccg.murdoch.edu.au>
2014-11-21 12:55:34 +0800

    RDR-654 Removed unused rpc command to create patient from relative ( now called from patient relative form full clean

--- rdrf/rdrf/rpc_commands.py
+++ rdrf/rdrf/rpc_commands.py
@@ -2,24 +2,6 @@ import logging
 logger = logging.getLogger('registry_log')
 
 
-def rpc_create_fh_patient_from_relative(request, relative_pk, family_name, given_names):
-    from registry.patients.models import Patient
-    from registry.patients.models import PatientRelative
-    relative = PatientRelative.objects.get(pk=relative_pk)
-    logger.info("creating a patient for relative %s" % relative)
-    p = Patient()
-    p.family_name = family_name
-    p.given_names = given_names
-    p.date_of_birth = '1965-06-14' #todo remove hardcoded dob for patientrelative
-    p.consent = True
-    p.active = True
-    p.save()
-    relative.relative_patient = p
-    relative.save()
-
-    return p.pk
-
-
 def rpc_visibility(request, element):
     user = request.user
     if user.can("see", element):
