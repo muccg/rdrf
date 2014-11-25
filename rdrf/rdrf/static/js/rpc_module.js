4effc4483174ee1330ea149b30f461549f1ac42e
Lee Render <lrender@ccg.murdoch.edu.au>
2014-11-25 16:20:25 +0800

    RDR-654 Forgot to add these to hg

---
+++ rdrf/rdrf/static/js/rpc_module.js
@@ -0,0 +1,23 @@
+var RPC = (function (){
+
+    var rpcObject = function (endPointUrl, csrfToken) {
+        this.endPoint = endPointUrl;
+        this.send = function (commandName, commandData, successCallBack) {
+            var packet = {
+                             rpc_command: commandName,
+                             args: commandData
+            };
+            var packetJSON = JSON.stringify(packet);
+            $.ajaxSetup({beforeSend: function (xhr) {
+                             xhr.setRequestHeader('X-CSRFToken', csrfToken);
+            }});
+            $.post(this.endPoint, packetJSON,  successCallBack, 'json');
+        }
+    };
+
+    return {
+        RPC: rpcObject
+    }
+})();
+
+
