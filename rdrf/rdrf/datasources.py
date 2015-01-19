a66d145dd60a56f93331266bfccc27b2ace38fe4
Lee Render <id2359@yahoo.com>
2015-01-19 14:35:22 +0800

    RDR-667 removed extra logging

--- rdrf/rdrf/datasources.py
+++ rdrf/rdrf/datasources.py
@@ -16,17 +16,14 @@ class PatientCentres(DataSource):
     questionnaire_context is a string like au or nz ( ophg wanted different centre dropdowns for DM1 in au vs nz for example)
     """
     def values(self):
-        logger.debug("Datasource Patientcentres values()")
         registry_model = self.context["registry_model"]
         if "patientCentres" in registry_model.metadata:
-            logger.debug("found metadata")
             questionnaire_context = self.context.get("questionnaire_context", "au")
-            logger.debug("XXXXXX QUESTIONNAIRE CONTEXT = %s" % questionnaire_context)
             return registry_model.metadata["patientCentres"][questionnaire_context]
         else:
             from registry.groups.models import WorkingGroup
             items = []
             for working_group in WorkingGroup.objects.filter(registry=registry_model).order_by('name'):
                 items.append((working_group.name, working_group.name))
-            return items
 
+            return items
