b92f66c55d12e72568fb46a3d6a5767a58331778
Maciej Radochonski <mradochonski@gmail.com>
2015-02-11 11:57:37 +0800

    RDR-684 presentation

--- rdrf/registry/patients/admin.py
+++ rdrf/registry/patients/admin.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.core import urlresolvers
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.templatetags.static import static
 import os
 import json
 import datetime
@@ -129,8 +130,10 @@ class PatientAdmin(admin.ModelAdmin):
         for form in forms:
             if form.is_questionnaire:
                 continue
+            is_current = obj.form_currency(form)
+            flag = "images/%s.png" % ("tick" if is_current else "cross")
             url = reverse('registry_form', args=(rdrf.code, form.id, obj.id))
-            content += "<a href=%s>%s</a> Current: %s</br>" % (url, nice_name(form.name), obj.form_currency(form))
+            content += "<img src=%s> <strong>%d%%</strong> <a href=%s>%s</a></br>" % (static(flag), obj.form_progress(form)[1] , url, nice_name(form.name))
         
         return "<button type='button' class='btn btn-info btn-small' data-toggle='popover' data-content='%s' id='data-modules-btn'>Show Modules</button>" % content
     
