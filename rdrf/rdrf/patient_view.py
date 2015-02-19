a8e0212c0e642645ce427ad1804cda807f875b41
Maciej Radochonski <mradochonski@gmail.com>
2015-02-19 09:43:06 +0800

    FKRP-54 check for registry only if user is authenticated

--- rdrf/rdrf/patient_view.py
+++ rdrf/rdrf/patient_view.py
@@ -12,17 +12,18 @@ class PatientView(View):
             'access': False
         }
         
-        try:
-            registry = Registry.objects.get(code=registry_code)
-            if registry in request.user.registry.all():
-                context['access']  = True
-        except Registry.DoesNotExist:
-            context['error_msg'] = "Registry does not exist"
-        
-        try:
-            forms = RegistryForm.objects.filter(registry__code=registry_code).filter(is_questionnaire=True)
-            context['forms'] = forms
-        except RegistryForm.DoesNotExist:
-            pass
+        if request.user.is_authenticated():
+            try:
+                registry = Registry.objects.get(code=registry_code)
+                if registry in request.user.registry.all():
+                    context['access']  = True
+            except Registry.DoesNotExist:
+                context['error_msg'] = "Registry does not exist"
+            
+            try:
+                forms = RegistryForm.objects.filter(registry__code=registry_code).filter(is_questionnaire=True)
+                context['forms'] = forms
+            except RegistryForm.DoesNotExist:
+                pass
 
         return render_to_response('rdrf_cdes/patient.html', context, context_instance=RequestContext(request))
