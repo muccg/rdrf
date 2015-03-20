dc29501c1619c1b2b5069232d5ef4536706d8efa
Lee Render <id2359@yahoo.com>
2015-03-20 14:16:30 +0800

    RDR-717 Fixed KeyError when adding a new RegistryForm in the admin interface. Added a selenium test for this.

---
+++ rdrf/rdrf/selenium_test/selenium_adding_registry_form_is_ok.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from selenium import selenium
+import unittest, time, re
+from base import Base
+
+
+class SaveNewlyCreatedRegisterForm(Base):
+
+    def test_adding_registry_form_is_ok(self):
+        sel = self.selenium
+        sel.open("/admin")
+        sel.wait_for_page_to_load("30000")
+        sel.type("id=id_password", "admin")
+        sel.type("id=id_username", "admin")
+        sel.click("css=input.btn.btn-info")
+        sel.wait_for_page_to_load("30000")
+        sel.click("link=Registry Forms")
+        sel.wait_for_page_to_load("30000")
+        sel.click("link=Add registry form")
+        sel.wait_for_page_to_load("30000")
+        sel.select("id=id_registry", "label=Facioscapulohumeral Muscular Dystrophy (fshd)")
+        sel.type("id=id_name", "testingformforfshd")
+        sel.click("name=_save")
+        sel.wait_for_page_to_load("30000")
+        try: self.assertEqual("Please correct the error below.", sel.get_text("css=div.alert.alert-error"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.type("id=id_sections", "adummysection")
+        sel.click("name=_save")
+        sel.wait_for_page_to_load("30000")
+        try: self.assertEqual(u"× The registry form \"Facioscapulohumeral Muscular Dystrophy (fshd) testingformforfshd Form comprising adummysection\" was added successfully.", sel.get_text("//div[@id='suit-center']/div"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
