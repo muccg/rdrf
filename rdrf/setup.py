564faf3b9578143d0936779d8d67dc2bd2e4c133
Lee Render <id2359@yahoo.com>
2015-05-11 15:41:17 +0800

    RDR-736  bumped version to 0.8.20

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -45,7 +45,7 @@ else:
 
 
 setup(name='django-rdrf',
-    version="0.8.19",
+    version="0.8.20",
     packages=packages,
     description='RDRF',
     long_description='Rare Disease Registry Framework',
