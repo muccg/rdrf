9f9f6bca6fa25a0271d9d89b2639191569abeff3
Lee Render <id2359@yahoo.com>
2015-05-14 16:28:55 +0800

    RDR-743 Added explanatory comments to JS code

--- rdrf/rdrf/static/js/generic_validator.js
+++ rdrf/rdrf/static/js/generic_validator.js
@@ -1,6 +1,5 @@
 // Plumbing code tp launch a constructor form for a DE value and return it back to the main form
 
-
 function goodValue(element) {
     $(element).next(".validationindicator").html("<span class='validationindicator'>VALID</span>");
 }
@@ -10,19 +9,23 @@ function badValue(element) {
 }
 
 function generic_constructor(element, constructorName, constructorFormUrl) {
-
+   // element here is the constructor button next to the input field - we have
+   // to use navigate via jquery traversal as the field may be in a multisection and have been cloned
    function updateValue(value) {
-       $(element).closest("td").find("input[type='text']").val(value);
+       var textField = $(element).closest("td").find("input[type='text']");
+       textField.val(value);  // we assume that the DE is text input field
+       textField.trigger("keyup");
    }
    var w = window.open(constructorFormUrl, "Construct " + constructorName, "location=no,width=200,height=200,scrollbars=yes,top=100,left=100,resizable = no");
    // NB this function is/must be called on the child form to allow the constructed data value to be passed back to the form
    w.updateParentForm = updateValue;
+
 }
 
 function generic_validate(element, rpcEndPoint, rpcCommand) {
-
+    // element is the text box
     var value = $(element).val();
-    var csrfToken = $("input[name='csrfmiddlewaretoken']").val();
+    var csrfToken = $("input[name='csrfmiddlewaretoken']").val(); // this will/must appear on our django form
 
     var rpc = new RPC.RPC(rpcEndPoint, csrfToken);
     rpc.send(rpcCommand, [value], function (response) {
