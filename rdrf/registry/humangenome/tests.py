ce9fb5150a4927bd19b6158a2a23f6cc86fe581b
Lee Render <id2359@yahoo.com>
2015-05-14 14:40:53 +0800

    RDR-743 initial add of human genome code from old framework

---
+++ rdrf/registry/humangenome/tests.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    import unittest
+    from tests import suite
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ rdrf/registry/humangenome/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns
+from django.contrib import admin
+
+urlpatterns = patterns('',
+)
