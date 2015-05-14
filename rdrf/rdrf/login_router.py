f011016f0000e4ccd502342752a1f8e84113d221
Maciej Radochonski <mradochonski@gmail.com>
2015-05-14 10:27:01 +0800

    FKRP-95 Login routing for FKRP users

--- rdrf/rdrf/login_router.py
+++ rdrf/rdrf/login_router.py
@@ -10,6 +10,8 @@ def in_fkrp(user):
     user_reg_codes = [ r.code for r in user.registry.all()]
     return "fkrp" in user_reg_codes
 
+_ADMIN_PATIENT_LISTING = "admin:patients_patient_changelist"
+_NEW_PATIENT_LISTING = "patientslisting"
 
 class RouterView(View):
 
@@ -20,20 +22,23 @@ class RouterView(View):
 
         if user.is_authenticated():
             if user.is_superuser:
-                redirect_url = reverse("admin:patients_patient_changelist")
+                redirect_url = reverse(_ADMIN_PATIENT_LISTING)
             elif user.is_clinician:
                 if in_fkrp(user):
-                    redirect_url = reverse("patientslisting")
+                    redirect_url = reverse(_NEW_PATIENT_LISTING)
                 else:
-                    redirect_url = reverse("admin:patients_patient_changelist")
+                    redirect_url = reverse(_ADMIN_PATIENT_LISTING)
             elif user.is_genetic_staff:
-                redirect_url = reverse("admin:patients_patient_changelist")
+                redirect_url = reverse(_ADMIN_PATIENT_LISTING)
             elif user.is_working_group_staff:
-                redirect_url = reverse("admin:patients_patient_changelist")
+                redirect_url = reverse(_ADMIN_PATIENT_LISTING)
             elif user.is_genetic_curator:
-                redirect_url = reverse("admin:patients_patient_changelist")
+                redirect_url = reverse(_ADMIN_PATIENT_LISTING)
             elif user.is_curator:
-                redirect_url = reverse("admin:patients_patient_changelist")
+                if in_fkrp(user):
+                    redirect_url = reverse(_NEW_PATIENT_LISTING)
+                else:
+                    redirect_url = reverse(_ADMIN_PATIENT_LISTING)
             elif user.is_patient:
                 regs = user.get_registries()
                 if regs:
