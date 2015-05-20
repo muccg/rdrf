f6d74eaa776756670be4c0281830907053ddfbb6
Lee Render <id2359@yahoo.com>
2015-05-20 10:30:19 +0800

    linting js

--- rdrf/rdrf/static/js/calculated_field_plugin.js
+++ rdrf/rdrf/static/js/calculated_field_plugin.js
@@ -1,5 +1,5 @@
-(function ( $ ) {
-    $.fn.add_calculation = function( options ) {
+(function($) {
+    $.fn.add_calculation = function(options) {
         var settings = $.extend({
             // These are the defaults.
             calculation: function (context) { context.result = "???"; },
@@ -9,12 +9,12 @@
             observer: '',  // the cde code of the output e,g, CDE03
             // Stuff below added to allow calculations to retrieve remote model properties
             injected_model: "",  // e.g. Patient  ( model class name)
-            injected_model_id: null,  // the id of the injected model instance to retrieve  ie Patient id for registry
-            tastypie_url: "",  // the url to request model data on - includes resource name eg /api/v1/patient/1
+            injected_model_id: null,  // the id of the injected model instance to retrieve
+            tastypie_url: ""  //the url to request model data on-includes resource eg /api/v1/patient/1
 
-        }, options );
+        }, options);
 
-        //var subject_codes_string  = _.map(settings.subjects.split(","), function(code){return "#id_" + settings.prefix + code;}).join()
+
 
         // locate the codes anywhere on the page ( we assume only one occurance of given cde for now
         function locate_code(code) {
@@ -22,17 +22,18 @@
             return "#" + id;
         }
 
-        var subject_codes_string = _.map(settings.subjects.split(","), function(code) { return locate_code(code);}).join();
-        
+        var subject_codes_string = _.map(settings.subjects.split(","), function(code)
+            { return locate_code(code);}).join();
+
         function get_object(model, model_id) {
             var d = $.Deferred();
-             if ( model_id == -1) {
+             if (model_id == -1) {
                  d.resolve([]);
                  return;
             }
 
             $.get(settings.tastypie_url)
-                .done(function (object){
+                .done(function(object) {
                     d.resolve(object);
                 })
                 .fail(d.reject);
@@ -40,18 +41,18 @@
             return d.promise();
         }
 
-        var update_function = function () {
+        var update_function = function() {
             var context = {};
             var subject_codes = settings.subjects.split(",");
 
-            for(var i=0;i<subject_codes.length;i++) {
+            for (var i = 0; i < subject_codes.length; i++) {
                 // Note how we use the prefix to map from the page to the context variable names
                 // and reverse map to update the output
                 var subject_code_id_on_page = locate_code(subject_codes[i]);
                 context[subject_codes[i]] = $(subject_code_id_on_page).val();
             }
 
-            var model_promise = get_object(settings.injected_model.toLowerCase(), settings.injected_model_id); // get injected model
+            var model_promise = get_object(settings.injected_model.toLowerCase(), settings.injected_model_id);
 
             $.when(model_promise)
              .done(function(injected_models) {
@@ -76,4 +77,4 @@
         }
     };
 
-}( jQuery ));
+}(jQuery));
