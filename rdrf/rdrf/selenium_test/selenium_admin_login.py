e8710ba79c07f8d590aff22b4c70adcecd6f3dac
Lee Render <id2359@yahoo.com>
2015-05-20 14:02:25 +0800

    Fixed broken admin login test after change to Home screen

--- rdrf/rdrf/selenium_test/selenium_admin_login.py
+++ rdrf/rdrf/selenium_test/selenium_admin_login.py
@@ -2,6 +2,7 @@ from selenium import selenium
 import unittest, time, re
 from base import Base
 
+
 class AdminLogin(Base):
 
     def test_admin_login(self):
@@ -11,7 +12,10 @@ class AdminLogin(Base):
         sel.type("id=id_password", "admin")
         sel.click("css=input.btn.btn-info")
         sel.wait_for_page_to_load("30000")
-        self.assertEqual("Show full list of links", sel.get_text("css=#full-menu-alert > h4"))
+        try:
+            self.assertEqual("Home", sel.get_text("css=li.active"))
+        except AssertionError, e:
+            self.verificationErrors.append(str(e))
         sel.click("link=Log out")
         sel.wait_for_page_to_load("30000")
         self.assertEqual("Need a patient registry for your department, clinic or community?", sel.get_text("css=h3"))
