591eb265408a80726d18dcf429a18d549a496b0b
Lee Render <id2359@yahoo.com>
2015-06-19 13:35:08 +0800

    Bumped version number to 0.8.31

--- rdrf/rdrf/__init__.py
+++ rdrf/rdrf/__init__.py
@@ -1,2 +1,2 @@
-VERSION = "0.8.30"
+VERSION = "0.8.31"
 __version__ = VERSION
--- rdrf/setup.py
+++ rdrf/setup.py
@@ -45,7 +45,7 @@ else:
 
 
 setup(name='django-rdrf',
-    version="0.8.30",
+    version="0.8.31",
     packages=packages,
     description='RDRF',
     long_description='Rare Disease Registry Framework',
