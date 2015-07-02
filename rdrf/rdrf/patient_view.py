533d1215cdd4a09744bfa80776b7c35387d63ee4
Lee Render <id2359@yahoo.com>
2015-07-02 12:14:12 +0800

    lint patient_doctor objects retrieved but unused

--- rdrf/rdrf/patient_view.py
+++ rdrf/rdrf/patient_view.py
@@ -386,7 +386,6 @@ class PatientFormMixin(PatientMixin):
 
         if registry.get_metadata_item("patient_form_doctors"):
             if not patient_doctor_form:
-                patient_doctor = PatientDoctor.objects.filter(patient=patient).values()
                 patient_doctor_formset = inlineformset_factory(Patient, Patient.doctors.through,
                                                                form=PatientDoctorForm,
                                                                extra=0,
