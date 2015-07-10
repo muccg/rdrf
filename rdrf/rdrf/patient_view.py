fb8ff7a5283e38fff108dcb985c3290ebafd307e
Lee Render <id2359@yahoo.com>
2015-07-10 10:41:12 +0800

    RDR-952 Demographics Personal Details -> Patients Personal Details

--- rdrf/rdrf/patient_view.py
+++ rdrf/rdrf/patient_view.py
@@ -314,7 +314,7 @@ class PatientFormMixin(PatientMixin):
 
             patient_address_form = patient_address_formset(instance=patient, prefix="patient_address")
 
-        personal_details_fields = ('Personal Details', [
+        personal_details_fields = ('Patients Personal Details', [
             "family_name",
             "given_names",
             "maiden_name",
