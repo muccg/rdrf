0ed03363dcb3c59c514ac462a69752a2684c551e
Lee Render <id2359@yahoo.com>
2015-07-17 14:10:43 +0800

    RDR-970 removed unncessary javascript on PatientRelativeLinkWidget

--- rdrf/registry/patients/patient_widgets.py
+++ rdrf/registry/patients/patient_widgets.py
@@ -18,44 +18,15 @@ class PatientRelativeLinkWidget(widgets.Widget):
     REGISTRY_CODE = 'fh'   # how to set!
 
     def render(self, name, value, attrs=None):
-        script = """<script>
-                        function setupCreatePatientCheckBox(id) {
-                            var checkBox = $("#" + id);
-                            checkBox.hide();
-                            checkBox.closest("tr").find(":input[type='hidden']").each(function () {
-                                var inputName = $(this).attr("name");
-                                if (inputName.match(/^relatives-\d+-id$/)) {
-                                    var value = $(this).attr("value");
-                                    if (value != null) checkBox.show();
-                                };
-                            });
-                        }
-
-                        $(document).ready(function() {
-                            $("#" + "%s").siblings(".add-another").hide();
-                            setupCreatePatientCheckBox("%s");
-                        });
-                    </script>
-                """
         if value is None:
-            control_id = attrs['id']
-            html = """<input type="checkbox" id="%s" name="%s">""" % (attrs['id'], name)
-            return html + script % (control_id, control_id)
+            return """<input type="checkbox" id="%s" name="%s">""" % (attrs['id'], name)
         else:
             reg_code = self.REGISTRY_CODE
-            # random guid for id ensures we can locate the link to hide the + easily
-            control_id = hashlib.sha1(str(random.random())).hexdigest()
-            script = """
-            <script>
-            $(document).ready(function() {
-                            $("#" + "%s").siblings(".add-another").hide();
-            });
-            </script>
-            """ % control_id
+
             hidden_field = """<input type=hidden id="%s" name="%s" value="%s">""" % (
                 attrs['id'], name, value)  # ensure that the link to patient not lost on submission!
+
             patient_url = reverse("patient_edit", args=[reg_code, value])
 
-            html = """<a id="%s" href='%s'>Patient in registry</a>%s""" % (
-                control_id, patient_url, hidden_field)
-            return html + script
+            html = """<a  href='%s'>Patient in registry</a>%s""" % (patient_url, hidden_field)
+            return html
