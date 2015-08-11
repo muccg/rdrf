f80bc94c9ccff8b5ed00401af914b0644d41d471
Lee Render <id2359@yahoo.com>
2015-08-11 16:28:26 +0800

    RDR-995 adjusted timeout on import yaml testcase

--- rdrf/rdrf/selenium_test/base.py
+++ rdrf/rdrf/selenium_test/base.py
@@ -26,13 +26,13 @@ class Base(unittest.TestCase):
 
     def import_registry(self, yaml_file_name):
         sel = self.selenium
-        sel.open("/admin")
+        sel.open("/admin/")
         sel.wait_for_page_to_load("30000")
         sel.click("link=Log in")
         sel.wait_for_page_to_load("30000")
         sel.type("id=id_username", "admin")
         sel.type("id=id_password", "admin")
-        sel.wait_for_page_to_load("30000")
+        sel.wait_for_page_to_load("10000")
         sel.click("link=Settings")
         sel.click("link=Importer")
         sel.wait_for_page_to_load("30000")
