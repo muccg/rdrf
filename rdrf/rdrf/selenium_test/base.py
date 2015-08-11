130249b84e14a7c49cc990903f5d650f164073fc
Lee Render <id2359@yahoo.com>
2015-08-11 16:45:36 +0800

    RDR-995 removed action in test

--- rdrf/rdrf/selenium_test/base.py
+++ rdrf/rdrf/selenium_test/base.py
@@ -28,8 +28,6 @@ class Base(unittest.TestCase):
         sel = self.selenium
         sel.open("/admin/")
         sel.wait_for_page_to_load("30000")
-        sel.click("link=Log in")
-        sel.wait_for_page_to_load("30000")
         sel.type("id=id_username", "admin")
         sel.type("id=id_password", "admin")
         sel.wait_for_page_to_load("10000")
