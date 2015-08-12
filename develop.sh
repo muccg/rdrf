6e2316b58439d6cbe846ea771f27e12c8e6d229a
Lee Render <id2359@yahoo.com>
2015-08-12 11:12:45 +0800

    RDR-995 locate yaml files in ./definitions !

--- develop.sh
+++ develop.sh
@@ -74,12 +74,12 @@ lettuce() {
 selenium() {
     mkdir -p data/selenium
     chmod o+rwx data/selenium
+    find ./definitions -name "*.yaml" -exec cp "{}" data/selenium \;
 
     make_virtualenv
     . ${VIRTUALENV}/bin/activate
     pip install fig
 
-    find ../definitions -name "*.yaml" -exec cp "{}" data/selenium \;
 
     fig --project-name rdrf -f fig-selenium.yml rm --force
     fig --project-name rdrf -f fig-selenium.yml build
