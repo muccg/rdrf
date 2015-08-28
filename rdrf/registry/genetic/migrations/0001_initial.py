ad4839ac52c48b04eebea7fa45347fc089f616d8
Lee Render <id2359@yahoo.com>
2015-08-28 16:57:58 +0800

    RDR-1030 Removed original migrations and created new ones

--- rdrf/registry/genetic/migrations/0001_initial.py
+++ rdrf/registry/genetic/migrations/0001_initial.py
@@ -1,80 +1,49 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Gene'
-        db.create_table(u'genetic_gene', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('symbol', self.gf('django.db.models.fields.TextField')()),
-            ('hgnc_id', self.gf('django.db.models.fields.TextField')()),
-            ('name', self.gf('django.db.models.fields.TextField')()),
-            ('status', self.gf('django.db.models.fields.TextField')()),
-            ('chromosome', self.gf('django.db.models.fields.TextField')()),
-            ('accession_numbers', self.gf('django.db.models.fields.TextField')()),
-            ('refseq_id', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal(u'genetic', ['Gene'])
+    dependencies = [
+    ]
 
-        # Adding model 'Technique'
-        db.create_table(u'genetic_technique', (
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50, primary_key=True)),
-        ))
-        db.send_create_signal(u'genetic', ['Technique'])
-
-        # Adding model 'Laboratory'
-        db.create_table(u'genetic_laboratory', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('address', self.gf('django.db.models.fields.TextField')(max_length=200, blank=True)),
-            ('contact_name', self.gf('django.db.models.fields.CharField')(max_length=200, blank=True)),
-            ('contact_email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
-            ('contact_phone', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
-        ))
-        db.send_create_signal(u'genetic', ['Laboratory'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Gene'
-        db.delete_table(u'genetic_gene')
-
-        # Deleting model 'Technique'
-        db.delete_table(u'genetic_technique')
-
-        # Deleting model 'Laboratory'
-        db.delete_table(u'genetic_laboratory')
-
-
-    models = {
-        u'genetic.gene': {
-            'Meta': {'ordering': "['symbol']", 'object_name': 'Gene'},
-            'accession_numbers': ('django.db.models.fields.TextField', [], {}),
-            'chromosome': ('django.db.models.fields.TextField', [], {}),
-            'hgnc_id': ('django.db.models.fields.TextField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.TextField', [], {}),
-            'refseq_id': ('django.db.models.fields.TextField', [], {}),
-            'status': ('django.db.models.fields.TextField', [], {}),
-            'symbol': ('django.db.models.fields.TextField', [], {})
-        },
-        u'genetic.laboratory': {
-            'Meta': {'object_name': 'Laboratory'},
-            'address': ('django.db.models.fields.TextField', [], {'max_length': '200', 'blank': 'True'}),
-            'contact_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'contact_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
-            'contact_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'})
-        },
-        u'genetic.technique': {
-            'Meta': {'object_name': 'Technique'},
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'primary_key': 'True'})
-        }
-    }
-
-    complete_apps = ['genetic']
+    operations = [
+        migrations.CreateModel(
+            name='Gene',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('symbol', models.TextField()),
+                ('hgnc_id', models.TextField(verbose_name=b'HGNC ID')),
+                ('name', models.TextField()),
+                ('status', models.TextField()),
+                ('chromosome', models.TextField()),
+                ('accession_numbers', models.TextField()),
+                ('refseq_id', models.TextField(verbose_name=b'RefSeq ID')),
+            ],
+            options={
+                'ordering': ['symbol'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Laboratory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=256)),
+                ('address', models.TextField(max_length=200, blank=True)),
+                ('contact_name', models.CharField(max_length=200, blank=True)),
+                ('contact_email', models.EmailField(max_length=254, blank=True)),
+                ('contact_phone', models.CharField(max_length=50, blank=True)),
+            ],
+            options={
+                'verbose_name_plural': 'laboratories',
+            },
+        ),
+        migrations.CreateModel(
+            name='Technique',
+            fields=[
+                ('name', models.CharField(max_length=50, serialize=False, primary_key=True)),
+            ],
+        ),
+    ]
---
+++ rdrf/registry/groups/migrations/0002_auto_20150828_1519.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('groups', '0001_initial'),
+        ('auth', '0006_require_contenttypes_0002'),
+        ('rdrf', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='workinggroup',
+            name='registry',
+            field=models.ForeignKey(to='rdrf.Registry', null=True),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='groups',
+            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups'),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='registry',
+            field=models.ManyToManyField(related_name='registry', to='rdrf.Registry'),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='user_permissions',
+            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions'),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='working_groups',
+            field=models.ManyToManyField(related_name='working_groups', null=True, to='groups.WorkingGroup'),
+        ),
+    ]
