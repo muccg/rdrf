807423634272077512a34801d4d40fa9d166a66c
Lee Render <id2359@yahoo.com>
2015-09-07 16:54:59 +0800

    RDR-1042 marking patient as an index in the clinical form only occurs once on patient creation

--- rdrf/rdrf/hooks/fh_hooks.py
+++ rdrf/rdrf/hooks/fh_hooks.py
@@ -9,17 +9,22 @@ logger = getLogger("registry_log")
 def mark_as_relative_in_clinical_form(patient):
     # Ensure that a patient created from a relative is marked as a relative in the clinical form
     if patient.in_registry('fh'):
-        patient.set_form_value(
-            "fh", "ClinicalData", "fhDateSection", "CDEIndexOrRelative", "fh_is_relative")
+        patient.set_form_value("fh", "ClinicalData", "fhDateSection", "CDEIndexOrRelative", "fh_is_relative")
 
 
 @hook("registry_added")
 def mark_created_patient_as_index(patient, registry_ids):
     from rdrf.models import Registry
+    from rdrf.dynamic_data import DynamicDataWrapper
+
+    def has_no_mongo_data(patient):
+        wrapper = DynamicDataWrapper(patient)
+        data = wrapper.load_dynamic_data('fh', 'cdes')
+        return data is None
+
     fh = Registry.objects.get(code="fh")
-    if fh.has_feature('family_linkage') and fh.pk in registry_ids:
-        logger.debug("marking patient %s as index in FH mongo db" % patient)
-        patient.set_form_value(
-            "fh", "ClinicalData", "fhDateSection", "CDEIndexOrRelative", "fh_is_index")
-    else:
-        logger.debug("registry changed but no FH marking made for %s" % patient)
+
+    if fh.has_feature('family_linkage') and fh.pk in registry_ids and has_no_mongo_data(patient):
+        # patient has just been added to fh
+        logger.debug("fh registry added hook running setting to index")
+        patient.set_form_value("fh", "ClinicalData", "fhDateSection", "CDEIndexOrRelative", "fh_is_index")
