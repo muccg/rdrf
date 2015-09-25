8df4ec6950d4e74e07464720f6026527856c8e81
Maciej Radochonski <mradochonski@gmail.com>
2015-09-25 10:36:45 +0800

    Version 0.9.2

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -47,7 +47,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="0.9.1",
+      version="0.9.2",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
