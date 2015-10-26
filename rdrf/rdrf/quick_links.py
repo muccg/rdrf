000e9987574ed711d50c02102784af6cd1e7700b
Maciej Radochonski <mradochonski@gmail.com>
2015-10-26 13:52:17 +0800

    FKRP-171 'Other' clinician field should prompt a text box
    Conflicts:
    	rdrf/rdrf/quick_links.py
    
    Conflicts:
    	rdrf/rdrf/quick_links.py
    
    Conflicts:
    	rdrf/rdrf/quick_links.py

--- rdrf/rdrf/quick_links.py
+++ rdrf/rdrf/quick_links.py
@@ -48,10 +48,10 @@ class QuickLinks(object):
     Importer = QuickLink("import_registry", "Importer", True)
     Groups = QuickLink("admin:auth_group_changelist", "Groups", True)
     NextOfKinRelationship = QuickLink(
-        "admin:patients_nextofkinrelationship_changelist", "Next of Kin Relationship", True)
-    CdePolicy = QuickLink("admin:rdrf_cdepolicy_changelist", "CDE Policy", True)
-    States = QuickLink("admin:patients_state_changelist", "States")
-    ClinicianOther = QuickLink("admin:patients_clinicianother_changelist", "Other Clinicians")
+        "admin:patients_nextofkinrelationship_changelist", _("Next of Kin Relationship"), True)
+    CdePolicy = QuickLink("admin:rdrf_cdepolicy_changelist", _("CDE Policy"), True)
+    States = QuickLink("admin:patients_state_changelist", _("States"))
+    ClinicianOther = QuickLink("admin:patients_clinicianother_changelist", _("Other Clinicians"))
 
     #FamilyLinkage = QuickLink("family_linkage", "Family Linkage")
 
--- rdrf/registry/groups/models.py
+++ rdrf/registry/groups/models.py
@@ -218,6 +218,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
         return links
 
+CustomUser._meta.get_field('username')._unique = False
 
 @receiver(user_registered)
 def user_registered_callback(sender, user, request, **kwargs):
