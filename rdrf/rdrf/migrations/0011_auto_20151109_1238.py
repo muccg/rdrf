35f4b23e16093354dc1c26e4bf045a603ac1671d
Maciej Radochonski <mradochonski@gmail.com>
2015-11-09 13:00:56 +0800

    FKRP-138 allow recipient field to use template data

---
+++ rdrf/rdrf/migrations/0011_auto_20151109_1238.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rdrf', '0010_emailnotificationhistory_template_data'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='emailnotification',
+            name='recipient',
+            field=models.CharField(max_length=100, null=True, blank=True),
+        ),
+    ]
