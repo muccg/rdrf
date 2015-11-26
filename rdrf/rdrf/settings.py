38036d8f1cee4eed44791d301b28add4e09a4bca
Lee Render <id2359@yahoo.com>
2015-11-26 17:19:05 +0800

    REG-10 Added settings context grid listing

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -485,3 +485,93 @@ GRID_PATIENT_LISTING = [
         "order": 7
     }
 ]
+
+
+GRID_CONTEXT_LISTING = [
+    {
+        "access": {
+            "default": True,
+            "permission": ""
+        },
+        "data": "full_name",
+        "label": "Patient",
+        "model": "Patient",
+        "order": 1
+    }, {
+        "access": {
+            "default": True,
+            "permission": ""
+        },
+        "data": "date_of_birth",
+        "label": "Date of Birth",
+        "model": "Patient",
+        "order": 2
+    },
+    {
+        "access": {
+            "default": True,
+            "permission": ""
+        },
+        "data": "display_name",
+        "label": "Name",
+        "model": "RDRFContext",
+        "order": 3
+    },
+    {
+        "access": {
+            "default": True,
+            "permission": ""
+        },
+        "data": "created_at",
+        "label": "Created",
+        "model": "RDRFContext",
+        "order": 4
+    },
+
+    {
+        "access": {
+            "default": False,
+            "permission": "patients.can_see_working_groups"
+        },
+        "data": "working_groups_display",
+        "label": "Working Groups",
+        "model": "Patient",
+        "order": 5
+    }, {
+        "access": {
+            "default": False,
+            "permission": "patients.can_see_diagnosis_progress"
+        },
+        "data": "diagnosis_progress",
+        "label": "Diagnosis Entry Progress",
+        "model": "Patient",
+        "order": 6
+    }, {
+        "access": {
+            "default": False,
+            "permission": "patients.can_see_diagnosis_currency"
+        },
+        "data": "diagnosis_currency",
+        "label": "Updated < 365 days",
+        "model": "Patient",
+        "order": 7
+    }, {
+        "access": {
+            "default": False,
+            "permission": "patients.can_see_genetic_data_map"
+        },
+        "data": "genetic_data_map",
+        "label": "Genetic Data",
+        "model": "Patient",
+        "order": 8
+    }, {
+        "access": {
+            "default": True,
+            "permission": ""
+        },
+        "data": "data_modules",
+        "label": "Modules",
+        "model": "Patient",
+        "order": 9
+    }
+]
