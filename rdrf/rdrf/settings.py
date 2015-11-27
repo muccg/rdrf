b3beab44c0727997987c4e18bfbeb95f74641a7b
Lee Render <id2359@yahoo.com>
2015-11-27 15:03:47 +0800

    REG-10 make context_menu column clear

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -495,9 +495,9 @@ GRID_CONTEXT_LISTING = [
             "default": True,
             "permission": ""
         },
-        "data": "full_name",
+        "data": "context_menu",
         "label": "Patient",
-        "model": "Patient",
+        "model": "func",
         "order": 1
     }, {
         "access": {
@@ -566,14 +566,5 @@ GRID_CONTEXT_LISTING = [
         "label": "Genetic Data",
         "model": "Patient",
         "order": 8
-    }, {
-        "access": {
-            "default": True,
-            "permission": ""
-        },
-        "data": "data_modules",
-        "label": "Modules",
-        "model": "Patient",
-        "order": 9
     }
 ]
