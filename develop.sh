5339d99570899b255ce72ca866d1dcd35cc8fced
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-11-30 21:39:41 +0800

    docker-compose: expose return values for test runs

--- develop.sh
+++ develop.sh
@@ -129,10 +129,15 @@ lettuce() {
     _selenium_stack_up
 
     set -x
-    docker-compose --project-name ${PROJECT_NAME} -f docker-compose-lettuce.yml up
+    set +e
+    docker-compose --project-name ${PROJECT_NAME} -f docker-compose-lettuce.yml run --rm lettucehost
+    rval=$?
+    set -e
     set +x
 
     _selenium_stack_down
+
+    exit $rval
 }
 
 
@@ -140,10 +145,15 @@ selenium() {
     _selenium_stack_up
 
     set -x
-    docker-compose --project-name ${PROJECT_NAME} -f docker-compose-seleniumtests.yml up
+    set +e
+    docker-compose --project-name ${PROJECT_NAME} -f docker-compose-seleniumtests.yml run --rm seleniumtesthost
+    rval=$?
+    set -e
     set +x
 
     _selenium_stack_down
+
+    exit $rval
 }
 
 
@@ -178,10 +188,15 @@ unit_tests() {
     docker-compose --project-name ${PROJECT_NAME} -f docker-compose-teststack.yml build ${DOCKER_COMPOSE_BUILD_OPTIONS}
     docker-compose --project-name ${PROJECT_NAME} -f docker-compose-teststack.yml up -d
 
-    docker-compose --project-name ${PROJECT_NAME} -f docker-compose-unittests.yml up
+    set +e
+    docker-compose --project-name ${PROJECT_NAME} -f docker-compose-unittests.yml run --rm testhost
+    rval=$?
+    set -e
 
     docker-compose --project-name ${PROJECT_NAME} -f docker-compose-teststack.yml stop
     set +x
+
+    return $rval
 }
 
 
