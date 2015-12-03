39110252a225eaf9aa798b3c2018e89cdcb21bb0
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-12-03 16:28:40 +0800

    develop.sh: pulled correct image during build to warm cache

--- develop.sh
+++ develop.sh
@@ -56,7 +56,7 @@ dockerbuild() {
     echo "############################################################# ${PROJECT_NAME} ${gittag}"
 
     # attempt to warm up docker cache
-    docker pull ${image} || true
+    docker pull ${image}:${gittag} || true
 
     for tag in "${image}:${gittag}" "${image}:${gittag}-${DATE}"; do
         echo "############################################################# ${PROJECT_NAME} ${tag}"
