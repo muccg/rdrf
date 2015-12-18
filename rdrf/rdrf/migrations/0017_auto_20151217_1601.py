33ddc3679cb4a7010e2e6c8c0cf35d4255ddb114
Lee Render <id2359@yahoo.com>
2015-12-18 16:19:07 +0800

    RDR-1122 added code to try to fix context_ids ( needs testing )

--- rdrf/rdrf/migrations/0017_auto_20151217_1601.py
+++ rdrf/rdrf/migrations/0017_auto_20151217_1601.py
@@ -4,37 +4,86 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 from rdrf.mongo_client import construct_mongo_client
+from rdrf.utils import mongo_db_name
+
+from registry.patients.models import Patient
 
 
 class MongoContextFixer(object):
-    def __init__(self, client):
+    def __init__(self, client, apps, schema_editor):
         self.client = client
-        self.registries = self._get_registries_to_check()
+        self.apps = apps
+        self.schema_editor = schema_editor
+        self.registry_codes = self._get_registry_codes()
 
-    def _get_registries_to_check(self):
-        return []
+    def _get_registry_codes(self):
+        Registry = self.apps.get_model("rdrf", 'Registry')
+        codes = [r.code for r in Registry.objects.all()]
+        print "found codes: %s" % ",".join(codes)
+        return codes
 
-    def _get_patient_model(self, registry, patient_record):
-        pass
+    def _get_patient_model(self, patient_record):
+        patient_id = None
+        try:
+            patient_id = patient_record.get("django_id", None)
+            Patient = self.apps.get_model("registry.patients", "Patient")
+            patient_model = Patient.objects.get(pk=patient_id)
+            return patient_model
+        except Exception, ex:
+            print "Error getting patient model for %s: %s" % (patient_id, ex)
+            return None
 
-    def _create_default_context(self, registry, patient_model):
-        pass
+    def _get_registry_model(self, registry_code):
+        Registry = self.apps.get_model("rdrf", 'Registry')
+        return Registry.objects.get(code-registry_code)
+
+    def _create_default_context(self, registry_model, patient_model):
+        from rdrf.contexts_api import RDRFContextManager
+        rdrf_context_manager = RDRFContextManager(registry_model)
+        RDRFContext = self.apps.get_model("rdrf", "RDRFContext")
+        patient_contexts = [c for c in RDRFContext.objects.filter(content_type=patient_content_type,
+                                                                  object_id=patient_model.pk,
+                                                                  registry=registry_model)]
+
+        num_contexts = len(patient_contexts)
+
+        if num_contexts == 0:
+            default_context = rdrf_context_manager.create_context(patient_model, "default")
+            default_context.save()
+            print "created default context for patient %s" % patient_model
+            return default_context
+        else:
+            print "There are already %s contexts for patient %s in registry %s" % (num_contexts,
+                                                                                   patient_model.pk,
+                                                                                   registry_model.code)
 
     def _update_record(self, registry, patient_record, context_id):
-        pass
+        mongo_id = patient_record['_id']
+        patient_record["context_id"] = context_id
+        registry["cdes"].update({'_id': mongo_id}, {"$set": patient_record}, upsert=False)
+        print "Updated patient %s mongo record with context_id %s" % (patient_record["django_id"],
+                                                                      context_id)
+
+
 
     def fix_registries(self):
-        for registry in self.registries:
+        for registry_code in self.registry_codes:
+            registry = self.client[mongo_db_name(registry_code)]
+            registry_model = self._get_registry_model(registry_code)
+
             for patient_record in registry["cdes"].find({"django_model": "Patient"}):
                 if "context_id" not in patient_record:
-                    patient_model = self._get_patient_model(registry, patient_record)
-                    default_context_model = self._create_default_context(registry, patient_model)
-                    self._update_record(registry, patient_record, default_context_model.pk)
+                    patient_model = self._get_patient_model(patient_record)
+                    default_context_model = self._create_default_context(registry_model, patient_model)
+                    if default_context_model is not None:
+                        self._update_record(registry, patient_record, default_context_model.pk)
+                    else:
+                        print ""
 
 
 def forwards_func(apps, schema_editor):
     client = construct_mongo_client()
-    context_fixer = MongoContextFixer(client)
+    context_fixer = MongoContextFixer(client, apps, schema_editor)
     context_fixer.fix_registries()
 
 
