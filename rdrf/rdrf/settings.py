5c8bbf77ee903d9448db666fd045e1dccbc54924
Lee Render <id2359@yahoo.com>
2016-01-15 16:35:36 +0800

    RDR-1127 moved DOB column to same place in contexts listing as in patients listing

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -514,18 +514,19 @@ GRID_CONTEXT_LISTING = [
             "default": True,
             "permission": ""
         },
-        "data": "context_menu",
-        "label": "Modules",
-        "model": "func",
+        "data": "date_of_birth",
+        "label": "Date of Birth",
+        "model": "Patient",
         "order": 1
-    }, {
+    },
+    {
         "access": {
             "default": True,
             "permission": ""
         },
-        "data": "date_of_birth",
-        "label": "Date of Birth",
-        "model": "Patient",
+        "data": "context_menu",
+        "label": "Modules",
+        "model": "func",
         "order": 2
     },
     {
@@ -585,5 +586,16 @@ GRID_CONTEXT_LISTING = [
         "label": "Genetic Data",
         "model": "Patient",
         "order": 8
+    },
+
+     {
+        "access": {
+            "default": True,
+            "permission": ""
+        },
+        "data": "diagnosis_progress",
+        "label": "Data Entry Progress",
+        "model": "Patient",
+        "order": 9
     }
 ]
