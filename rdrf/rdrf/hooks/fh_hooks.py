69a48a4db0b36cda18a0c51fc889733c614a860f
Lee Render <id2359@yahoo.com>
2016-01-18 15:48:48 +0800

    RDR-1134 addition of contexts broke the way FH updates clinical form data on patient creation

--- rdrf/rdrf/hooks/fh_hooks.py
+++ rdrf/rdrf/hooks/fh_hooks.py
@@ -17,14 +17,20 @@ def mark_created_patient_as_index(patient, registry_ids):
     from rdrf.models import Registry
     from rdrf.dynamic_data import DynamicDataWrapper
 
-    def has_no_mongo_data(patient):
-        wrapper = DynamicDataWrapper(patient)
-        data = wrapper.load_dynamic_data('fh', 'cdes')
+    def has_no_mongo_data(patient, registry_model):
+        logger.debug("checking mongo data")
+        default_context = patient.default_context(registry_model)
+        if default_context is None:
+            # for new patient this will be true
+            return True
+        wrapper = DynamicDataWrapper(patient, rdrf_context_id=default_context.pk)
+        data = wrapper.load_dynamic_data(registry_model.code, 'cdes')
+        logger.debug("loaded dynamic data = %s" % data)
         return data is None
 
     fh = Registry.objects.get(code="fh")
 
-    if fh.has_feature('family_linkage') and fh.pk in registry_ids and has_no_mongo_data(patient):
+    if fh.has_feature('family_linkage') and fh.pk in registry_ids and has_no_mongo_data(patient, fh):
         # patient has just been added to fh
         logger.debug("fh registry added hook running setting to index")
         patient.set_form_value("fh", "ClinicalData", "fhDateSection", "CDEIndexOrRelative", "fh_is_index")
