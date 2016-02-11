d8eb44d1a05ac8a629e6d26eb55cef10de0a1746
Maciej Radochonski <mradochonski@gmail.com>
2016-02-11 12:40:18 +0800

    ANG-66 If user is logged in and got to registry splash screen it fails

--- rdrf/rdrf/registry_view.py
+++ rdrf/rdrf/registry_view.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.views.generic.base import View
 from django.template.context_processors import csrf
 from django.core.exceptions import ObjectDoesNotExist
+from registry.patients.models import ParentGuardian
 import logging
 
 from models import Registry
@@ -13,6 +14,11 @@ logger = logging.getLogger("registry_log")
 class RegistryView(View):
 
     def get(self, request, registry_code):
+        
+        parent = None
+        if request.user.is_authenticated():
+            parent = ParentGuardian.objects.get(id=request.user.id)
+        
         try:
             registry = Registry.objects.get(code=registry_code)
         except ObjectDoesNotExist:
@@ -21,7 +27,8 @@ class RegistryView(View):
 
         context = {
             'body': registry.splash_screen,
-            'registry_code': registry_code
+            'registry_code': registry_code,
+            'parent': parent
         }
 
         context.update(csrf(request))
