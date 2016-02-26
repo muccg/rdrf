68f36a383accdd3d70e140bc25a34990503f7a8d
Lee Render <id2359@yahoo.com>
2016-02-26 10:35:05 +0800

    RDR-1181 default flag on ContextFormGroup

---
+++ rdrf/rdrf/migrations/0022_contextformgroup_is_default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rdrf', '0021_auto_20160223_1344'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='contextformgroup',
+            name='is_default',
+            field=models.BooleanField(default=False),
+        ),
+    ]
--- rdrf/rdrf/models.py
+++ rdrf/rdrf/models.py
@@ -1762,6 +1762,7 @@ class ContextFormGroup(models.Model):
     context_type = models.CharField(max_length=1, default="F", choices=CONTEXT_TYPES)
     name = models.CharField(max_length=80)
     naming_scheme = models.CharField(max_length=1, default="D", choices=NAMING_SCHEMES)
+    is_default = models.BooleanField(default=False)
 
     @property
     def forms(self):
