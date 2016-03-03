45cc35d86be6b2681444e2f9a368f2a90a7bcc5c
Lee Render <id2359@yahoo.com>
2016-03-03 17:10:52 +0800

    RDR-1200 fh hook was dumping into a new context

--- rdrf/rdrf/hooks/fh_hooks.py
+++ rdrf/rdrf/hooks/fh_hooks.py
@@ -17,13 +17,18 @@ def mark_created_patient_as_index(patient, registry_ids):
     from rdrf.models import Registry
     from rdrf.dynamic_data import DynamicDataWrapper
 
+    def get_clinical_context(patient_model, registry_model):
+        for context_model in patient_model.context_models:
+           if context_model.context_form_group.pk == registry_model.default_context_form_group.pk:
+               return context_model
+
     def has_no_mongo_data(patient, registry_model):
         logger.debug("checking mongo data")
-        default_context = patient.default_context(registry_model)
-        if default_context is None:
-            # for new patient this will be true
+        context_model  = get_clinical_context(patient, registry_model)
+        if context_model is None:
+            # true when a new patient
             return True
-        wrapper = DynamicDataWrapper(patient, rdrf_context_id=default_context.pk)
+        wrapper = DynamicDataWrapper(patient, rdrf_context_id=context_model.pk)
         data = wrapper.load_dynamic_data(registry_model.code, 'cdes')
         logger.debug("loaded dynamic data = %s" % data)
         return data is None
