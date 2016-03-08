2df87c1aba2dc754d381973d217cd7488cf25e56
Lee Render <id2359@yahoo.com>
2016-03-08 17:16:50 +0800

    RDR-1181 fh hooks failing

--- rdrf/rdrf/hooks/fh_hooks.py
+++ rdrf/rdrf/hooks/fh_hooks.py
@@ -1,21 +1,33 @@
 from rdrf.hooking import hook
+from rdrf.models import RDRFContext, ContextFormGroup
+from rdrf.models import Registry
+from rdrf.dynamic_data import DynamicDataWrapper
 
 from logging import getLogger
-
 logger = getLogger("registry_log")
-
 
 @hook("patient_created_from_relative")
 def mark_as_relative_in_clinical_form(patient):
     # Ensure that a patient created from a relative is marked as a relative in the clinical form
     if patient.in_registry('fh'):
-        patient.set_form_value("fh", "ClinicalData", "fhDateSection", "CDEIndexOrRelative", "fh_is_relative")
+        fh = Registry.objects.get(code="fh")
+        cfg = fh.default_context_form_group
+        new_context = RDRFContext(registry=fh,
+                                  context_form_group=cfg,
+                                  content_object=patient)
+        new_context.display_name = cfg.get_default_name(patient)
+        new_context.save()
+        
+        patient.set_form_value("fh",
+                               "ClinicalData",
+                               "fhDateSection",
+                               "CDEIndexOrRelative",
+                               "fh_is_relative",
+                               context_model=new_context)
 
 
 @hook("registry_added")
 def mark_created_patient_as_index(patient, registry_ids):
-    from rdrf.models import Registry
-    from rdrf.dynamic_data import DynamicDataWrapper
 
     def get_clinical_context(patient_model, registry_model):
         for context_model in patient_model.context_models:
@@ -38,7 +50,6 @@ def mark_created_patient_as_index(patient, registry_ids):
     if fh.has_feature('family_linkage') and fh.pk in registry_ids and has_no_mongo_data(patient, fh):
         # patient has just been added to fh
         # get the current context form group
-        from rdrf.models import RDRFContext, ContextFormGroup
         try:
             cfg = fh.default_context_form_group
             new_context = RDRFContext(registry=fh,
