802b2aa15ab4730ce1c232c71165fc0800f4c2a3
Lee Render <id2359@yahoo.com>
2016-03-18 13:26:09 +0800

    RDR-1143 timing code/removed unnecessary logging

--- rdrf/rdrf/reporting_table.py
+++ rdrf/rdrf/reporting_table.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, MetaData
 from rdrf.dynamic_data import DynamicDataWrapper
 import explorer
 from explorer.utils import DatabaseUtils
+from rdrf.utils import timed
 
 import logging
 logger = logging.getLogger("registry_log")
@@ -53,10 +54,12 @@ class ReportingTableGenerator(object):
         logger.debug("connection_string = %s" % connection_string)
         return create_engine(connection_string)
 
+    @timed
     def create_table(self):
         self.table.create()
         logger.debug("created table based on schema")
 
+    @timed
     def drop_table(self):
         drop_table_sql = "DROP TABLE %s" % self.table_name
         conn = self.engine.connect()
@@ -75,8 +78,8 @@ class ReportingTableGenerator(object):
 
     def _add_reverse_mapping(self, key, value):
         self.reverse_map[key] = value
-        logger.debug("reverse map %s --> %s" % (key, value))
 
+    @timed
     def create_columns(self, sql_metadata, mongo_metadata):
         logger.debug("creating columns from sql and mongo metadata")
         self.columns = set([])
@@ -103,7 +106,6 @@ class ReportingTableGenerator(object):
             self.columns.add(column_from_mongo)
 
     def _create_column_from_sql(self, column_metadata):
-        logger.debug("column metadata = %s" % column_metadata)
         column_name = column_metadata["name"]
         type_name = column_metadata["type_name"]
         # Not sure if this good idea ...
@@ -121,7 +123,6 @@ class ReportingTableGenerator(object):
         else:
             datatype = alc.String
 
-        logger.debug("mapped sql alc data type = %s" % datatype)
         return self._create_column(column_name, datatype)
 
     def _create_column_from_mongo(self, column_name, form_model, section_model, cde_model):
@@ -129,7 +130,6 @@ class ReportingTableGenerator(object):
         self._add_reverse_mapping((form_model, section_model, cde_model), column_name)
         if section_model.allow_multiple:
             # This map is used when we unroll/"denormalise" the multisection data
-            logger.debug("multisection_column_map = %s" % self.multisection_column_map)
             if section_model.code in self.multisection_column_map:
                 self.multisection_column_map[section_model.code].append(column_name)
             else:
@@ -137,6 +137,7 @@ class ReportingTableGenerator(object):
 
         return self._create_column(column_name, column_data_type)
 
+    @timed
     def run_explorer_query(self, database_utils):
         self.create_table()
         self.multisection_unroller.multisection_column_map = self.multisection_column_map
@@ -154,13 +155,11 @@ class ReportingTableGenerator(object):
 
     def insert_row(self, value_dict):
         # each row will be a denormalised section item
-        logger.debug("inserting context dict: %s" % value_dict)
         self.engine.execute(self.table.insert().values(**value_dict))
 
     def _create_column(self, name, datatype=alc.String):
         column = alc.Column(name, datatype, nullable=True)
         self.columns.add(column)
-        logger.debug("added column %s type %s" % (name, datatype))
         return column
 
     def create_schema(self):
@@ -282,5 +281,4 @@ class MongoFieldSelector(object):
 
             projected_cdes.append(value_dict)
 
-        logger.debug("projected cdes = %s" % projected_cdes)
         return json.dumps(projected_cdes)
