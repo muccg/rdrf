a21a721c4fba31e5e3532205f3bed0dfa4d5c2cc
Lee Render <id2359@yahoo.com>
2016-04-05 14:18:08 +0800

    FKRP-100 removed unnecessary import

--- rdrf/explorer/views.py
+++ rdrf/explorer/views.py
@@ -121,7 +121,6 @@ class QueryView(LoginRequiredMixin, View):
 
         if request.is_ajax():
             # populate temporary table
-            from rdrf.reporting_table import ReportingTableGenerator
             humaniser = Humaniser(registry_model)
             multisection_unrollower = MultisectionUnRoller({})
             rtg = ReportingTableGenerator(request.user, registry_model, multisection_unrollower, humaniser)
