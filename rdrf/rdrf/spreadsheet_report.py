266bc9cf5f7d02de4b89cb5702aa0aa8c692438f
Lee Render <id2359@yahoo.com>
2016-04-06 14:40:53 +0800

    FKRP-100 Modifying report after seeing new example

--- rdrf/rdrf/spreadsheet_report.py
+++ rdrf/rdrf/spreadsheet_report.py
@@ -1,19 +1,12 @@
 from rdrf.utils import get_cde_value
 import openpyxl as xl
 import logging
+import json
 from collections import OrderedDict
+from rdrf.utils import generalised_field_expression
+from rdrf.dynamic_data import DynamicDataWrapper
 
 logger = logging.getLogger("registry_log")
-
-
-class SpreadSheetReportFormat:
-    WIDE = "WIDE"
-    LONG = "LONG"
-
-
-class SpreadSheetReportType:
-    CURRENT = "CURRENT"
-    LONGITUDINAL = "LONGITUDINAL"
 
 
 def default_time_window():
@@ -27,33 +20,29 @@ def default_time_window():
 class SpreadSheetReport(object):
 
     def __init__(self,
-                 user,
-                 registry_model,
-                 working_groups,
-                 cde_triples,
-                 time_window=default_time_window(),
-                 report_format=SpreadSheetReportFormat.WIDE,
-                 mongo_client=None,
+                 query_model,
                  testing=False):
-        self.user = user
+        self.registry_model = query_model.registry
+        self.projection_list = json.loads(query_model.projection)
+        self.longitudinal_column_map = self._build_longitudinal_column_map()
         self.output_filename = "%s_report.xlsx" % registry_model.code
         self.work_book = xl.WorkBook()
-        self.working_groups = working_groups
-        self.registry_model = registry_model
-        self.report_format = report_format
-        if time_window is None:
-            self.report_type = SpreadSheetReportType.CURRENT
-        else:
-            self.report_type = SpreadSheetReportType.LONGITUDINAL
-
-        # pair of datetimes (start, finish) ( inclusive)
-        self.time_window = time_window
-        self.cdes_triples = cde_triples  # triples of form_model, section_model, cde_model
-        self.mongo_client = mongo_client
         self.testing = testing
         self.current_sheet = None
         self.current_row = 1
         self.current_col = 1
+
+    def _build_longitudinal_column_map(self):
+        d = {}
+        for cde_dict in self.projection_list:
+            if cde_dict["longitudinal"]:
+                form_name = cde_dict["formName"]
+                section_code = cde_dict["sectionCode"]
+                if (form_name, section_code) in d:
+                    d[(form_name, section_code)].append(cde_dict["cdeCode"])
+                else:
+                    d[(form_name, section_code)] = [cde_dict["cdeCode"]]
+        return d
 
     def _move_right(self):
         self.current_col += 1
@@ -78,115 +67,110 @@ class SpreadSheetReport(object):
             row=self.current_row, column=self.current_col)
         cell.value = value
         logger.debug("cell updated: row %s col %s -> %s" % (self.current_row,
-                                              self.current_col,
-                                              value)
+                                                            self.current_col,
+                                                            value))
 
     def generate(self):
-        for cde_triple in self.cde_triples:
-            self._create_sheet(cde_triple)
+        config = json.loads(self.query_model.sql_query)
+        static_sheets = config["static_sheets"]
+        for static_sheet_dict in static_sheets:
+            name = registry_model.code.upper() + static_sheet["name"]
+            columns = static_sheet["columns"]
+            self._create_static_sheet(name, columns)
+        # these columns are always included prior
+        universal_columns = config["universal_columns"]
+        # to the longitudinal data
+        for form_model in self.registry_model.forms:
+            for section_model in form_model.section_models:
+                if self._section_in_report(form_model, section_model):
+                    self._create_longitudinal_section_sheet(
+                        universal_columns, form_model, section_model)
 
-    def _create_sheet(self, cde_triple):
-        sheet_name=self._get_sheet_name(cde_triple)
-        sheet=self.work_book.create_sheet()
-        self.current_sheet=sheet
+    def _section_in_report(self, form_model, section_model):
+        for cde_dict in self.projection_list:
+            if form_model.name == cde_dict["formName"] and section_model.code == cde_dict["sectionCode"]:
+                return True
+
+    def _create_static_sheet(self, name, columns):
+        self._create_sheet(name)
+        self._write_header_row(columns)
+        self._next_row()
 
         for patient in self._get_patients():
-            self._add_patient_rows(patient, cde_triple)
+            self._write_row(patient, columns)
+            self._next_row()
 
-    def _get_sheet_name(self, cde_triple):
-        form_model, section_model, cde_model=cde_triple
-        name="%s %s %s" % (form_model.name[:3],
-                             section_model.code[:3],
-                             cde_model.name)
-        return name.upper()
+    def _write_row(self, patient, columns):
+        for column in columns:
+            value = evaluate_field_expression(
+                self.registry_model, patient, column)
+            self._write_cell(value)
+            self._next_cell()
+
+    def _create_longitudinal_section_sheet(self, form_model, section_model, universal_columns):
+        sheet_name = self.registry_model.code.upper() + section_model.code
+        self._create_sheet(sheet_name)
+        self._write_longitudinal_header(
+            universal_columns, form_model, section_model)
+        self.next_row()
+        for patient in self._get_patients():
+            self._write_row(patient, universal_columns)
+            self._write_longitudinal_row(patient, form_model, section_model)
+            self.next_row()
+
+    def _create_sheet(self, name):
+        sheet = self.work_book.create_sheet()
+        self.current_sheet = sheet
+        self.current_row = 1
+        self.current_col = 1
+
+    def _write_header_row(self, columns):
+        for column_name in columns:
+            self._write_cell(column_name)
+            self.next_cell()
 
     def _get_patients(self):
         from rdrf.registry.patients import Patient
         return Patient.objects.filter(working_groups__in=[self.working_groups],
-                                      rdrf_registry__in=[self.registry_model])
+                                      rdrf_registry__in=[self.registry_model]).order_by("id")
 
-    def _get_static_data(self, patient):
-        # the demographic "fixed" data to appear in the row
-        d=OrderedDict()
-        d["id"]=patient.pk
-        d["sex"]=patient.sex
-        d["dob"]=patient.date_of_birth
-        return d
+    def _write_longitudinal_header(self, universal_columns, form_model):
 
+    def _write_longitudinal_row(self, patient, form_model, section_model):
+        cde_codes = self._longitudinal_column_map.get(
+            (form_model.name, section_model.code), [])
+        for snapshot in self._get_snapshots(patient):
+            timestamp = self._get_timestamp_from_snapshot(snapshot)
+            values = []
+            for cde_code in cde_codes:
+                value = self._get_cde_value_from_snapshot(snapshot,
+                                                          form_model.name,
+                                                          section_model.code,
+                                                          cde_code)
+                values.append(value)
+            self._write_cell(timestamp)
+            self.next_cell()
+            for value in values:
+                self._write_cell(value)
+                self.next_cell()
 
-    def _get_longitudinal_data(self, patient, cde_triple):
-        from rdrf.dynamic_data import DynamicDataWrapper
-        wrapper=DynamicDataWrapper(patient)
-        wrapper.testing=self.testing
-        wrapper.client=self.mongo_client
-        # get all snapshots for this patient
-        history_collection=wrapper._get_collection(
+    @cached
+    def _get_snapshots(self, patient_model):
+        wrapper = DynamicDataWrapper(patient)
+        wrapper.testing = self.testing
+        wrapper.client = self.mongo_client
+        history_collection = wrapper._get_collection(
             self.registry_model.code, "history")
-        lower_bound, upper_bound=self._get_timestamp_bounds()
+        lower_bound, upper_bound = self._get_timestamp_bounds()
 
-        patient_snapshots=history_collection.find({"django_id": patient.pk,
+        patient_snapshots = history_collection.find({"django_id": patient.pk,
                                                      "registry_code": self.registry_model.code,
                                                      "django_model": "Patient",
                                                      "record_type": "snapshot"})
-
-        # return a list of date pairs [ (timestamp1, value1), (timestamp2,
-        # value2), ...]
-        pairs=self._get_value_pairs(patient_snapshots, cde_triple)
-        logger.debug("pairs = %s" % pairs)
-        return pairs
-
-    def _get_value_pairs(self, patient_snapshots, cde_triple):
-        pairs=[]
-        form_model, section_model, cde_model=cde_triple
-        snapshots=[s for s in patient_snapshots]
-        logger.debug("snapshots = %s" % snapshots)
-        for snapshot in snapshots:
-            timestamp=snapshot["timestamp"]
-            patient_record=snapshot["record"]
-            value=get_cde_value(form_model, section_model,
-                                  cde_model, patient_record)
-            pairs.append((timestamp, value))
-        return pairs
+        return patient_snapshots
 
     def _get_timestamp_bounds(self):
-        dt_lower, dt_upper=self.time_window
-        lower_bound=dt_lower
-        upper_bound=dt_upper
+        dt_lower, dt_upper = self.time_window
+        lower_bound = dt_lower
+        upper_bound = dt_upper
         return (lower_bound, upper_bound)
-
-    def _add_wide_row(self, patient, cde_triple, static_data, longitudinal_data):
-        # id|sex|date_of_birth|diagnosis|etc|date1|cde value1|date2|cde value2|
-        # ...
-        self._write_static_data(patient, static_data)
-        self._write_longitudinal_wide(longitudinal_data)
-
-    def _write_static_data(self, patient, static_data):
-        for field in static_data:
-            value=static_data[field]
-            self._write_cell(value)
-            self._move_right()
-
-    def _write_longitudinal_wide(self, longitudinal_data):
-        for timestamp, field_value in longitudinal_data:
-            self._write_cell(timestamp)
-            self._move_right()
-            self._write_cell(field_value)
-
-    def _add_long_rows(self, patient, cde_triple, static_data, longitudinal_data):
-        # id|sex|date_of_birth|diagnosis|etc|date1|cde value1
-        # id|sex|date_of_birth|diagnosis|etc|date2|cde value2
-        # id|sex|date_of_birth|diagnosis|etc|date3|cde value3
-        pass
-
-    def _add_patient_rows(self, patient, cde_triple):
-        # the non-varying part - should this be configured per varying cde
-        # though?
-        static_data=self._get_static_data(patient)
-        longitudinal_data=self._get_longitudinal_data(patient, cde_triple)
-        if self.report_format == SpreadSheetReportFormat.WIDE:
-            self._add_wide_row(patient, cde_triple,
-                               static_data, longitudinal_data)
-            self._move_next_row()
-        else:
-            self._add_long_rows(patient, cde_triple,
-                                static_data, longitudinal_data)
