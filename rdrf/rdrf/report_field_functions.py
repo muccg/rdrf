f32891abd4c2e99aef7cf1bb8a87751bc686d59c
Lee Render <id2359@yahoo.com>
2016-04-07 15:30:00 +0800

    FKRP-100 Added country report function

--- rdrf/rdrf/report_field_functions.py
+++ rdrf/rdrf/report_field_functions.py
@@ -1,5 +1,17 @@
 from rdrf.utils import report_function
+from registry.patients.models import PatientAddress
+
 
 @report_function
 def professionals(patient_model):
     return patient_model.clinician
+
+@report_function
+def country(patient_model):
+    try:
+        patient_address = PatientAddress.objects.get(patient=patient_model)
+        return patient_address.country
+    except:
+        pass
+    
+
