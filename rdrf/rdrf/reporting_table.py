5f67f8b1725ceb4c39a130f958c4e3a822cea2f6
Lee Render <id2359@yahoo.com>
2016-04-18 14:46:23 +0800

    RDR-1222 column label length restricted

--- rdrf/rdrf/reporting_table.py
+++ rdrf/rdrf/reporting_table.py
@@ -368,7 +368,7 @@ class ReportTable(object):
             form_model = RegistryForm.objects.get(pk=int(form_pk))
             section_model = Section.objects.get(pk=int(section_pk))
             cde_model = CommonDataElement.objects.get(code=cde_code)
-            s = form_model.name[:3] + "_" + section_model.display_name[:3] + "_" + cde_model.name
+            s = form_model.name[:3] + "_" + section_model.display_name[:3] + "_" + cde_model.name[:30]
             return s.upper().encode('ascii', 'replace')
             
         except Exception, ex:
