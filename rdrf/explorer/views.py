edd5b0fc386ee626baef6c047fd3c0ce73716957
Lee Render <id2359@yahoo.com>
2016-04-19 13:21:32 +0800

    FKRP-100 if query invalid dont 500...

--- rdrf/explorer/views.py
+++ rdrf/explorer/views.py
@@ -141,6 +141,9 @@ class QueryView(LoginRequiredMixin, View):
                 m.save()
                 query_form.save_m2m()
                 return redirect(m)
+            else:
+                return redirect(query_model)
+                
 
 
 class DownloadQueryView(LoginRequiredMixin, View):
