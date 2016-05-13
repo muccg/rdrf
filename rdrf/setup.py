1f159d725b46027a816fd83b2c1211c5534772c7
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2016-05-13 12:51:58 +0800

    update links in footer

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -47,7 +47,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="1.2.2.1",
+      version="1.2.3.0",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
