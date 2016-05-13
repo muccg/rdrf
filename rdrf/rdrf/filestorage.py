7f3a1449532c71ef252679c8e8cec62ef28ea783
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-05-13 17:30:45 +0100

    RDR-1265 Add new file uploads to django file storage backend

--- rdrf/rdrf/filestorage.py
+++ rdrf/rdrf/filestorage.py
@@ -1,39 +1,76 @@
-# Attempt to clean up the interaction with GridFS
 import logging
+import re
+import gridfs
+from bson.objectid import ObjectId
+from .models import Registry, CDEFile
+from .utils import models_from_mongo_key
 
 logger = logging.getLogger(__name__)
 
+__all__ = ["get_id", "delete_file_wrapper", "get_file",
+           "store_file", "store_file_by_key"]
 
-class GridFSApi(object):
-    def __init__(self, gridfs_filestore):
-        self.gridfs_filestore = gridfs_filestore
+def get_id(value):
+    if isinstance(value, dict):
+        return value.get("gridfs_file_id") or value.get("django_file_id")
+    return None
 
-    # Public API
+def delete_file_wrapper(fs, file_ref):
+    gridfs_file_id = file_ref.get("gridfs_file_id")
+    django_file_id = file_ref.get("django_file_id")
+    logger.debug("existing file ids: gridfs = %s django = %s" % (gridfs_file_id, django_file_id))
+    if gridfs_file_id is not None:
+        if not fs.exists(gridfs_file_id):
+            logger.info("%s did not exist in filestore" % gridfs_file_id)
 
-    def store(self, registry_model, patient_model, file_cde_model, file_like_object):
-        """
-        Store a file uploaded by a user into gridfs - return the the gridfs file id
-        """
-        original_filename = file_like_object.name
-        added_file_id = self.gridfs_filestore.put(file_like_object.read(), filename=original_filename)
-        logger.info("Added file to gridfs for %s %s %s %s id = %s" % (registry_model.code,
-                                                                      patient_model,
-                                                                      file_cde_model.code,
-                                                                      original_filename,
-                                                                      added_file_id))
-        gridfs_ref_dict = {"gridfs_file_id": added_file_id, "file_name": original_filename}
-        logger.debug("gridfs_ref_dict = %s" % gridfs_ref_dict)
-        return gridfs_ref_dict
+        try:
+            fs.delete(gridfs_file_id)
+        except Exception, ex:
+            logger.error("Error deleting file id %s in gridfs: %s" % (gridfs_file_id,
+                                                                      ex))
+            return None
+        logger.debug("deleted gridfs file OK")
+        return gridfs_file_id
 
-    def delete(self, gridfs_file_id):
-        if self.gridfs_filestore.exists(gridfs_file_id):
-            self.gridfs_filestore.delete(gridfs_file_id)
-            logger.info("GridFS API: deleted %s OK" % gridfs_file_id)
-        else:
-            logger.info("%s did not exist in filestore")
+    if django_file_id is not None:
+        try:
+            CDEFile.objects.get(id=django_file_id).delete()
+        except CDEFile.DoesNotExist:
+            logger.warning("Tried to delete CDEFile id=%s which doesn't exist" % django_file_id)
+        except Exception as e:
+            logger.exception("Couldn't delete CDEFile id=%s" % django_file_id)
+        return django_file_id
 
-    # Private
+    return None
 
-    def _get_gridfs_filename(self, registry_model, patient_model, file_cde_model, original_filename):
-        return "%s_%s_%s_%s" % (registry_model.code, patient_model, file_cde_model.code, original_filename)
+def store_file(registry, cde, file_obj, form=None, section=None):
+    cde_file = CDEFile(registry=registry,
+                       form=form, section=section, cde=cde,
+                       item=file_obj, filename=file_obj.name)
+    cde_file.save()
 
+    return {
+        "django_file_id": cde_file.id,
+        "file_name": file_obj.name
+    }
+
+def store_file_by_key(registry_code, patient_record, key, file_obj):
+    registry = Registry.objects.get(code=registry_code)
+    form, section, cde = models_from_mongo_key(registry, key)
+    return store_file(registry, cde, file_obj, form, section)
+
+oid_pat = re.compile(r"[0-9A-F]{24}", re.I)
+
+def get_file(file_id, gridfs=None):
+    if gridfs and oid_pat.match(str(file_id)):
+        data = gridfs.get(ObjectId(file_id))
+        filename = data.filename.split("****")[-1]
+        return data, filename
+    else:
+        try:
+            cde_file = CDEFile.objects.get(id=file_id)
+            return cde_file.item, cde_file.filename
+        except CDEFile.DoesNotExist:
+            pass
+
+    return None, None
