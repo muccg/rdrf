8a32c47b9416d53ff37bfe97f8e1db51927c96fb
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-05-13 11:48:14 +0100

    Remove GridFS Django storage backend
    It's not feasible to use a GridFS Django storage backend because the
    mongo database changes depending on the registry code.

--- rdrf/rdrf/settings.py
+++ rdrf/rdrf/settings.py
@@ -160,7 +160,6 @@ INSTALLED_APPS = [
     'templatetag_handlebars',
     'iprestrict',
     'rest_framework',
-    'storages',
 ]
 
 
@@ -634,8 +633,3 @@ GRID_CONTEXT_LISTING = [
         "order": 8
     }
 ]
-
-DEFAULT_FILE_STORAGE = 'rdrf.storage.GridFSStorage'
-GRIDFS_HOST = MONGOSERVER
-GRIDFS_PORT = MONGOPORT
-GRIDFS_COLLECTION = "test_files"
