52ab3c9b606e9312483d13a736bc1509d0e43cff
Maciej Radochonski <mradochonski@gmail.com>
2016-05-25 16:06:43 +0800

    Merge aftermath

--- rdrf/rdrf/parent_view.py
+++ rdrf/rdrf/parent_view.py
@@ -123,7 +123,7 @@ class ParentView(BaseParentView):
                 self.set_rdrf_context(patient, context_id)
                 patients.append({
                     "patient": patient,
-                    "consent": consent_status_for_patient(registry_code, patient)
+                    "consent": consent_status_for_patient(registry_code, patient),
                     "context_id": self.rdrf_context.pk
                 })
 
