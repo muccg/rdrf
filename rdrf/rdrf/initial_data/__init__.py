87e9f732773b6dd444757474ec74e26f0f294ab6
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2016-05-31 16:40:50 +0300

    RDR-1296: Removed some of the datasets from --list

--- rdrf/rdrf/initial_data/__init__.py
+++ rdrf/rdrf/initial_data/__init__.py
@@ -2,13 +2,9 @@ from . import (base, groups, users, sample_registry, sample_laboratories,
     genes, genes_smaller_sample, DEV, iprestrict_permissive)
 
 datasets = [
-    base,
     DEV,
     groups,
     users,
-    sample_registry,
-    sample_laboratories,
     genes,
-    genes_smaller_sample,
     iprestrict_permissive,
 ]
