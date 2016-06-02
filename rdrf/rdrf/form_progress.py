54e76027f3dc41923c05ecb3ef8bcca31386319d
Lee Render <id2359@yahoo.com>
2016-06-02 15:11:42 +0800

    RDR-1311 make form progress not cark at None

--- rdrf/rdrf/form_progress.py
+++ rdrf/rdrf/form_progress.py
@@ -114,6 +114,8 @@ class FormProgress(object):
         return result
 
     def _get_values_from_multisection(self, form_model, section_model, cde_model, dynamic_data):
+        if dynamic_data is None:
+            return []
         for form_dict in dynamic_data["forms"]:
             if form_dict["name"] == form_model.name:
                 for section_dict in form_dict["sections"]:
@@ -132,6 +134,10 @@ class FormProgress(object):
             return 1
         else:
             n = 0
+            if dynamic_data is None:
+                return 0
+            if "forms" not in dynamic_data:
+                return 0
             for form_dict in dynamic_data["forms"]:
                 if form_dict["name"] == form_model.name:
                     for section_dict in form_dict["sections"]:
