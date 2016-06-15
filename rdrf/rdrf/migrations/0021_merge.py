79dd5c67e994ace0a28ace64f1a9c31c9b8720db
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-06-15 22:33:32 +0100

    Django migration merge

---
+++ rdrf/rdrf/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rdrf', '0020_emailnotification_disabled'),
+        ('rdrf', '0020_cdefile'),
+    ]
+
+    operations = [
+    ]
