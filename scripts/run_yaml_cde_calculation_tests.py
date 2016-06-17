7a1fd918e9f9acd56cb969e266b0ec07346d46f3
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-06-17 09:01:14 +0100

    fixup! Add a script for running registry yaml csv test cases
    Fix python2 unicode things.
    
    Add more info to the output so that it's easier to develop tests.

--- scripts/run_yaml_cde_calculation_tests.py
+++ scripts/run_yaml_cde_calculation_tests.py
@@ -23,8 +23,11 @@ def main():
     parser.add_argument("test_csvs", metavar="CSV", nargs="+",
                         type=argparse.FileType("r"),
                         help="CSV files containing test definitions")
-    parser.add_argument("--outfile", metavar="FILE", type=argparse.FileType("w"),
+    parser.add_argument("--outfile", metavar="FILE", type=argparse.FileType("wb"),
                         help="Output file")
+    parser.add_argument("--verbose", "-v", action="count",
+                        help="More info for debugging tests")
+
     args = parser.parse_args()
 
     if not args.outfile: args.outfile = sys.stdout
@@ -33,7 +36,7 @@ def main():
 
     success = True
     for infile in args.test_csvs:
-        success = run_tests(registry, infile, args.outfile) and success
+        success = run_tests(registry, infile, args) and success
     return 0 if success else 1
 
 Registry = namedtuple("Registry", ("names", "calculations"))
@@ -51,44 +54,51 @@ def load_yaml(file_obj):
     return Registry(names, calculations)
 
 TestResult = namedtuple("TestResult", ("test", "expected", "actual", "error", "output"))
-TestCase = namedtuple("TestCase", ("file", "number", "check_code", "params"))
+TestCase = namedtuple("TestCase", ("file", "number", "check_code", "params", "desc"))
 
-def run_tests(registry, csv_file, outfile):
+def run_tests(registry, csv_file, opts):
     num_tests = 0
     num_success = 0
     num_errors = 0
     for index, row in enumerate(csv.DictReader(csv_file)):
-        t = setup_test(row, index + 1, csv_file.name)
         num_tests += 1
+        t = setup_test(row, index + 1, csv_file.name)
         res = run_test(registry, t)
 
         if res.error:
-            print_error(registry, res, row, outfile)
+            print_error(registry, res, t.params, opts)
             num_errors += 1
         elif res.expected == res.actual:
+            print_success(registry, res, t.params, opts)
             num_success += 1
         else:
-            print_failure(registry, res, row, outfile)
+            print_failure(registry, res, t.params, opts)
 
     return num_tests == num_success
 
 def setup_test(cols, num, filename):
-    check_code = cols["check"]
-    params = ((code, val) for code, val in cols.items() if code != "check")
-    return TestCase(filename, num, check_code, dict(params))
+    check_code = cols["check"].decode("utf-8")
+    desc = cols.get("testcase", "").decode("utf-8")
+    params = {code.decode("utf-8"): val.decode("utf-8")
+              for code, val in cols.items()
+              if code not in ("check", "testcase")}
+    return TestCase(filename, num, check_code, params, desc)
 
 def load_adsafe_js():
     adsafe_path = "../rdrf/rdrf/static/js/vendor/adsafe-min.js"
     return io.open(os.path.join(os.path.dirname(__file__), adsafe_path)).read()
 
 def run_test(registry, test):
-    context = dict((code, val) for code, val in test.params.items() if code in registry.names)
-    patient = dict((code, val) for code, val in test.params.items() if code not in registry.names)
+    context = {code: val for code, val in test.params.items()
+               if code in registry.names}
+    patient = {code: val for code, val in test.params.items()
+               if code not in registry.names}
     script = registry.calculations[test.check_code]
 
     script = u"""
         "use strict";
         var document = {};
+        var window = { console: console };
         %s
 
         var patient = %s;
@@ -103,13 +113,17 @@ def run_test(registry, test):
     success, output = exec_script(script)
 
     if success:
-        lines = [o.strip() for o in output.split("\n")]
-        non_empty = filter(bool, lines)
-        context_result = non_empty[-1] if non_empty else None
+        context_result, output = parse_output(output)
         return TestResult(test, test.params[test.check_code].strip(), context_result,
-                          False, "\n".join(lines[0:-1]))
+                          False, output)
     else:
         return TestResult(test, None, None, True, output)
+
+def parse_output(output):
+    lines = [o.strip() for o in output.split("\n")]
+    non_empty = filter(bool, lines)
+    result = non_empty[-1] if non_empty else None
+    return (result, "\n".join(lines[0:-1]) + "\n")
 
 def exec_script(script):
     with tempfile.NamedTemporaryFile(mode="w+", suffix=".js", prefix="registry_test_") as js:
@@ -130,11 +144,26 @@ def exec_script(script):
             return False, "Couldn't execute %s: %s" % (script, e)
     return False, "Test bug"
 
-def print_error(registry, result, params, outfile):
-    outfile.write("ERROR: %s\n" % str(result.output))
+def print_success(registry, result, params, opts):
+    if opts.verbose > 1:
+        log(opts, u"PASS: %s:%s %s\n" % (result.test.file, result.test.number, result.test.desc))
 
-def print_failure(registry, result, params, outfile):
-    outfile.write("FAIL %s:%s: %s (%s) was \"%s\", expected \"%s\".\n" % (result.test.file, result.test.number, result.test.check_code, registry.names[result.test.check_code], result.actual, result.expected))
+def print_error(registry, result, params, opts):
+    log(opts, u"ERROR: %s\n" % str(result.output))
+
+def print_failure(registry, result, params, opts):
+    log(opts, u"FAIL %s:%s: %s (%s) was \"%s\", expected \"%s\".\n" % (result.test.file, result.test.number, result.test.check_code, registry.names[result.test.check_code], result.actual, result.expected))
+    if opts.verbose:
+        for param in sorted(params):
+            if param in registry.names:
+                log(opts, u"    %s (%s) = \"%s\"\n" % (param, registry.names[param], params[param]))
+            else:
+                log(opts, u"    Patient %s = \"%s\"\n" % (param, params[param]))
+        if result.output:
+            log(opts, result.output)
+
+def log(opts, text):
+    opts.outfile.write(text.encode("utf-8"))
 
 if __name__ == '__main__':
     sys.exit(main())
