7a97c10dc5621fc9760f6abeaf9cc4ce15938abb
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2016-07-08 10:18:22 +0800

    Migration for consent files.

---
+++ rdrf/registry/patients/migrations/0014_auto_20160708_1011.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import registry.patients.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('patients', '0013_auto_20160604_2245'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='patientconsent',
+            name='form',
+            field=models.FileField(storage=registry.patients.models.PatientConsentStorage(), upload_to=b'consents', null=True, verbose_name=b'Consent form', blank=True),
+        ),
+    ]
