fc0b89e09c58db707b3ac225276edbb319c7d84a
Maciej Radochonski <mradochonski@gmail.com>
2016-08-01 15:14:51 +0800

    Missed one conflict

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -45,11 +45,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-<<<<<<< HEAD
       version="1.6.0",
-=======
-      version="1.5.7",
->>>>>>> next_release
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
