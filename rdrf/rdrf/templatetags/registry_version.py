e1d401b53848f0e9f2d34c659597762e3632d69a
Maciej Radochonski <mradochonski@gmail.com>
2016-08-29 13:52:56 +0800

    Added registry_version template tag

---
+++ rdrf/rdrf/templatetags/registry_version.py
@@ -0,0 +1,9 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.simple_tag
+def registry_version(registry_code):
+    pkg = __import__(registry_code)
+    return "%s (%s)" % (pkg.VERSION, settings.FALLBACK_REGISTRY_CODE.upper())
