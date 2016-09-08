016e97bf704c1af2824ab30be0aa66bfe7231592
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-09-08 23:02:07 +0100

    form_view: Fix missing file cde download link in post view (resolves #219)

--- rdrf/rdrf/file_upload.py
+++ rdrf/rdrf/file_upload.py
@@ -1,5 +1,7 @@
+from itertools import izip_longest
 import logging
 from django.core.urlresolvers import reverse, NoReverseMatch
+from django.core.files.uploadedfile import UploadedFile
 from . import filestorage
 
 logger = logging.getLogger(__name__)
@@ -30,11 +32,13 @@ class FileUpload(object):
                         self.gridfs_dict.get("gridfs_file_id")),
         }
 
-        try:
-            return reverse("file_upload", kwargs=kwargs)
-        except NoReverseMatch:
-            logger.info("Couldn't make URL for file record %s" % str(self.gridfs_dict))
-            return ""
+        if kwargs["file_id"]:
+            try:
+                return reverse("file_upload", kwargs=kwargs)
+            except NoReverseMatch:
+                logger.info("Couldn't make URL for file record %s" % str(self.gridfs_dict))
+
+        return ""
 
     def __unicode__(self):
         """
@@ -62,9 +66,38 @@ def wrap_gridfs_data_for_form(registry, data):
             return [wrap(item, key) for item in value]
         elif filestorage.get_id(value):
             return FileUpload(registry, key, value)
+        elif isinstance(value, UploadedFile):
+            return FileUpload(registry, key,
+                              {"file_name": value.name, "django_file_id": 0})
         elif isinstance(value, dict):
-            for key, item in value.items():
-                value[key] = wrap(item, key)
+            return { key: wrap(item, key) for key, item in value.items() }
         return value
 
     return wrap(data, None)
+
+
+def merge_gridfs_data_for_form(registry, form_data, dyn_patient):
+    """
+    Merges existing patient mongo from dyn_patient into form_data, if
+    it's missing.
+    Because form_data is cleaned_data from a Django form, file uploads
+    are missing. In order to display the currently existing files,
+    they need to be copied over from dyn_patient.
+    """
+    def merge(form, dyn, key):
+        if isinstance(form, list) and isinstance(dyn, list):
+            return [merge(form_item, dyn_item, key)
+                    for (form_item, dyn_item) in izip_longest(form, dyn)]
+        elif isinstance(form, UploadedFile):
+            return FileUpload(registry, key,
+                              {"file_name": form.name, "django_file_id": 0})
+        elif isinstance(form, FileUpload):
+            return form
+        elif not filestorage.get_id(form) and filestorage.get_id(dyn):
+            return FileUpload(registry, key, dyn)
+        elif isinstance(form, dict) and isinstance(dyn, dict):
+            for key, item in form.items():
+                form[key] = merge(item, dyn.get(key), key)
+        return form
+
+    return merge(form_data, dyn_patient, None)
