cf69d9bbbcc308edb8670ead7cd2d79f16a98dfc
Lee Render <id2359@yahoo.com>
2016-09-23 10:16:02 +0800

    rdrf #243 cleaned up branch/pep8

--- rdrf/rdrf/file_upload.py
+++ rdrf/rdrf/file_upload.py
@@ -36,7 +36,8 @@ class FileUpload(object):
             try:
                 return reverse("file_upload", kwargs=kwargs)
             except NoReverseMatch:
-                logger.info("Couldn't make URL for file record %s" % str(self.gridfs_dict))
+                logger.info("Couldn't make URL for file record %s" %
+                            str(self.gridfs_dict))
 
         return ""
 
@@ -70,10 +71,11 @@ def wrap_gridfs_data_for_form(registry, data):
             return FileUpload(registry, key,
                               {"file_name": value.name, "django_file_id": 0})
         elif isinstance(value, dict):
-            return { key: wrap(item, key) for key, item in value.items() }
+            return {key: wrap(item, key) for key, item in value.items()}
         return value
 
     return wrap(data, None)
+
 
 def wrap_file_cdes(registry_code, section_data, mongo_data, multisection=False):
     # Wrap file cde data for display in the form
@@ -82,40 +84,38 @@ def wrap_file_cdes(registry_code, section_data, mongo_data, multisection=False):
     # to debug.
 
     # The behaviour of file cdes/fields is as follows:
-    
+
     # File CDEs are stored as dictionaries in Mongo : {"django_file_id": <fileid> , "name": <filename>}.
     # The filestorage module handles the actual file content and retrieval.
-    
+
     # If no file has been uploaded mongo will contain the value None.
-    
+
     # In the gui , the widget for a file cde shows a download link and a clear checkbox.
     # The download link is created by a wrapper class: "FileUpload".
     # The clear button if checked , results in a value of False in the django form cleaned data for that field.
-    # If no file has been uploaded but one already exists for that cde in mongo, the cleaned data actually has value None for that field.
-    
-    # The code below checks each key and value in the cleaned data of a section form ( or multisection formset)
-    # If the field is not a file cde, the value is not unchanged ( i.e. not wrapped)
+    # If no file has been uploaded but one already exists for that cde in
+    # mongo, the cleaned data actually has value None for that field.
 
-    # If the field IS a file cde , we wrap the field's value ( or the correspondign value in Mongo), appropriately.
-    
+    # The code below checks each key and value in the cleaned data of a section form ( or multisection formset)
+    # If the field is not a file cde, the value is not unchanged ( i.e. not
+    # wrapped)
+
+    # If the field IS a file cde , we wrap the field's value ( or the
+    # correspondign value in Mongo), appropriately.
+
     # For a single section section_data will be a dictionary
     # For multisections, section_data will be a list of section dictionaries
 
     # The special value of False in the cleaned form data for a file cde indicates the file has been cleared(deleted)
-    # The value None means no file uploaded ( but there might already be data uploaded in mongo)
+    # The value None means no file uploaded ( but there might already be data
+    # uploaded in mongo)
 
     # So cases are: no upload and no data in mongo - don't wrap
     #               no upload but file already uploaded - wrap mongo data
     #               no upload and value False in form to indicate clearing - don't wrap
-    #               upload of file : wrap form data 
+    #               upload of file : wrap form data
 
-    
-    
-    
     from rdrf.utils import is_file_cde, get_code, get_form_section_code
-
-    logger.debug("wrapping file cdes : mongo_data = %s" % mongo_data)
-    
 
     def is_upload_file(value):
         return isinstance(value, UploadedFile)
@@ -128,7 +128,7 @@ def wrap_file_cdes(registry_code, section_data, mongo_data, multisection=False):
             return False
         if section_index is None:
             # section is not multi
-            return value is None  and key in mongo_data
+            return value is None and key in mongo_data
         else:
             # multisection ...
             try:
@@ -137,33 +137,19 @@ def wrap_file_cdes(registry_code, section_data, mongo_data, multisection=False):
                 return value is None and key in section_dict
             except:
                 return False
-    
+
     def should_wrap(section_index, key, value):
         try:
             cde_code = get_code(key)
         except:
             # not a delimited mongo key
-            logger.debug("FILECDES %s will not be wrapped" % key)
             return False
-            
+
         if is_file_cde(cde_code):
             u = is_upload_file(value)
             fs = is_filestorage_dict(value)
             im = is_existing_in_mongo(section_index, key, value)
-            result = u or fs or im
-            logger.debug("file %s value = %s u = %s fs = %s im = %s will wrap = %s" % (key,
-                                                                                       value,
-                                                                                       u,
-                                                                                       fs,
-                                                                                       im,
-                                                                                       result))
-            
-            if result:
-                logger.debug("FILECDES %s IS a file and will be wrapped" % key)
-                return True
-            else:
-                logger.debug("FILECDES *** file but not wrapped??? key = %s value = %s" % (key, value))
-                return False
+            return u or fs or im
 
         return False
 
@@ -174,27 +160,24 @@ def wrap_file_cdes(registry_code, section_data, mongo_data, multisection=False):
         return FileUpload(registry_code, key, value)
 
     def get_mongo_value(section_index, key):
-        logger.debug("getting mongo value for file %s section index = %s" % (key, section_index))
         if section_index is None:
-            logger.debug("not in a multisection - return value directly...")
-            value =  mongo_data[key]
+            value = mongo_data[key]
         else:
-            logger.debug("retrieving data from a multisection")
             section_code = get_form_section_code(key)[-2]
-            logger.debug("returning mongo data for file in section %s" % section_code)
             section_dicts = mongo_data[section_code]
             correct_section_dict = section_dicts[section_index]
             value = correct_section_dict[key]
 
-        logger.debug("returning value = %s" % value)
         return value
 
     def wrap(section_index, key, value):
+        # NB we need section index in case we're looking inside a multisection
+        # a multisection is just a list of section dicts indexed by
+        # section_index
         if not should_wrap(section_index, key, value):
             return value
 
         if is_filestorage_dict(value):
-            logger.debug("wrapping filestorage dict %s" % value)
             return wrap_filestorage_dict(key, value)
 
         if is_upload_file(value):
@@ -202,11 +185,10 @@ def wrap_file_cdes(registry_code, section_data, mongo_data, multisection=False):
 
         if is_existing_in_mongo(section_index, key, value):
             mongo_value = get_mongo_value(section_index, key)
-            logger.debug("wrapping filestorage dict for key %s section index %s in mongo %s" % (key, section_index, mongo_value))
             return wrap_filestorage_dict(key, mongo_value)
 
     def wrap_section(section_index, section_dict):
-        return { key: wrap(section_index, key, value) for key, value in section_dict.items() }
+        return {key: wrap(section_index, key, value) for key, value in section_dict.items()}
 
     def wrap_multisection(multisection_list):
         return [wrap_section(section_index, section_dict) for section_index, section_dict in enumerate(multisection_list)]
@@ -215,41 +197,3 @@ def wrap_file_cdes(registry_code, section_data, mongo_data, multisection=False):
         return wrap_multisection(section_data)
     else:
         return wrap_section(None, section_data)
-    
-
-def merge_gridfs_data_for_form(registry, form_data, dyn_patient):
-    """
-    Merges existing patient mongo from dyn_patient into form_data, if
-    it's missing.
-    Because form_data is cleaned_data from a Django form, file uploads
-    are missing. In order to display the currently existing files,
-    they need to be copied over from dyn_patient.
-    """
-    def merge(form, dyn, key):
-        if form is False:
-            pass  # deleted file
-        elif isinstance(form, list) and isinstance(dyn, list):
-            return [merge(form_item, dyn_item, key)
-                    for (form_item, dyn_item) in izip_longest(form, dyn)
-                    if form_item is not False]
-        elif isinstance(form, UploadedFile):
-            return FileUpload(registry, key,
-                              {"file_name": form.name, "django_file_id": 0})
-        elif isinstance(form, FileUpload):
-            return form
-        elif not filestorage.get_id(form) and filestorage.get_id(dyn):
-            return FileUpload(registry, key, dyn)
-        elif isinstance(form, dict) and isinstance(dyn, dict):
-            for key, item in form.items():
-                form[key] = merge(item, dyn.get(key), key)
-        return form
-
-    return merge(form_data, dyn_patient, None)
-
-def merge_gridfs_data_for_form_multi(registry, form_data, dyn_patient):
-    """
-    Merges multisection mongo file data into the form fields.
-    """
-    flat_dyn = chain.from_iterable(v for k, v in dyn_patient.items()
-                                   if isinstance(v, list))
-    return merge_gridfs_data_for_form(registry, form_data, list(flat_dyn))
--- rdrf/rdrf/form_view.py
+++ rdrf/rdrf/form_view.py
@@ -18,7 +18,7 @@ from dynamic_forms import create_form_class_for_section
 from dynamic_data import DynamicDataWrapper
 from django.http import Http404
 from questionnaires import PatientCreator
-from file_upload import wrap_gridfs_data_for_form, merge_gridfs_data_for_form, merge_gridfs_data_for_form_multi
+from file_upload import wrap_gridfs_data_for_form
 from file_upload import wrap_file_cdes
 from . import filestorage
 from utils import de_camelcase
@@ -414,15 +414,8 @@ class FormView(View):
                     dynamic_data = form.cleaned_data
                     # save all sections ONLY is all valid!
                     sections_to_save.append(SectionInfo(dyn_patient, False, registry_code, "cdes", dynamic_data))
-                    #dyn_patient.save_dynamic_data(registry_code, "cdes", dynamic_data)
-
                     current_data = dyn_patient.load_dynamic_data(self.registry.code, "cdes")
-                    #form_data = wrap_gridfs_data_for_form(registry_code, dynamic_data)
-                    #merge_gridfs_data_for_form(registry_code, form_data, current_data)
-
                     form_data = wrap_file_cdes(registry_code, dynamic_data, current_data, multisection=False)
-
-
                     form_section[s] = form_class(dynamic_data, initial=form_data)
                 else:
                     logger.debug("form is invalid")
@@ -460,17 +453,11 @@ class FormView(View):
                     for i in reversed(to_remove):
                         del dynamic_data[i]
 
-
-                    logger.debug("CLEANED DATA FOR MULTISECTION:\n%s" % dynamic_data)
-
                     section_dict = {s: dynamic_data}
-
                     sections_to_save.append(SectionInfo(dyn_patient, True, registry_code,
                                                         "cdes", section_dict, index_map))
+
                     current_data = dyn_patient.load_dynamic_data(self.registry.code, "cdes")
-                    #form_data = wrap_gridfs_data_for_form(registry_code, dynamic_data)
-                    #if current_data:
-                    #    merge_gridfs_data_for_form_multi(registry_code, form_data, current_data)
 
                     form_data = wrap_file_cdes(registry_code, dynamic_data, current_data, multisection=True)
 
