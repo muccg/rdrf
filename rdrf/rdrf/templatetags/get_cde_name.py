f7c9954036d236a921ba93e9110db74e18e2a504
Lee Render <id2359@yahoo.com>
2016-10-05 15:35:22 +0800

    mtm #26 get cde name for completion in template

---
+++ rdrf/rdrf/templatetags/get_cde_name.py
@@ -0,0 +1,13 @@
+from django import template
+from rdrf.models import CommonDataElement
+
+register = template.Library()
+
+
+@register.filter()
+def get_cde_name(cde_code):
+    try:
+        cde_model = CommonDataElement.objects.get(code=cde_code)
+        return cde_model.name
+    except CommonDataElement.DoesNotExist:
+        return "%s does not exist?" % cde_code
