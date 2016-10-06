5fc6e0a8d85e12e5feb71b2da6c46f3a8b5b1a9c
Lee Render <id2359@yahoo.com>
2016-10-06 16:33:25 +0800

    mtm #29 reference data module wasn't imported

--- rdrf/rdrf/initial_data/__init__.py
+++ rdrf/rdrf/initial_data/__init__.py
@@ -1,5 +1,5 @@
 from . import (base, groups, users, sample_registry, sample_laboratories,
-               genes, genes_smaller_sample, DEV, iprestrict_permissive)
+               genes, genes_smaller_sample, DEV, iprestrict_permissive,reference_data)
 
 datasets = [
     DEV,
