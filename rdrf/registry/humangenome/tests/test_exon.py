c12d34fc5b6ecb994079d5823b403850f893584e
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-10-07 13:16:30 +0100

    humangenome: Revert linting
    Nose test runner searches around and loads python modules. It failed to
    load this module.
    
    Errors were introduced into this code to hide lint messages.
    
    This humangenome module is most likely unused and could be removed along
    with the other unused code.

--- rdrf/registry/humangenome/tests/test_exon.py
+++ rdrf/registry/humangenome/tests/test_exon.py
@@ -3,17 +3,19 @@ from registry.humangenome.exon import *
 
 
 class TestExonVariation(unittest.TestCase):
-
     def test_fail_deletion(self):
         self.assertRaises(Variation.Malformed, lambda: ExonVariation("42delA"))
 
+
     def test_fail_duplication(self):
         self.assertRaises(Variation.Malformed, lambda: ExonVariation("42dupA"))
+
 
     def test_fail_position(self):
         self.assertRaises(Position.Malformed, lambda: Position("foo"))
         self.assertRaises(Position.Malformed, lambda: Position("42x"))
         self.assertRaises(Position.Malformed, lambda: Position("i"))
+
 
     def test_fail_range(self):
         self.assertRaises(Range.Malformed, lambda: Range("42"))
@@ -21,60 +23,54 @@ class TestExonVariation(unittest.TestCase):
         self.assertRaises(Position.Malformed, lambda: Range("_42"))
         self.assertRaises(Position.Malformed, lambda: Range("42i_i"))
 
+
     def test_fail_variation(self):
         # Generic variations that should fail to pass.
         self.assertRaises(Variation.Malformed, lambda: ExonVariation("42x"))
         self.assertRaises(Variation.Malformed, lambda: ExonVariation("42ins"))
         self.assertRaises(Variation.Malformed, lambda: ExonVariation("x"))
 
+
     def test_parse_deletion(self):
         ev = ExonVariation("42del")
         self.assertEqual(ev.gene, None, "Gene is set")
-        self.assertTrue(
-            isinstance(ev.variations[0], Deletion), "Variation object is the wrong type")
-        self.assertEqual(
-            ev.variations[0].location.position, 42, "Variation position is incorrect")
+        self.assertTrue(isinstance(ev.variations[0], Deletion), "Variation object is the wrong type")
+        self.assertEqual(ev.variations[0].location.position, 42, "Variation position is incorrect")
         self.assertEqual(str(ev), "42del", "Exon variation string is incorrect")
+
 
     def test_parse_duplication(self):
         ev = ExonVariation("42dup")
         self.assertEqual(ev.gene, None, "Gene is set")
-        self.assertTrue(
-            isinstance(ev.variations[0], Duplication), "Variation object is the wrong type")
-        self.assertEqual(
-            ev.variations[0].location.position, 42, "Variation position is incorrect")
+        self.assertTrue(isinstance(ev.variations[0], Duplication), "Variation object is the wrong type")
+        self.assertEqual(ev.variations[0].location.position, 42, "Variation position is incorrect")
         self.assertEqual(str(ev), "42dup", "Exon variation string is incorrect")
+
 
     def test_parse_nochange(self):
         ev = ExonVariation("42")
         self.assertEqual(ev.gene, None, "Gene is set")
-        self.assertTrue(
-            isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
-        self.assertEqual(
-            ev.variations[0].location.position, 42, "Variation position is incorrect")
+        self.assertTrue(isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
+        self.assertEqual(ev.variations[0].location.position, 42, "Variation position is incorrect")
         self.assertEqual(str(ev), "42", "Exon variation string is incorrect")
 
         ev = ExonVariation("42i_43")
         self.assertEqual(ev.gene, None, "Gene is set")
-        self.assertTrue(
-            isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
+        self.assertTrue(isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
         self.assertEqual(str(ev), "42i_43", "Exon variation string is incorrect")
 
         ev = ExonVariation("DMD:42")
         self.assertEqual(ev.gene.gene, "DMD", "Gene is not set")
-        self.assertTrue(
-            isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
-        self.assertEqual(
-            ev.variations[0].location.position, 42, "Variation position is incorrect")
+        self.assertTrue(isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
+        self.assertEqual(ev.variations[0].location.position, 42, "Variation position is incorrect")
         self.assertEqual(str(ev), "DMD:42", "Exon variation string is incorrect")
 
         ev = ExonVariation("exon42")
         self.assertEqual(ev.gene, None, "Gene is set")
-        self.assertTrue(
-            isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
-        self.assertEqual(
-            ev.variations[0].location.position, 42, "Variation position is incorrect")
+        self.assertTrue(isinstance(ev.variations[0], NoChange), "Variation object is the wrong type")
+        self.assertEqual(ev.variations[0].location.position, 42, "Variation position is incorrect")
         self.assertEqual(str(ev), "42", "Exon variation string is incorrect")
+
 
     def test_parse_position(self):
         position = Position("42")
@@ -112,6 +108,7 @@ class TestExonVariation(unittest.TestCase):
         self.assertTrue(position.uncertain, "Position is certain")
         self.assertEqual(str(position), "(42i)", "Position string is incorrect")
 
+
     def test_parse_range(self):
         range = Range("42_43")
         self.assertFalse(range.start.intron, "Position intron is true")
@@ -145,3 +142,4 @@ class TestExonVariation(unittest.TestCase):
         self.assertEqual(range.end.position, 43, "Position is not 43")
         self.assertEqual(range.end.uncertain, False, "Position is uncertain")
         self.assertEqual(str(range), "(42i)_43c", "Range string is incorrect")
+
