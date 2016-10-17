dd93004634e1b93be43d0152adb1d2dfd7388790
Tamas Szabo <szabtam@gmail.com>
2016-10-17 09:06:34 +0300

    reexport_test_zips fails with error
    Run the migrations from the re_export_zips script.
    
    Fixes #282

--- scripts/reexport_zip.sh
+++ scripts/reexport_zip.sh
@@ -2,9 +2,13 @@
 
 echo "Reexporting $1"
 
+# Making sure we migrated to the latest schema
+django-admin.py migrate
+
 COPY="$1.SAVED"
 echo "Saving a copy first to $COPY"
 cp "$1" "$COPY"
+
 
 echo "Force-importing $1"
 django-admin.py import "$1" --force
