a72e2a7404747f5e28706aa63f53b60f20cf0879
Lee Render <id2359@yahoo.com>
2016-10-18 17:00:13 +0800

    reexport_zip.sh was missing registry-code

--- scripts/reexport_zip.sh
+++ scripts/reexport_zip.sh
@@ -14,4 +14,4 @@ echo "Force-importing $1"
 django-admin.py import "$1" --force
 
 echo "Exporting it to $1"
-django-admin.py export registry --filename=$1
+django-admin.py export registry --filename=$1 --registry-code=$2
