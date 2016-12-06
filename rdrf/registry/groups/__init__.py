ecaadb910a0262df03e73f12c3bd3d4235cff2b3
aahunter <ahunter@ccg.murdoch.edu.au>
2016-12-06 13:53:18 +0800

    correct comment

--- rdrf/registry/groups/__init__.py
+++ rdrf/registry/groups/__init__.py
@@ -1,6 +1,6 @@
 # Provide a lookup for group names of the form rdrf.registry.groups.GROUPS.GENETIC_STAFF
 # The tuple values are used to match against group names in the db (explicitly case insensitive or using .lower())
-# GROUPS.ALL is a sentinel used to trigger membership to all groups (typically for superuser)
+# GROUPS.SUPER_USER is a sentinel used to trigger membership to all groups for superuser
 from collections import namedtuple
 GroupLookup = namedtuple('GroupLookup',
                          ['PATIENT',
