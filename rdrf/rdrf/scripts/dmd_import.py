9ffb34b646083d310b611eeece1830074b69c4ac
Lee Render <id2359@yahoo.com>
2016-12-08 10:43:26 +0800

    rdrf-ccg #859 we don't use State objects anymore

--- rdrf/rdrf/scripts/dmd_import.py
+++ rdrf/rdrf/scripts/dmd_import.py
@@ -13,7 +13,6 @@ from rdrf.generalised_field_expressions import MultiSectionItemsExpression
 
 from registry.patients.models import Patient
 from registry.patients.models import PatientAddress
-from registry.patients.models import State
 from registry.groups.models import WorkingGroup
 from rdrf.contexts_api import RDRFContextManager
 
@@ -832,18 +831,10 @@ class OldRegistryImporter(object):
         address_model.suburb = suburb
         address_model.postcode = postcode
         address_model.patient = self.patient_model
-        state_model = self.convert_state(state)
-        if state_model is not None:
-            address_model.state = self.convert_state(state)
+        address_model.state  = state
+        address_model.country = "AU"
         address_model.save()
 
-    def convert_state(self, state):
-        try:
-            return State.objects.get(short_name=state)
-        except State.DoesNotExist:
-            return None
-        
-        
 
     def convert_registry_patient(self, old_id):
         return self._id_map.get(old_id)
