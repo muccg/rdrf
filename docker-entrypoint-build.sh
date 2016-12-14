910975dce4c1a58e34367fb1cf87e41d6abba3cc
aahunter <ahunter@ccg.murdoch.edu.au>
2016-12-14 22:16:42 +0800

    correct build subdir name and improve logging for release tarball

--- docker-entrypoint-build.sh
+++ docker-entrypoint-build.sh
@@ -1,31 +1,62 @@
 #!/bin/bash
 
+info () {
+    printf "\r  [\033[00;34mINFO\033[0m] %s\n" "$1"
+}
+
 trap exit SIGHUP SIGINT SIGTERM
 env | grep -iv PASS | sort
 
 # prepare a tarball of build
 if [ "$1" = 'releasetarball' ]; then
-    echo "[Run] Preparing a release tarball"
+    info "[Run] Preparing a release tarball"
+    info "GIT_TAG ${GIT_TAG}"
+    info "PROJECT_SOURCE ${PROJECT_SOURCE}"
 
     set -e
-    cd /app
     rm -rf /app/*
-    echo $GIT_TAG
-    set -x
-    git clone --depth=1 --branch=${GIT_TAG} ${PROJECT_SOURCE} .
-    git ls-remote ${PROJECT_SOURCE} ${GIT_TAG} > .version
 
-    # Note: Environment vars are used to control the behaviour of pip (use local devpi for instance)
-    pip install -e rdrf
+    # clone and install the app
+    set -x
+    cd /app
+    git clone --depth=1 --branch="${GIT_TAG}" "${PROJECT_SOURCE}" .
+    git ls-remote "${PROJECT_SOURCE}" "${GIT_TAG}" > .version
+    cat .version
+    pip install -e "${PROJECT_NAME}"
     set +x
 
-    # create release tarball
-    DEPS="/env /app/uwsgi /app/docker-entrypoint.sh /app/rdrf /app/scripts"
-    cd /data
-    exec tar -cpzf ${PROJECT_NAME}-${GIT_TAG}.tar.gz ${DEPS}
+    # vars for creating release tarball
+    ARTIFACTS="/env
+               /app/docker-entrypoint.sh
+               /app/uwsgi
+               /app/scripts
+               /app/${PROJECT_NAME}"
+    TARBALL="/data/${PROJECT_NAME}-${GIT_TAG}.tar"
+    # shellcheck disable=SC2037
+    TAR_OPTS="--exclude-vcs
+              --verify
+              --checkpoint=1000
+              --checkpoint-action=dot
+              --create
+              --preserve-permissions"
+
+    info "ARTIFACTS ${ARTIFACTS}"
+    info "TARBALL ${TARBALL}"
+
+    # create tar from / so relative and absolute paths are identical
+    # allows archive verification to work
+    cd /
+    set -x
+    # shellcheck disable=SC2086
+    rm -f "${TARBALL}" && tar ${TAR_OPTS} -f "${TARBALL}" ${ARTIFACTS}
+    set +x
+    info "$(ls -lath "${TARBALL}")"
+    rm -f "${TARBALL}.gz" && gzip "${TARBALL}"
+    info "$(ls -lath "${TARBALL}.gz")"
+    exit 0
 fi
 
-echo "[RUN]: Builtin command not provided [releasetarball]"
-echo "[RUN]: $@"
+info "[RUN]: Builtin command not provided [releasetarball]"
+info "[RUN]: $*"
 
 exec "$@"
