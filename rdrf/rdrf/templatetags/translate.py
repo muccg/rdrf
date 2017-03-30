ac4ecd4b04c352c40fabce62585797a906932b83
lee <id2359@yahoo.com>
2017-03-30 11:46:16 +0800

    rdrf #399 Added template filter to perform a translation "inline"

---
+++ rdrf/rdrf/templatetags/translate.py
@@ -0,0 +1,9 @@
+from django import template
+from django.utils.translation import ugettext as _
+register = template.Library()
+
+@register.filter(name='translate')
+def translate(s):
+    return _(s)
+
+
