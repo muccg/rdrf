481599b57b4e154664a054ef75593ff8d58323a0
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2017-05-08 12:02:45 +0800

    bump 2.1.1

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -81,7 +81,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="2.1.0",
+      version="2.1.1",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
