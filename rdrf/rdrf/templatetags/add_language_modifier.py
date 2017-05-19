89c114af278225af09d5d7dacc65d3f80885090f
lee <id2359@yahoo.com>
2017-05-19 13:07:45 +0800

    mtm #107 handle language codes like de-ch

--- rdrf/rdrf/templatetags/add_language_modifier.py
+++ rdrf/rdrf/templatetags/add_language_modifier.py
@@ -10,6 +10,10 @@ def add_language_modifier(file_name, request):
     """
     allowed_languages = [pair[0].upper() for pair in settings.LANGUAGES]
     language = request.META.get("HTTP_ACCEPT_LANGUAGE", "EN").upper()
+    if "-" in language:
+        # cases like en-US, de-CH
+        language = language.split("-")[0]
+        
     if language != "EN":
         if language in allowed_languages:
             # docs/filename  -> docs/DE_filename
