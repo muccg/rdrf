8c380d647183729a63becc991b144453554fc277
lee <id2359@yahoo.com>
2017-07-05 15:44:05 +0800

    rdrf #466 Basic validator added

--- rdrf/rdrf/management/commands/check_structure.py
+++ rdrf/rdrf/management/commands/check_structure.py
@@ -4,7 +4,7 @@ from rdrf.models import Modjgo
 import yaml
 import jsonschema
 
-explanation = "to do"
+explanation = "This command checks for schema validation errors"
 
 class Command(BaseCommand):
     help = 'Checks in clinical db against json schema(s)'
@@ -20,30 +20,36 @@ class Command(BaseCommand):
         print(explanation)
 
     def handle(self, *args, **options):
-        self.schemas = self._load_schema_file()
+        self.schema = self._load_schema()
         collection = options.get("collection", "cdes")
 
         for modjgo_model in Modjgo.objects.filter(collection=collection):
             data = modjgo_model.data
-            problems = self._check_for_problems(data, collection)
-
-
-    def _load_schema_file(self):
+            problem = self._check_for_problem(collection, data)
+            if problem is not None:
+                django_model, django_id, message = problem
+                print("%s/%s : %s" % (django_model,
+                                      django_id,
+                                      message))
+                
+    def _load_schema(self):
         modjgo_schema_file = "/app/rdrf/rdrf/schemas/modjgo.yaml"
         with open(modjgo_schema_file) as sf:
             return yaml.load(sf)
 
-    def _get_schema(self, collection):
-        return self.schemas[collection]
+    def _get_key(self, data, key):
+        if data is None:
+            return None
+        if key in data:
+            return data[key]
 
-    def _check_for_problems(self, data, collection):
-        schema = self._get_schema(collection)
-        jsonschema.validate({collection: data}, schema)
-        
-
-        
-        
-
-        
-        
-        
+    def _check_for_problem(self, collection, data):
+        schema = self._load_schema()
+        try:
+            jsonschema.validate({collection: data}, schema)
+            return None
+        except Exception as verr:
+            message = verr.message
+            django_id = self._get_key(data, "django_id")
+            django_model = self._get_key(data, "django_model")
+            return django_model, django_id, message
