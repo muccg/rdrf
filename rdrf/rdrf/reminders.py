7f51f84a715b5499eaf417aabf605b471c1e7ec3
lee <id2359@yahoo.com>
2017-08-02 16:10:29 +0800

    mtm #129 rules for sending

--- rdrf/rdrf/reminders.py
+++ rdrf/rdrf/reminders.py
@@ -1,14 +1,34 @@
 from rdrf.email_notification import process_notification
+from datetime import datetime
+import logging
 
-class Reminder:
+logger = logger.getLogger(__name__)
+
+class ReminderProcessor:
     def __init__(self, user, registry_model):
         self.registry_model = registry_model
         self.user = user
 
     def _can_send(self):
-        pass
+        # These are the rules for MTM - should we push into config?
+        now = datetime.now()
+        existing_reminders = self._get_reminders()
+        num_sent = len(existing_reminders)
+        if num_sent >= 2:
+            return False
+        elif num_sent == 1:
+            last_reminder = existing_reminders[-1]
+            delta = now - last_reminder.date
+            return  delta.days >= 14
+        else:
+            return True
 
-    def send(self):
+    def _get_reminders(self):
+        # to do - look at email notification history
+        return []
+        
+
+    def process(self):
         if self._can_send():
             template_data = {"user": self.user,
                              "registry": self.registry_model}
