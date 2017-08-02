73aebedbee7e806307e3ae41e310f28bb907ed59
lee <id2359@yahoo.com>
2017-08-02 15:48:11 +0800

    mtm #129 made reminder sender a class

--- rdrf/rdrf/reminders.py
+++ rdrf/rdrf/reminders.py
@@ -1,10 +1,19 @@
 from rdrf.email_notification import process_notification
 
-def process_reminder(user, registry_model):
-    template_data = {"user": user,
-                     "registry": registry_model}
+class Reminder:
+    def __init__(self, user, registry_model):
+        self.registry_model = registry_model
+        self.user = user
+
+    def _can_send(self):
+        pass
+
+    def send(self):
+        if self._can_send():
+            template_data = {"user": self.user,
+                             "registry": self.registry_model}
     
-    process_notification(registry_model.code,
-                         "reminder",
-                         template_data)
+            process_notification(self.registry_model.code,
+                                 "reminder",
+                                 template_data)
     
