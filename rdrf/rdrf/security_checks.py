2c940da8e314bf6c1f96a48941f243159221fae2
lee <id2359@yahoo.com>
2017-08-25 16:20:22 +0800

    rdrf-ccg #944 corrected logic

--- rdrf/rdrf/security_checks.py
+++ rdrf/rdrf/security_checks.py
@@ -1,28 +1,21 @@
+from registry.patiets.models import Patient
 from registry.patients.models import ParentGuardian
 
 def check_patient_user(user, patient_model):
     if user.is_patient or user.is_parent or user.is_carrier:
-        # check self patient
-        if user.self_patient:
-            if user.self_patient.pk == patient_model.pk:
-                return True
 
-        # check parent guardian and own children
+        # check patients who have registred as users with this user
+        for user_patient in Patient.objects.filter(user=user):
+            if user_patient.pk == patient_model.pk:
+                return True
+        
+        # check parent guardian self patient and own children
         for parent in ParentGuardian.objects.filter(user=user):
             if patient_model in parent.children:
+                return True
+            if parent.self_patient and parent.self_patient.pk == patient_model.pk:
                 return True
 
         return False
     else:
         return True
-    
-
-    
-
-    
-    
-
-    
-
-        
-        
