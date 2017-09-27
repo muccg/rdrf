1b1aed88c7c743ec3167b974cd05c3374b385706
lee <id2359@yahoo.com>
2017-09-27 15:14:09 +0800

    Use Custom RunPython subclass to correctly run rdrf 0056

--- rdrf/rdrf/migrations/0056_auto_20170911_1039.py
+++ rdrf/rdrf/migrations/0056_auto_20170911_1039.py
@@ -4,6 +4,20 @@ from __future__ import unicode_literals
 from django.db import migrations
 from django.db.utils import ProgrammingError
 
+class ClinicalDBRunPython(migrations.RunPython):
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        # RunPython has access to all models. Ensure that all models are
+        # reloaded in case any are delayed.
+        from_state.clear_delayed_apps_cache()
+        #if router.allow_migrate(schema_editor.connection.alias, app_label, **self.hints):
+        if schema_editor.connection.alias == 'clinical':
+            # We now execute the Python code in a context that contains a 'models'
+            # object, representing the versioned models as an app registry.
+            # We could try to override the global cache, but then people will still
+            # use direct imports, so we go with a documentation approach instead.
+            self.code(from_state.apps, schema_editor)
+
+
 def extract_model_field_data_from_clinical_data_json_field(apps, schema_editor):
     # Our database router ignores hints (below)
     # from the Django source code this looks like dbname
@@ -38,5 +52,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(extract_model_field_data_from_clinical_data_json_field),
+        ClinicalDBRunPython(extract_model_field_data_from_clinical_data_json_field),
     ]
