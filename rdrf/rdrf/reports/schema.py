63077ab65d48dbac1de48c3a10cdd674e6523c25
lee <id2359@yahoo.com>
2017-10-27 12:47:02 +0800

    rdrf #540 planning

--- rdrf/rdrf/reports/schema.py
+++ rdrf/rdrf/reports/schema.py
@@ -1,5 +1,44 @@
 import sqlalchemy as alc
 from sqlalchemy import create_engine, MetaData
+from django.conf import settings
+from sqlalchemy import create_engine, MetaData
+
+
+# generate relational schema
+
+# but what schema?
+
+# most "natural" seems to be this:
+
+# for registries without form groups, context_id is redundant and confusing
+# so better to not extract at all - hence:
+
+# one demographics table
+# one table per form
+# multisections of forms have to be separate tables
+# one table for addresses
+# one table for consents
+# one table for relatives
+# one history table (?)
+
+# for registries with form groups/contexts:
+
+# same as above but embed the context id in each form and multisection table
+# as well as owning form group name
+
+
+
+
+def pg_uri(db):
+    "PostgreSQL connection URI for a django database settings dict"
+    user = db.get("USER")
+    password = db.get("PASSWORD")
+    name = db.get("NAME", "")
+    host = db.get("HOST")
+    port = db.get("PORT")
+    userpass = "".join([user, ":" + password if password else "", "@"])
+    return "".join(["postgresql://", userpass if user else "",
+                    host, ":" + port if port else "", "/", name])
 
 TYPE_MAP = {"float": alc.Float,
             "decimal": alc.Float,
@@ -42,9 +81,13 @@ def get_models(registry_model):
                     yield registry_model, form_model, section_model, cde_model
 
 class SchemaGenerator(object):
-    def __init__(self, registry_model, reporting_db_conn):
+    def __init__(self, registry_model):
         self.registry_model = registry_model
-        self.conn = reporting_db_conn
+        self.engine = self._create_engine()
+
+    def _create_engine(self):
+        # we should probably add a reporting db ...
+        return create_engine(pg_uri(settings.DATABASES["reporting"]))
 
     def clear(self):
         pass
@@ -91,20 +134,18 @@ class SchemaGenerator(object):
         last_user_column = alc.Column("user", alc.String, nullable=True)
         timestamp_column = alc.Column("timestamp", alc.Date, nullable=True)
         form_group_column = alc.Column("form_group", alc.String, nullable=True)
+
         columns.extend(patient_id_column,
                        context_column,
                        last_user_column,
                        timestamp_column,
                        form_group_column)
 
-        for col in cols:
-            column = self._create_column(col)
-            columns.append(column)
-
+        columns.extend([self._create_column(col) for col in cols])
+        
         table_name = table_code
         table = alc.Table(table_name, MetaData(self.engine), *columns, schema=None)
         return table
-
 
     def _create_column(self, col):
         return alc.Column(col.name, col.datatype, nullable=True)
