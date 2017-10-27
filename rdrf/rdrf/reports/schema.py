e683252c394d067875943899a7e87ee29af93417
lee <id2359@yahoo.com>
2017-10-27 11:37:47 +0800

    rdrf #540 fleshing out

--- rdrf/rdrf/reports/schema.py
+++ rdrf/rdrf/reports/schema.py
@@ -1,4 +1,22 @@
-# generate relational schema from registry definition
+import sqlalchemy as alc
+from sqlalchemy import create_engine, MetaData
+
+TYPE_MAP = {"float": alc.Float,
+            "decimal": alc.Float,
+            "calculated": alc.String,
+            "integer": alc.Integer,
+            "Integer": alc.Integer,
+            "int": alc.Integer,
+            "string": alc.String,
+            "date": alc.Date,
+            "range": alc.String,
+            "file": alc.String,
+}
+
+def get_column_type(cde_model):
+    datatype = cde_model.datatype.lower().strip()
+    return TYPE_MAP.get(datatype, alc.String)
+
 
 class Column(object):
     def __init__(self, registry_model, form_model, section_model, cde_model):
@@ -10,9 +28,14 @@ class Column(object):
 
     @property
     def datatype(self):
-        pass
+        return get_column_type(self.cde_model.datatype)
+
+    @property
+    def name(self):
+        return self.cde_model.code
 
 def get_models(registry_model):
+    # this won't do for form groups ...
     for form_model in registry_model.forms:
         for section_model in form_model.section_models:
                for cde_model in section_model.cde_models:
@@ -46,14 +69,42 @@ class SchemaGenerator(object):
         return []
 
     def _create_main_table(self, columns):
-        pass
+        self._create_table("patient", columns)
 
     def _create_other_tables(self, columns):
-        pass
+        table_map = {}
+        for col in columns:
+            table_code = col.form_model.name + "_" + col.section_model.code
+            if table_code in table_map:
+                table_map[table_code].append(col)
+            else:
+                table_map[table_code] = [col]
 
-        
+        for table_code, cols in table_map.items():
+            self._create_table(table_code,cols)
 
-        
+    def _create_table(self, table_code, cols):
+        # create some universal columns
+        columns = []
+        patient_id_column = alc.Column("patient_id", alc.Integer,nullable=False)
+        context_column = alc.Column("context_id", alc.Integer, nullable=False)
+        last_user_column = alc.Column("user", alc.String, nullable=True)
+        timestamp_column = alc.Column("timestamp", alc.Date, nullable=True)
+        form_group_column = alc.Column("form_group", alc.String, nullable=True)
+        columns.extend(patient_id_column,
+                       context_column,
+                       last_user_column,
+                       timestamp_column,
+                       form_group_column)
 
-    
-        
+        for col in cols:
+            column = self._create_column(col)
+            columns.append(column)
+
+        table_name = table_code
+        table = alc.Table(table_name, MetaData(self.engine), *columns, schema=None)
+        return table
+
+
+    def _create_column(self, col):
+        return alc.Column(col.name, col.datatype, nullable=True)
