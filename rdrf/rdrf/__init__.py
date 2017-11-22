267efaabadb8651bdeb504d0797292bf66de6076
lee <id2359@yahoo.com>
2017-11-22 11:24:02 +0800

    Bumped version to 3.0.5

--- rdrf/rdrf/__init__.py
+++ rdrf/rdrf/__init__.py
@@ -1,4 +1,4 @@
-VERSION = "3.0.4"
+VERSION = "3.0.5"
 __version__ = VERSION
 
 # Ensures db router system check is registered
--- rdrf/setup.py
+++ rdrf/setup.py
@@ -91,7 +91,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="3.0.4",
+      version="3.0.5",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
