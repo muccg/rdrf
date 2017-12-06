c14616947e4439cf8907656e174fb3c4d8e94029
lee <id2359@yahoo.com>
2017-12-06 11:44:25 +0800

    Bumped version to 3.1.0

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -91,7 +91,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="3.0.7",
+      version="3.1.0",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
