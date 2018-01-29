125c516376a2e634a74752c11d9062d75a3bf8fe
Sam Chang <schang@ccg.murdoch.edu.au>
2018-01-29 15:20:12 +0800

    update to use project specific stylesheet (fkrp #425)

--- rdrf/rdrf/templatetags/project_logo_padding.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.conf import settings
-
-register = template.Library()
-
-@register.simple_tag
-def project_logo_padding():
-    if hasattr(settings, "PROJECT_LOGO_PADDING"):
-        return "%s" % (settings.PROJECT_LOGO_PADDING)
---
+++ rdrf/rdrf/templatetags/project_stylesheet.py
@@ -0,0 +1,9 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.simple_tag
+def project_stylesheet():
+    if hasattr(settings, "PROJECT_STYLESHEET"):
+        return "%s" % (settings.PROJECT_STYLESHEET)
