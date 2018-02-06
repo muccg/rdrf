5843903bad9a24791979c7700783d03c26016d2c
Sam Chang <schang@ccg.murdoch.edu.au>
2018-02-06 16:05:01 +0800

    hide empty menu

---
+++ rdrf/rdrf/static/js/rdrf.js
@@ -0,0 +1,9 @@
+function hide_empty_menu() {
+    var menu_element_count = $(".dropdown-menu-button ul li").length;
+
+    if (menu_element_count == 0) {
+        $(".dropdown-menu-button").hide();
+    }
+
+    console.log(menu_element_count);
+}
