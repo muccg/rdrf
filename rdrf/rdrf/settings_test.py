4ebd23d9d7b7800c62ba60756c84fd87b903447b
lee <id2359@yahoo.com>
2018-02-23 15:07:18 +0800

    rdrf #582  behavioural tests

--- rdrf/rdrf/settings_test.py
+++ rdrf/rdrf/settings_test.py
@@ -11,7 +11,7 @@ TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
 
 # Used by Aloe tests, custom TestRunner
 # We don't want to run against the Test DB and we don't want a Transaction Test Case
-GHERKIN_TEST_RUNNER = 'rdrf.features.runner.GherkinNoDjangoTestDBTestRunner'
+GHERKIN_TEST_RUNNER = 'rdrf.testing.behaviour.features.runner.GherkinNoDjangoTestDBTestRunner'
 GHERKIN_TEST_CLASS = 'aloe.testclass.TestCase'
 
 MIGRATION_MODULES = {"iprestrict": None}
