77d79d26be703a8f986a6c23934309ec1e6c14fd
lee <id2359@yahoo.com>
2018-04-26 10:14:35 +0800

    reverting tasks change

--- rdrf/rdrf/apps.py
+++ rdrf/rdrf/apps.py
@@ -14,9 +14,3 @@ class RDRFConfig(AppConfig):
         # migration wasn't being found - importing here fixed that
         import rdrf.models.verification.models
 
-        # fkrp #431:
-        # Write to a sentinel to a known location
-        # so that the tasks.py ( running in a seperate process )
-        # can detect that the app is ready
-        with open("/tmp/webapp_initialised", "w") as f:
-            f.write("initialised")
--- rdrf/tasks.py
+++ rdrf/tasks.py
@@ -14,32 +14,17 @@ import django
 
 logger = logging.getLogger(__name__)
 
-
-def webapp_initialised():
-    # use a sentinel file
-    return os.path.exists("/tmp/webapp_initialised")
-
-
 @timer(300)
 def django_cron_heartbeat(num):
     """
     A heartbeat for django-cron (https://github.com/Tivix/django-cron)
     """
-    # fkrp #431 previous apps.apps_ready was working
-    # but I suspect this was due to pre-forking being used.
 
-    if not webapp_initialised():
+    if not apps.apps_ready:
         # Django is not initialised, the logger config in settings will not be active
         print("Deferring calling django-cron heartbeat until django app is ready")
         return
 
-    # safe to load
-    try:
-        django.setup()
-    except RuntimeError as r:
-        if str(r) == "populate() isn't reentrant":
-            return
-    
     # Do not perform this import until the django app is ready
     from django.core.management import call_command
     out_stream = io.StringIO("")
