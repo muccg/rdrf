d8bf8cb4a0727ec20db37cb34e8a035eb67d7174
lee <id2359@yahoo.com>
2018-05-01 11:49:18 +0800

    Bumped version to 4.1.7 to fix date of death on ver form

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -127,7 +127,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="4.1.6",
+      version="4.1.7",
       packages=find_packages(),
       description='RDRF',
       long_description='Rare Disease Registry Framework',
