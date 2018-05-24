e9ab03a7f58aa6b6407170651686e8b6baf77627
lee <id2359@yahoo.com>
2018-05-24 14:18:58 +0800

    rdrf #602 added test function

--- rdrf/rdrf/frontend/src/index.ts
+++ rdrf/rdrf/frontend/src/index.ts
@@ -1,14 +1,26 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as search from './search';
 
 
 declare global {
     interface Window {
-      otu_search_config: search.SearchConfig
+          dontcare: string;
+	
     }
 };
 
-// TODO revise if we should switch to multiple entry points instead.
-// ie. one entrypoint for each page
-export { init as init_tables } from './tables';
+
+
+export function testts(s: string) {
+       return "You typed " + s;
+}
+
+
+
+
+
+
+
+
+
+
