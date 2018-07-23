d7d6eddeb1cc2e1f9fb1fc3c59ff9f9b84757c28
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-07-23 12:17:12 +0800

    remove Travis branch from the list of production branches

--- travis-prodbuild.sh
+++ travis-prodbuild.sh
@@ -6,7 +6,7 @@ set -e
 # Production (deployable) build and tests
 #
 
-if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BRANCH_NAME" != x"travis" ]; then
+if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" ]; then
     echo "Branch $BRANCH_NAME is not deployable. Skipping prod build and tests"
     exit 0
 fi
