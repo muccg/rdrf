68117f577cc25706bdb3871590c1ae6f467224eb
lee <id2359@yahoo.com>
2018-09-11 16:05:28 +0800

    removed non-existanct checkout step

--- travis-prodbuild.sh
+++ travis-prodbuild.sh
@@ -11,7 +11,6 @@ if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" ]; then
     exit 0
 fi
 
-./develop.sh run-builder checkout
 ./develop.sh run build node
 ./develop.sh run-builder releasetarball
 sudo chown -R 1000 build
