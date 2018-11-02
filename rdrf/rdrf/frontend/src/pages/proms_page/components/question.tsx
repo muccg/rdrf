6b9dd2e34b1f8cfa38ab349363f13b61b3bcef4f
Mohammad Aminur Rashid <arashid@ccg.murdoch.edu.au>
2018-11-02 12:04:16 +0800

    rdrf #651 adding PROMS consent text at the end of survey

--- rdrf/rdrf/frontend/src/pages/proms_page/components/question.tsx
+++ rdrf/rdrf/frontend/src/pages/proms_page/components/question.tsx
@@ -8,62 +8,79 @@ import { QuestionInterface } from './interfaces';
 
 import * as actions from '../reducers';
 
-
-
 class Question extends React.Component<QuestionInterface, object> {
     constructor(props) {
-      super(props);
+        super(props);
     }
 
     handleChange(event) {
-	console.log("radio button clicked");
-	console.log(event);
-	let cdeValue  = event.target.value;
-	let cdeCode = event.target.name;
-	console.log("cde = " + cdeCode.toString());
-	console.log("value = " + cdeValue.toString());
-	this.props.enterData(cdeCode, cdeValue);
+        console.log("radio button clicked");
+        console.log(event);
+        let cdeValue = event.target.value;
+        let cdeCode = event.target.name;
+        console.log("cde = " + cdeCode.toString());
+        console.log("value = " + cdeValue.toString());
+        this.props.enterData(cdeCode, cdeValue);
+    }
+
+    handleConsent(event) {
+        let isConsentChecked = event.target.checked;
+        let cdeCode = event.target.name;
+        this.props.enterData(cdeCode, isConsentChecked);
     }
 
     render() {
-	let question = this.props.questions[this.props.stage];
-	return ( 
-		<Form>	 
-                   <FormGroup tag="fieldset">
-                <legend>{this.props.questions[this.props.stage].title}</legend>
-		<i>{this.props.questions[this.props.stage].instructions}</i>
-		   </FormGroup>
-
-		  {
-                      _.map(question.options, (option, index) => (
-		          <FormGroup check>
-		            <Label check>
-	                    <Input type="radio" name={this.props.questions[this.props.stage].cde} value={option.code}
-                                   onChange={this.handleChange.bind(this)}
-			           checked={option.code === this.props.answers[question.cde]}/>{option.text}
-		            </Label>
-                          </FormGroup>
-                      ))
-                  }
-
-		</Form>);
+        let question = this.props.questions[this.props.stage];
+        const isConsent = (this.props.questions.length - 1) == this.props.stage;
+        const consentText = "I consent to ongoing involvement in the CIC Cancer project" +
+            "and receiving a reminder for the next survey.";
+        return (
+            <Form>
+                <FormGroup tag="fieldset">
+                    <legend>{this.props.questions[this.props.stage].title}</legend>
+                    <i>{this.props.questions[this.props.stage].instructions}</i>
+                </FormGroup>
+                {
+                    (isConsent ?
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" name={this.props.questions[this.props.stage].cde}
+                                    onChange={this.handleConsent.bind(this)}
+                                    checked={this.props.answers[question.cde]} />
+                                {consentText}
+                            </Label>
+                        </FormGroup>
+                        :
+                        _.map(question.options, (option, index) => (
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="radio" name={this.props.questions[this.props.stage].cde} value={option.code}
+                                        onChange={this.handleChange.bind(this)}
+                                        checked={option.code === this.props.answers[question.cde]} />{option.text}
+                                </Label>
+                            </FormGroup>
+                        ))
+                    )
+                }
+            </Form>);
     }
 }
 
 function mapStateToProps(state) {
-    return {questions: state.questions,
-	    stage: state.stage,
-	    answers: state.answers,
-	   };
+    return {
+        questions: state.questions,
+        stage: state.stage,
+        answers: state.answers,
+    };
 }
 
 function mapPropsToDispatch(dispatch) {
     return ({
-	enterData: (cdeCode:string, cdeValue: any) => dispatch(actions.enterData({cde:cdeCode, value:cdeValue})),
+        enterData: (cdeCode: string, cdeValue: any) => dispatch(actions.enterData({ cde: cdeCode, value: cdeValue })),
     });
 }
 
-export default connect<{},{},QuestionInterface>(mapStateToProps, mapPropsToDispatch)(Question);
+export default connect<{}, {}, QuestionInterface>(mapStateToProps, mapPropsToDispatch)(Question);
 
-	
+
 
