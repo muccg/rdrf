2c2ba0e2a99471642432837f9ab37215c6eadae5
Mohammad Aminur Rashid <arashid@ccg.murdoch.edu.au>
2018-11-08 11:57:50 +0800

    rdrf #658 logo for clinical system

---
+++ rdrf/rdrf/templatetags/project_logo_link.py
@@ -0,0 +1,9 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.simple_tag
+def project_logo_link():
+    if settings.PROJECT_LOGO_LINK is not None:
+        return "%s" % (settings.PROJECT_LOGO_LINK)
