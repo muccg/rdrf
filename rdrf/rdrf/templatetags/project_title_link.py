9ec8f1e3c581f90308c93efb5d6b08a6cc843c86
Mohammad Aminur Rashid <arashid@ccg.murdoch.edu.au>
2019-02-19 17:01:25 +0800

    proms site project link

--- rdrf/rdrf/templatetags/project_title_link.py
+++ rdrf/rdrf/templatetags/project_title_link.py
@@ -5,9 +5,13 @@ from rdrf.system_role import SystemRoles
 
 register = template.Library()
 
+_HOME_PAGE = "admin:index"
+
 
 @register.simple_tag
 def project_title_link():
-    if settings.SYSTEM_ROLE is not SystemRoles.CIC_PROMS:
+    if settings.SYSTEM_ROLE is SystemRoles.CIC_PROMS:
+        return reverse(_HOME_PAGE)
+    else:
         url_name = settings.PROJECT_TITLE_LINK
         return reverse(url_name)
