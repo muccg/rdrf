ff434fe64cb2dc5fcab8ecdcec2870475bda92d3
Mohammad Aminur Rashid <arashid@ccg.murdoch.edu.au>
2019-02-19 14:12:57 +0800

    rdrf #780 copyright text is visible for CIC_ roles

--- rdrf/rdrf/context_processors/context_processors.py
+++ rdrf/rdrf/context_processors/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from rdrf.system_role import SystemRoles
 
 
 def production(request):
@@ -8,4 +9,13 @@ def production(request):
 def common_settings(request):
     return {
         'ACCOUNT_SELF_UNLOCK_ENABLED': settings.ACCOUNT_SELF_UNLOCK_ENABLED,
+    }
+
+
+def cic_system_role(request):
+    return {
+        'cic_system_role': settings.SYSTEM_ROLE in (SystemRoles.CIC_CLINICAL,
+                                                    SystemRoles.CIC_DEV,
+                                                    SystemRoles.CIC_PROMS
+                                                    ),
     }
