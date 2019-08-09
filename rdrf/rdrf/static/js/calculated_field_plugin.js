448ffd4c4f4b310d668c0824524bdba44c9dc178
Jerome Mouneyrac <jerome@mouneyrac.com>
2019-08-09 11:01:37 +0800

    Compile the calculated field JS file with babel repl + add the word var in front of calculated_cde_inputs_json_values declaration

--- rdrf/rdrf/static/js/calculated_field_plugin.js
+++ rdrf/rdrf/static/js/calculated_field_plugin.js
@@ -1,85 +1,110 @@
-(function ($) {
+"use strict";
 
-        let required_cde_inputs = {};
-        let calculated_cde_inputs = {}
-        let patient_date_of_birth = '';
-        let patient_sex = '';
+function _toConsumableArray(arr) {
+  return (
+    _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread()
+  );
+}
 
-        const update_function = function (calculated_cdes) {
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance");
+}
 
-            calculated_cdes.forEach(cde_code => {
-                // Retrieve all values of input
-                calculated_cde_inputs_json_values = {};
-                calculated_cde_inputs[cde_code].forEach((required_input_cde) => {
+function _iterableToArray(iter) {
+  if (
+    Symbol.iterator in Object(iter) ||
+    Object.prototype.toString.call(iter) === "[object Arguments]"
+  )
+    return Array.from(iter);
+}
 
-                    let cde_value = $(`[id$=__${required_input_cde}]`).val();
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  }
+}
 
-                    // check if it is a date like dd-mm-yyyy and convert it in yyyy-mm-dd
-                    if (moment(cde_value, "D-M-YYYY",true).isValid()) {
-                        cde_value = moment(cde_value, "D-M-YYYY",true).format('YYYY-MM-DD');
-                    }
+(function($) {
+  var required_cde_inputs = {};
+  var calculated_cde_inputs = {};
+  var patient_date_of_birth = "";
+  var patient_sex = "";
 
-                    // check if it is a number and convert it in a number
-                    if ($(`[id$=__${required_input_cde}]`).attr('type') === 'number') {
-                        cde_value = parseFloat(cde_value);
-                    }
+  var update_function = function update_function(calculated_cdes) {
+    calculated_cdes.forEach(function(cde_code) {
+      // Retrieve all values of input
+      var calculated_cde_inputs_json_values = {};
+      calculated_cde_inputs[cde_code].forEach(function(required_input_cde) {
+        var cde_value = $("[id$=__".concat(required_input_cde, "]")).val(); // check if it is a date like dd-mm-yyyy and convert it in yyyy-mm-dd
 
+        if (moment(cde_value, "D-M-YYYY", true).isValid()) {
+          cde_value = moment(cde_value, "D-M-YYYY", true).format("YYYY-MM-DD");
+        } // check if it is a number and convert it in a number
 
-                    calculated_cde_inputs_json_values[required_input_cde] = cde_value;
-                });
-
-                const body = {
-                    'cde_code': cde_code,
-                    'patient_date_of_birth': patient_date_of_birth,
-                    'patient_sex': patient_sex,
-                    'form_values': calculated_cde_inputs_json_values
-                };
-
-                fetch(`/api/v1/calculatedcdes/`, {
-                    method: 'post',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRFToken': $("[name=csrfmiddlewaretoken]").val()
-                    },
-                    body: JSON.stringify(body)
-                })
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (myJson) {
-                        $(`[id$=__${cde_code}]`).val(myJson)
-                        $(`[id$=__${cde_code}]`).trigger("change");
-                    });
-            });
+        if (
+          $("[id$=__".concat(required_input_cde, "]")).attr("type") === "number"
+        ) {
+          cde_value = parseFloat(cde_value);
         }
 
-        $.fn.add_calculation = function (options) {
+        calculated_cde_inputs_json_values[required_input_cde] = cde_value;
+      });
+      var body = {
+        cde_code: cde_code,
+        patient_date_of_birth: patient_date_of_birth,
+        patient_sex: patient_sex,
+        form_values: calculated_cde_inputs_json_values
+      };
+      fetch("/api/v1/calculatedcdes/", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": $("[name=csrfmiddlewaretoken]").val()
+        },
+        body: JSON.stringify(body)
+      })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          $("[id$=__".concat(cde_code, "]")).val(myJson);
+          $("[id$=__".concat(cde_code, "]")).trigger("change");
+        });
+    });
+  };
 
-            patient_date_of_birth = options.patient_date_of_birth;
-            patient_sex = options.patient_sex;
+  $.fn.add_calculation = function(options) {
+    patient_date_of_birth = options.patient_date_of_birth;
+    patient_sex = options.patient_sex;
+    calculated_cde_inputs[options.observer] = options.cde_inputs;
+    options.cde_inputs.forEach(function(input_cde_code) {
+      required_cde_inputs[input_cde_code] =
+        required_cde_inputs[input_cde_code] != undefined
+          ? [].concat(_toConsumableArray(required_cde_inputs[input_cde_code]), [
+              options.observer
+            ])
+          : [options.observer];
+    });
 
-            calculated_cde_inputs[options.observer] = options.cde_inputs;
+    try {
+      // call on initial page load
+      update_function([options.observer]); //call it to ensure if calculation changes on server
+      //update the onchange
 
-            options.cde_inputs.forEach((input_cde_code) => {
-                required_cde_inputs[input_cde_code] = required_cde_inputs[input_cde_code] != undefined ?
-                    [...required_cde_inputs[input_cde_code], options.observer] : [options.observer];
-            });
-
-            try {
-                // call on initial page load
-                update_function([options.observer]); //call it to ensure if calculation changes on server
-
-                //update the onchange
-                Object.keys(required_cde_inputs).forEach(function (cde_input) {
-                    $(`[id$=__${cde_input}]`).off("change");
-                    $(`[id$=__${cde_input}]`).on('change keyup', _.debounce((e) => {update_function(required_cde_inputs[cde_input])}, 250))
-                });
-
-            } catch (err) {
-                alert(err);
-            }
-        };
+      Object.keys(required_cde_inputs).forEach(function(cde_input) {
+        $("[id$=__".concat(cde_input, "]")).off("change");
+        $("[id$=__".concat(cde_input, "]")).on(
+          "change keyup",
+          _.debounce(function(e) {
+            update_function(required_cde_inputs[cde_input]);
+          }, 250)
+        );
+      });
+    } catch (err) {
+      alert(err);
     }
-    (jQuery)
-)
-;
+  };
+})(jQuery);
---
+++ rdrf/rdrf/static/js/calculated_field_plugin_es6.js
@@ -0,0 +1,85 @@
+(function ($) {
+
+        let required_cde_inputs = {};
+        let calculated_cde_inputs = {}
+        let patient_date_of_birth = '';
+        let patient_sex = '';
+
+        const update_function = function (calculated_cdes) {
+
+            calculated_cdes.forEach(cde_code => {
+                // Retrieve all values of input
+                calculated_cde_inputs_json_values = {};
+                calculated_cde_inputs[cde_code].forEach((required_input_cde) => {
+
+                    let cde_value = $(`[id$=__${required_input_cde}]`).val();
+
+                    // check if it is a date like dd-mm-yyyy and convert it in yyyy-mm-dd
+                    if (moment(cde_value, "D-M-YYYY",true).isValid()) {
+                        cde_value = moment(cde_value, "D-M-YYYY",true).format('YYYY-MM-DD');
+                    }
+
+                    // check if it is a number and convert it in a number
+                    if ($(`[id$=__${required_input_cde}]`).attr('type') === 'number') {
+                        cde_value = parseFloat(cde_value);
+                    }
+
+
+                    calculated_cde_inputs_json_values[required_input_cde] = cde_value;
+                });
+
+                const body = {
+                    'cde_code': cde_code,
+                    'patient_date_of_birth': patient_date_of_birth,
+                    'patient_sex': patient_sex,
+                    'form_values': calculated_cde_inputs_json_values
+                };
+
+                fetch(`/api/v1/calculatedcdes/`, {
+                    method: 'post',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRFToken': $("[name=csrfmiddlewaretoken]").val()
+                    },
+                    body: JSON.stringify(body)
+                })
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (myJson) {
+                        $(`[id$=__${cde_code}]`).val(myJson)
+                        $(`[id$=__${cde_code}]`).trigger("change");
+                    });
+            });
+        }
+
+        $.fn.add_calculation = function (options) {
+
+            patient_date_of_birth = options.patient_date_of_birth;
+            patient_sex = options.patient_sex;
+
+            calculated_cde_inputs[options.observer] = options.cde_inputs;
+
+            options.cde_inputs.forEach((input_cde_code) => {
+                required_cde_inputs[input_cde_code] = required_cde_inputs[input_cde_code] != undefined ?
+                    [...required_cde_inputs[input_cde_code], options.observer] : [options.observer];
+            });
+
+            try {
+                // call on initial page load
+                update_function([options.observer]); //call it to ensure if calculation changes on server
+
+                //update the onchange
+                Object.keys(required_cde_inputs).forEach(function (cde_input) {
+                    $(`[id$=__${cde_input}]`).off("change");
+                    $(`[id$=__${cde_input}]`).on('change keyup', _.debounce((e) => {update_function(required_cde_inputs[cde_input])}, 250))
+                });
+
+            } catch (err) {
+                alert(err);
+            }
+        };
+    }
+    (jQuery)
+)
+;
