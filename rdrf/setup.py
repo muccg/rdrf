8957b60a1a9df8b568e58553b5e183232ba87d49
Jerome Mouneyrac <jerome@mouneyrac.com>
2019-10-10 13:12:51 +0800

    #993 ldap: force is_active when user login from ldap

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -133,7 +133,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="6.0.11",
+      version="6.0.12",
       packages=find_packages(),
       description='RDRF',
       long_description='Rare Disease Registry Framework',
