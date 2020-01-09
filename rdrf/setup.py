ba329d770322c269f1eed07f6d1437b398bf529a
Jithendralal Nair <jithendralal@gmail.com>
2020-01-09 15:52:30 +0800

    rdrf #1159 Upgraded django-simple-history 2.7.2 -> 2.8.0

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -49,7 +49,7 @@ requirements = [
     "ua-parser==0.8.0",
     "user-agents==2.0",
     "django-user-agents==0.3.2",
-    "django-simple-history==2.7.2",
+    "django-simple-history==2.8.0",
     "django-formtools",
     "python-ldap",
     "django-auth-ldap",
