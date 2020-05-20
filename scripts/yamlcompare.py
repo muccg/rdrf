aae9cb28e056397af94c6eaf00d5a92ad6e6be3a
Jithendralal Nair <jithendralal@gmail.com>
2020-05-20 11:17:43 +0800

    rdrf #1321 yaml compare script modified - cde column shown as section/form/cde, header row added

--- scripts/yamlcompare.py
+++ scripts/yamlcompare.py
@@ -14,15 +14,15 @@ def get_dict_of_items(yaml_data, list_to_compare, identifier):
     return {item[identifier]: [[attr, str(item[attr])] for attr in item] for item in yaml_data[list_to_compare]}
 
 
-def get_question_string(question, cde_dict):
-    cde_code = question[0][1]
-    cde = cde_dict[cde_code]
-    for attr in cde:
-        if attr[0] == "name":
-            return attr[1]
+def get_cde_usage(code, forms):
+    for form in forms:
+        sections = form["sections"]
+        for section in sections:
+            if code in section["elements"]:
+                return "%s/%s/%s" % (form["name"], section["code"], code)
 
 
-def compare_dicts(category, dict1, dict2, result_file, cde_dict=None, survey_name=None):
+def compare_dicts(category, dict1, dict2, result_file, cde_dict=None, survey_name=None, forms=None):
     for key in sorted(list(set(list(dict1.keys()) + list(dict2.keys())))):
         if key not in dict1:
             result_file.write("%s,%s,%s,%s\n" % (category, key, "Not found", " "))
@@ -32,11 +32,12 @@ def compare_dicts(category, dict1, dict2, result_file, cde_dict=None, survey_nam
             for i in range(len(dict1[key]) or len(dict2[key])):
                 if dict1[key][i] != dict2[key][i]:
                     if category == "Survey Question":
-                        question = survey_name + " > " + key + " (" + get_question_string(dict1[key], cde_dict) + ")"
+                        question = survey_name + "/" + key
                         result_file.write("%s,%s,%s,%s,%s\n" % (category, question, dict1[key][i][0],
                                                                 dict1[key][i][1], dict2[key][i][1]))
                     else:
-                        result_file.write("%s,%s,%s,%s,%s\n" % (category, key,
+                        cde_string = get_cde_usage(key, forms)
+                        result_file.write("%s,%s,%s,%s,%s\n" % (category, cde_string,
                                                                 dict1[key][i][0], dict1[key][i][1].replace("\n", " "),
                                                                 dict2[key][i][1].replace("\n", " ")))
 
@@ -57,10 +58,15 @@ with open(yaml_file_1) as yf1, open(yaml_file_2) as yf2, open(out_file, 'w+') as
     data1 = yaml.load(yf1, yaml.SafeLoader)
     data2 = yaml.load(yf2, yaml.SafeLoader)
 
+    forms = data1["forms"]
+    result_file.write("Category,Item,Attribute,ValueA,ValueB\n")
+
+    # compare CDEs
     cde_dict1 = get_dict_of_items(data1, "cdes", "code")
     cde_dict2 = get_dict_of_items(data2, "cdes", "code")
-    compare_dicts("CDE", cde_dict1, cde_dict2, result_file)
+    compare_dicts("CDE", cde_dict1, cde_dict2, result_file, forms=forms)
 
+    # compare Survey Questions
     surveys1 = data1["surveys"]
     surveys2 = data2["surveys"]
 
