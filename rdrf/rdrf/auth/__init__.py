d7f1ca327be177cbd141cb4507bab2d8e62db7bc
Jithendralal Nair <jithendralal@gmail.com>
2020-05-27 15:42:57 +0800

    rdrf #1327 unwanted code removed from can_user_self_unlock

--- rdrf/rdrf/auth/__init__.py
+++ rdrf/rdrf/auth/__init__.py
@@ -8,4 +8,4 @@ def is_user_privileged(user):
 def can_user_self_unlock(user):
     return (
         getattr(settings, 'ACCOUNT_SELF_UNLOCK_ENABLED', False) and not
-        is_user_privileged(user) and not user.is_curator and not user.prevent_self_unlock)
+        is_user_privileged(user) and not user.prevent_self_unlock)
