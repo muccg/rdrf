1042edba67aa15b3e569da0f58cad56cae3dc8f3
Jithendralal Nair <jithendralal@gmail.com>
2020-07-09 16:03:19 +0800

    rdrf #1422 logging added - registry saved in both cases; override or not

--- rdrf/rdrf/management/commands/setup_proms.py
+++ rdrf/rdrf/management/commands/setup_proms.py
@@ -53,8 +53,18 @@ class Command(BaseCommand):
                 for klass in klasses:
                     klass.objects.all().delete()
 
-                importer.create_registry()
+                try:
+                    importer.create_registry()
+                except Exception as e:
+                    self.stderr.write("Exception %s" % e)
+                    raise e
+                else:
+                    self.stdout.write("No exception was caught in the importer")
+
+                self.stdout.write("Importer state: %s" % importer.state)
+                self.stdout.write("Importer errors: %s" % importer.errors)
 
                 if override_metadata == 'N' and current_metadata is not None:
                     registry.metadata_json = current_metadata
-                    registry.save()
+
+                registry.save()
