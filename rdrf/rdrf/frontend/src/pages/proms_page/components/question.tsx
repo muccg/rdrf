7ef47d7e08acffdc35a53ce0b722f1f91ad5f51a
Jithendralal Nair <jithendralal@gmail.com>
2020-07-14 15:40:31 +0800

    rdrf #1435 linting

--- rdrf/rdrf/frontend/src/pages/proms_page/components/question.tsx
+++ rdrf/rdrf/frontend/src/pages/proms_page/components/question.tsx
@@ -210,7 +210,7 @@ class Question extends React.Component<QuestionInterface, object> {
     }
 
     public renderMultiselect(question: any) {
-    	let defaultValue = "";
+        let defaultValue = "";
         if (this.props.answers[question.cde] !== undefined) {
             defaultValue = this.props.answers[question.cde].toString().replace("[","").replace("]","");
         }
@@ -228,7 +228,7 @@ class Question extends React.Component<QuestionInterface, object> {
                     <Col sm="12" md={{ size: 6, offset: 3 }}>
                         <Input type="select"
                             name={question.cde}
-	                        value={defaultValue}
+                            value={defaultValue}
                             onChange={this.handleMultiChange} multiple={true} >
                             {_.map(question.spec.options, (option, index) => (
                                 <option key={option.code} value={option.code}>
