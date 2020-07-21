c7d19fd7e46d2c3eeb862095c6bc8126012d83f4
lee <id2359@yahoo.com>
2020-07-21 22:08:55 +0800

    rdrf #1437 linted improved names

--- rdrf/rdrf/services/io/actions/deidentified_data_extract.py
+++ rdrf/rdrf/services/io/actions/deidentified_data_extract.py
@@ -1,8 +1,14 @@
 from datetime import datetime
+from datetime import datetime
+import json
+import zipfile
+from zipfile import ZipFile
+import uuid
 from django.db import connections
-from rdrf.helpers.utils import generate_deidentified_id
-import logging
+from django.http import HttpResponse
+from django.conf import settings
 
+import logging
 logger = logging.getLogger(__name__)
 
 
@@ -23,28 +29,19 @@ class PipeLine:
         self.conn_demo = connections['default']
         self.data = []
         self.id_map = self._construct_deident_map()
-        logger.debug("id_map = %s" % self.id_map)
 
     def _raw_sql(self, conn, sql):
-        logger.debug("running %s" % sql)
-        a = datetime.now()
         with conn.cursor() as c:
             c.execute(sql)
             rows = c.fetchall()
-            logger.debug("number of rows returned = %s" % len(rows))
-        b = datetime.now()
-        logger.debug("time to run = %s seconds" % (b-a).seconds)
         return rows
 
     def _construct_deident_map(self):
         return {row[0]: row[1] for row in self._raw_sql(self.conn_demo, SQL.id_query)}
 
     def deidentify(self):
-        a = datetime.now()
         self.data = [self._deidentify_row(row) for row in
                      self._raw_sql(self.conn_clin, SQL.clinical_data_query)]
-        b = datetime.now()
-        logger.debug("time to deidentify = %s seconds" % (b-a).seconds)
 
     def _deidentify_row(self, row):
         d = {}
@@ -65,13 +62,6 @@ def extract_data(custom_action):
 
 
 def execute(custom_action, user):
-    from django.http import HttpResponse, FileResponse
-    from datetime import datetime
-    from django.conf import settings
-    import json
-    import zipfile
-    from zipfile import ZipFile
-    import uuid
     a = datetime.now()
     timestamp = datetime.timestamp(a)
     guid = str(uuid.uuid1())
@@ -82,13 +72,13 @@ def execute(custom_action, user):
                         "version": PipeLine.VERSION,
                         "guid": guid}
     data["data"] = results
-
     json_data = json.dumps(data)
     response = HttpResponse(content_type="application/zip")
     zf = ZipFile(response, 'w', zipfile.ZIP_DEFLATED)
-    zf.writestr("export.json", json_data)
-    response['Content-Disposition'] = f'attachment; filename=export.zip'
-    b = datetime.now()
-    logger.debug("time to do everything = %s" % (b-a).seconds)
+    name = settings.DEIDENTIFIED_SITE_ID + "_" + a.strftime("%Y%m%d%H%M%S")
+    zip_name = name + ".zip"
+    json_name = name + ".json"
+    zf.writestr(json_name, json_data)
+    response['Content-Disposition'] = f'attachment; filename=%s' % zip_name
 
     return response
