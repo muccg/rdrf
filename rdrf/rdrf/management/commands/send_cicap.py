d3b767bd775e5602a2db66643518a7b0e4286148
lee <id2359@yahoo.com>
2020-09-09 16:31:31 +0800

    rdrf #1555 Added ftp send

--- rdrf/rdrf/management/commands/send_cicap.py
+++ rdrf/rdrf/management/commands/send_cicap.py
@@ -1,9 +1,9 @@
 import sys
+import ftplib as ftp
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from rdrf.models.definition.models import Registry
 from rdrf.services.io.actions import deidentified_data_extract as dde
-
 
 class Command(BaseCommand):
     help = 'Send deidentified data to CICAP'
@@ -15,31 +15,25 @@ class Command(BaseCommand):
                             default=None,
                             help='registry code')
 
-    def handle(self, *args, **options):
-        if hasattr(settings, "CICAP_ADDRESS"):
-            cicap_address = settings.CICAP_ADDRESS
+    def get_setting(self, key):
+        if not hasattr(settings, key):
+            self.stderr.write(f"Error: {key} not in settings")
+            sys.exit(1)
         else:
-            cicapp_address = None
-
-        if not cicapp_address:
-            self.stderr("CICAP_ADDRESS is not configured")
-            sys.exit(1)
-            
-        registry_code = options.registry_code
-
-        if registry_code is None:
-            self.stderr.write(f"Error: --registry-code argument is required")
-            sys.exit(1)
-
-        try:
-            registry_model = Registry.objects.get(code=registry_code)
-        except Registry.DoesNotExist:
-            self.stderr.write(f"{registry_code} does not exist")
-            sys.exit(1)
-
+            return getattr(settings, key)
         
 
+    def handle(self, *args, **options):
+        cicap_address = self.get_setting("CICAP_ADDRESS")
+        cicap_user = self.get_setting("CICAP_USER")
+        cicap_password = self.get_setting("CICAPP_PASSWORD")
 
+        custom_action = None
+        user = None
         
-
-
+        remote_filename, bytes_io = dde.execute(custom_action, user, create_bytes_io=True)
+        ftp_command = "STOR %s" % remote_filename
+        ftp_conn = ftp.FTP(cicap_address, cicap_user, cicap_password)
+        ftp_conn.storbinary(ftp_command, bytes_io)
+        ftp_conn.close()
+        bytes_io.close()
