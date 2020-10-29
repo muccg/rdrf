24d37d0dcece6fc0b2ef10c43901cdbdbec672a7
Alfie <amalone@ccg.murdoch.edu.au>
2020-10-29 10:40:53 +0800

    rdrf #1605 Replaced missed instances of 'base_name' with 'basename' for router.register()

--- rdrf/rdrf/services/rest/urls/api_urls.py
+++ rdrf/rdrf/services/rest/urls/api_urls.py
@@ -9,17 +9,17 @@ router.register(r'users', api_views.CustomUserViewSet)
 router.register(r'doctors', api_views.DoctorViewSet)
 router.register(r'nextofkinrelationship', api_views.NextOfKinRelationshipViewSet)
 router.register(r'workinggroups', api_views.WorkingGroupViewSet)
-router.register(r'countries', api_views.ListCountries, base_name='country')
-router.register(r'genes', api_views.LookupGenes, base_name='gene')
-router.register(r'laboratories', api_views.LookupLaboratories, base_name='laboratory')
+router.register(r'countries', api_views.ListCountries, basename='country')
+router.register(r'genes', api_views.LookupGenes, basename='gene')
+router.register(r'laboratories', api_views.LookupLaboratories, basename='laboratory')
 router.register(r'registries/(?P<registry_code>\w+)/indices',
-                api_views.LookupIndex, base_name='index')
+                api_views.LookupIndex, basename='index')
 router.register(r'registries/(?P<registry_code>\w+)/clinicians',
-                api_views.ListClinicians, base_name='clinician')
-router.register(r'calculatedcdes', api_views.CalculatedCdeValue, base_name='calculatedcde')
-router.register(r'tasks/(?P<task_id>[0-9a-z_\-]+)', api_views.TaskInfoView, base_name='task')
+                api_views.ListClinicians, basename='clinician')
+router.register(r'calculatedcdes', api_views.CalculatedCdeValue, basename='calculatedcde')
+router.register(r'tasks/(?P<task_id>[0-9a-z_\-]+)', api_views.TaskInfoView, basename='task')
 router.register(r'taskdownloads/(?P<task_id>[0-9a-z_\-]+)',
-                api_views.TaskResultDownloadView, base_name='download')
+                api_views.TaskResultDownloadView, basename='download')
 
 urlpatterns = [
     re_path(r'registries/(?P<code>\w+)/$', api_views.RegistryDetail.as_view(), name='registry-detail'),
