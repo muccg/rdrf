eb55f383d275127aaf5edc7c9d2556fe81b2f421
Alfie <amalone@ccg.murdoch.edu.au>
2021-03-04 10:10:18 +0800

    rdrf #1683 Added security checks to CalculatedCdeValue API view, added unit tests for new helper funcs implemented

--- rdrf/rdrf/helpers/utils.py
+++ rdrf/rdrf/helpers/utils.py
@@ -990,3 +990,25 @@ def supports_deidentification_workflow():
     if settings.DEIDENTIFIED_SITE_ID:
         return True
     return False
+
+
+def same_working_group(patient, user):
+    user_work_groups = set([wg.id for wg in user.working_groups.all()])
+    patient_work_groups = set([wg.id for wg in patient.working_groups.all()])
+    if not user.is_superuser and not user_work_groups.intersection(patient_work_groups):
+        return False
+    else:
+        return True
+
+
+def is_calculated_in_reg(cde, registry):
+    calc_cdes = []
+    for form_model in registry.forms:
+        for sec_model in form_model.section_models:
+            for cde_model in sec_model.cde_models:
+                if cde_model.datatype == "calculated":
+                    calc_cdes.append(cde_model)
+    if cde not in calc_cdes:
+        return False
+    else:
+        return True
