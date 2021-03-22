63b6c653151d91d0e4cfa88c74f276a95d7e9ef1
Alfie <amalone@ccg.murdoch.edu.au>
2021-03-22 16:41:31 +0800

    rdrf #1718 Added setupTests.js to initialise window.proms_config, added extra tests

--- rdrf/rdrf/frontend/src/pages/proms_page/components/question.unit.test.tsx
+++ rdrf/rdrf/frontend/src/pages/proms_page/components/question.unit.test.tsx
@@ -6,14 +6,18 @@ import Question from './question';
 import * as Logic from '../logic';
 
 //var elList = Logic.evalElements([{"cde": "doy"}], {});
-var newQuestion = <Question /> //title="test" />; //questions={elList}/>;
+//var newQuestion = <Question /> //title="test" />; //questions={elList}/>;
 
-describe('Question', () => {
+describe('An empty Question', () => {
+    let newQuestion = <Question />
     it('is defined', () => {
         expect(newQuestion).toBeDefined();
     });
     it('has properties', () => {
-        expect(newQuestion).toHaveProperty('props')
+        expect(newQuestion).toHaveProperty('props');
+    });
+    it('does not have questions', () => {
+        expect(newQuestion.props).not.toHaveProperty('questions');
     });
     // Etc. etc.
     // Check Jest API for more methods
---
+++ rdrf/rdrf/frontend/src/setupTests.js
@@ -0,0 +1,9 @@
+window.proms_config = {
+    patient_token: "",
+    questions: [{}],
+    survey_endpoint: "",
+    survey_name: "",
+    registry_code: "",
+    csrf_token: "",
+    completed_page: ""
+};
