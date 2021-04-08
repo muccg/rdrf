e2535863cf3ee6e7758022819c40119482fc537f
lee <id2359@yahoo.com>
2021-04-08 16:40:05 +0800

    bumped version to 6.3.13

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -139,7 +139,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="6.3.12",
+      version="6.3.13",
       packages=find_packages(),
       description='RDRF',
       long_description='Rare Disease Registry Framework',
