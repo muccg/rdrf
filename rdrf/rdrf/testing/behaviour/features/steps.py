d08c3c81a1dd9fe71665a8df0c79acfd4778c8cb
Alfie <amalone@ccg.murdoch.edu.au>
2021-06-14 14:38:51 +0800

    rdrf #1813 Fixed progress bar display
    - Updated style for dropdown button next to progress bar to allow
        bar to correctly display
    - Updated step the_progress_indicator_should_be in steps.py to
        correctly select progress bar percentage

--- rdrf/rdrf/testing/behaviour/features/steps.py
+++ rdrf/rdrf/testing/behaviour/features/steps.py
@@ -549,7 +549,7 @@ def click_user_menu(step):
 def the_progress_indicator_should_be(step, percentage):
     progress_bar = world.browser.find_element_by_xpath(
         '//div[@class="progress"]/div[@class="progress-bar"]')
-    assert_equal(progress_bar.text.strip(), percentage)
+    assert_equal(progress_bar.get_attribute("innerHTML").strip(), percentage)
 
 
 @step('I go home')
