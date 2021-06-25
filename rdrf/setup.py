7bdcb67687460bc43861864427352baedaadee53
lee <id2359@yahoo.com>
2021-06-25 16:47:04 +0800

    Bumped version 6.4.0 Bootstrap 5

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -140,7 +140,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="6.3.28",
+      version="6.4.0",
       packages=find_packages(),
       description='RDRF',
       long_description='Rare Disease Registry Framework',
