33b986cdbf2a7d635f806c99ba8f3539c0522dad
lee <id2359@yahoo.com>
2021-08-31 15:58:23 +0800

    rdrf #1589 use python hl7 parser

--- rdrf/intframework/models.py
+++ rdrf/intframework/models.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import hl7
 from django.db import models
 from intframework import utils
 from utils import TransformFunctionError
@@ -16,6 +17,9 @@ logger = logging.getLogger(__name__)
 
 
 class HL7Mapping(models.Model):
+    """
+    Facilitate HL7 --> RDRF conversions
+    """
     event_map = models.TextField(blank=True, null=True)
 
     def load(self):
@@ -24,11 +28,13 @@ class HL7Mapping(models.Model):
         except ValueError:
             return {}
 
-    def parse(self, hl7_message):
+    def parse(self, hl7_message) -> dict:
         """
         Rather than have lots of models specifying the hl7 translations
         We have a block of JSON
         Something like 
+
+        The keys are HL7 event types ( MSH.9.1 ? )
 
         {"QRY_A19": {"BaseLineClinical/TestSection/CDEName": { "path": "OBX.3.4", "transform": "foobar" },
                     ."<FieldMoniker> : { "path" : <path into hl7 message>,
@@ -39,11 +45,18 @@ class HL7Mapping(models.Model):
         event_map = self.load()
         event_code = self._get_event_code(hl7_message)
         value_map = {}
+
+        try:
+            msg = hl7.parse(hl7_message)
+        except hl7.ParseException as pex:
+            # what to do?
+            return {}
+
         if event_code in event_map:
             mapping_map = event_map[event_code]
             for field_moniker, mapping_data in mapping_map.items():
                 hl7_path = mapping_data["path"]
-                hl7_value = self._get_hl7_value(hl7_path, hl7_message)
+                hl7_value = self._get_hl7_value(hl7_path, msg)
                 transform_name = mapping_data["transform"]
                 try:
                     rdrf_value = self._apply_transform(transform_name, hl7_value)
@@ -52,8 +65,10 @@ class HL7Mapping(models.Model):
                     logger.error("Error transforming HL7 field")
             return value_map
 
-    def _get_hl7_value(self, path, message):
-        return "TODO"
+    def _get_hl7_value(self, path, parsed_message):
+        # the path notation is understood by python hl7 library
+        # see https://python-hl7.readthedocs.io/en/latest/accessors.html
+        return parsed_message[path]
 
     def _apply_transform(self, transform_name, hl7_value):
         if hasattr(utils, transform_name):
@@ -62,16 +77,11 @@ class HL7Mapping(models.Model):
                 raise TransformFunctionError(f"{transform_name} is not a function")
             if not hasattr(func, "hl7_transform_function"):
                 raise TransformFunctionError(f"{transform_name} is not a HL7 transform")
-
-
-class HL7Message(models.Model):
-    code = models.CharField(max_length=80)
-    hl7 = models.TextField()
-
-    def build(self, template_data):
-        from django.template import Template, Context
-        context = Context(template_data)
-        return Template(self.hl7).render(context)
+            try:
+                return func(hl7_value)
+            except Exception as ex:
+                raise TransformFunctionError(f"{transform_name} inner exception: {ex}")
+        raise TransformFunctionError(f"Unknown transform function: {transform_name}")
 
 
 class DataRequest(models.Model):
