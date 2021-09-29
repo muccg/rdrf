1451285c1e7d5a2254e5c85ec0306aad61d64398
Jithendralal Nair <jithendralal@gmail.com>
2021-09-29 16:35:04 +0800

    rdrf #1951 sex transform added - WIP

--- rdrf/intframework/utils.py
+++ rdrf/intframework/utils.py
@@ -28,3 +28,23 @@ def identity(hl7_value):
 def date(hl7_value):
     datetime_object = datetime.strptime(hl7_value, '%Y%m%d')
     return datetime_object
+
+
+"""
+HL7 values
+Value	Description
+F	    Female
+M	    Male
+O	    Other
+U	    Unknown
+A	    Ambiguous
+N	    Not applicable
+
+TODO: the mapping is to be finalised after consulting with CIC team  
+"""
+SEX_MAP = {"M": 1, "F": 2, "U": 3, "O": 3, "A": 3, "N": 3}
+
+
+@ transform
+def sex(hl7_value):
+    return f"{SEX_MAP[hl7_value]}"
