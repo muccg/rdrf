48b9da4b93272173258261d2fd9fdfaf44f68af0
Jithendralal Nair <jithendralal@gmail.com>
2021-10-28 11:31:06 +0800

    rdrf #1957 reverting prodbuild script

--- travis-prodbuild.sh
+++ travis-prodbuild.sh
@@ -4,7 +4,7 @@ set -e
 # Production (deployable) build and tests
 #
 
-if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BRANCH_NAME" != x"1957_lint" ]; then
+if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BRANCH_NAME" != x"staging" ]; then
     echo "Branch $BRANCH_NAME is not deployable. Skipping prod build and tests"
     exit 0
 fi
