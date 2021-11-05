e410765d721114c4f7fb372e187c7dd2bbd419e7
Jithendralal Nair <jithendralal@gmail.com>
2021-11-05 15:31:44 +0800

    rdrf #1519  prevented multi sample registry and working group addition

--- rdrf/rdrf/initial_data/sample_registry.py
+++ rdrf/rdrf/initial_data/sample_registry.py
@@ -9,16 +9,20 @@ deps = ['base', 'groups', 'users']
 
 
 def load_data(**kwargs):
-    registry, _ = Registry.objects.get_or_create(code='sample', defaults=dict(
-        splash_screen='<h1>Sample Registry</h2>',
-        name='Sample Registry',
-        desc='Sample Registry',
-    ))
-    wg1, _ = WorkingGroup.objects.get_or_create(
-        name='Sample Registry Working Group 1', registry=registry)
+    if not Registry.objects.all().count():
+        registry, _ = Registry.objects.get_or_create(code='sample', defaults=dict(
+            splash_screen='<h1>Sample Registry</h1>',
+            name='Sample Registry',
+            desc='Sample Registry',
+        ))
 
-    for username in ('admin', 'clinical', 'curator', 'genetic'):
-        user = CustomUser.objects.get(username=username)
-        user.registry.add(registry)
-        user.working_groups.add(wg1)
-        user.save()
+    if not WorkingGroup.objects.all().count():
+        registry = Registry.objects.get()
+        wg1, _ = WorkingGroup.objects.get_or_create(
+            name='Working Group', registry=registry)
+
+        for username in ('admin', 'clinical', 'curator', 'genetic'):
+            user = CustomUser.objects.get(username=username)
+            user.registry.add(registry)
+            user.working_groups.add(wg1)
+            user.save()
