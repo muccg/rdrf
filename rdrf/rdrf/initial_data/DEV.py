922ce34c9416d7d66cc5265596a0a39d37531865
Jithendralal Nair <jithendralal@gmail.com>
2021-11-22 15:09:47 +0800

    rdrf #1519 sample registry initial data made conditional

--- rdrf/rdrf/initial_data/DEV.py
+++ rdrf/rdrf/initial_data/DEV.py
@@ -8,18 +8,33 @@ Includes:
     - sample registry, only if a sample registry to be created
     - permissive iprestrict rules
 '''
+import os
 from registry.groups import models
 
+fi = os.environ.get('DJANGO_FIXTURES')
 
-deps = [
-    'reference_data',
-    'users',
-    'sample_laboratories',
-    'genes_smaller_sample',
-    # uncomment the line below, if a sample registry and working group to be created
-    # 'sample_registry',
-    'iprestrict_permissive',
-]
+deps = []
+
+# "dev" for runserver
+if fi == "dev":
+    deps = [
+        'reference_data',
+        'users',
+        'sample_laboratories',
+        'genes_smaller_sample',
+        'iprestrict_permissive',
+    ]
+
+# "none" for bahavioural and "test" for unit tests
+if fi == "none" or fi == "test":
+    deps = [
+        'reference_data',
+        'users',
+        'sample_laboratories',
+        'genes_smaller_sample',
+        'sample_registry',
+        'iprestrict_permissive',
+    ]
 
 
 def load_data(**kwargs):
