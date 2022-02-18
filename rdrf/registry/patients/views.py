fe253a16853f0a47ec9a7c4ea6e29f896b6e5198
lee <id2359@yahoo.com>
2022-02-18 10:58:31 +0800

    rdrf #2147 added group check on consent download view

--- rdrf/registry/patients/views.py
+++ rdrf/registry/patients/views.py
@@ -5,6 +5,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.http import Http404
 from .models import PatientConsent
+from rdrf.security.security_checks import security_check_user_patient
 
 
 class ConsentFileView(View):
@@ -12,6 +13,10 @@ class ConsentFileView(View):
     @method_decorator(login_required)
     def get(self, request, consent_id=None, filename=""):
         consent = get_object_or_404(PatientConsent, pk=consent_id)
+        patient = consent.patient
+        user = request.user
+        security_check_user_patient(user, patient)
+
         if consent.form and consent.form.file:
             response = FileResponse(consent.form.file, content_type='application/octet-stream')
             response['Content-disposition'] = "filename=%s" % consent.filename
