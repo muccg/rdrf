9c0d2a0f5db878d607edc9c78164a0010c600356
Lee Render <id2359@yahoo.com>
2022-08-26 11:39:25 +0800

    bumped version to 6.6.28

--- rdrf/setup.py
+++ rdrf/setup.py
@@ -144,7 +144,7 @@ for package in ['rdrf', 'registry.common', 'registry.genetic',
 
 
 setup(name='django-rdrf',
-      version="6.6.27",
+      version="6.6.28",
       packages=find_packages(),
       description='RDRF',
       long_description='Rare Disease Registry Framework',
