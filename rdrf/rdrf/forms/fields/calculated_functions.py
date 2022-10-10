7730765a82a55c27ff4245e080a7362155916476
Lee Render <id2359@yahoo.com>
2022-10-10 11:24:13 +0800

    rdrf #2246 removed c from values LC clinical stage Stage IV

--- rdrf/rdrf/forms/fields/calculated_functions.py
+++ rdrf/rdrf/forms/fields/calculated_functions.py
@@ -1230,7 +1230,7 @@ def get_lc_clinical_cancer_stage_spec():
            TNMCTLC = T3 TNMCNLC = N3 TNMCMLC = M0
            TNMCTLC = T4 TNMCNLC = N3 TNMCMLC = M0
            Stage IV
-           TNMCTLC = cT* TNMCNLC = cN* TNMCMLC = cM1
+           TNMCTLC = T* TNMCNLC = N* TNMCMLC = M1
            Stage IVA
            TNMCTLC = T* TNMCNLC = N* TNMCMLC = M1a
            TNMCTLC = T* TNMCNLC = N* TNMCMLC = M1b
