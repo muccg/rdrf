a882fdbfea8fe8e80123dab322efb2899d328cfa
Lee Render <id2359@yahoo.com>
2022-11-21 15:39:20 +0800

    rdrf #2261 removing dead code & linting

--- rdrf/dashboards/utils.py
+++ rdrf/dashboards/utils.py
@@ -1,30 +1,3 @@
-from rdrf.models.definition.models import Registry
-from registry.patients.models import Patient
-
-
-class PromsType:
-    baseline = "baseline"
-    follow_up = "follow_up"
-
-
-class Proms:
-    """
-    Wrapper to make it easier to work with baseline and followup
-    patient reported outcomes.
-    """
-
-    def __init__(self, registry, patient, clinical_data):
-        self.registry = registry
-        self.patient = patient
-        self.cd = clinical_data
-        self.collection_date = None
-        self.proms_type = None
-        self._load()
-
-    def _load(self):
-        pass
-
-
 def get_colour_map():
     color_discrete_map = {
         "Not at all": "lightgreen",
--- rdrf/dashboards/views.py
+++ rdrf/dashboards/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.contrib.auth.decorators import login_required
@@ -28,94 +28,6 @@ logger = logging.getLogger(__name__)
 login_required_method = method_decorator(login_required)
 
 
-def test_app():
-    logger.debug("creating DashApp")
-    app = DjangoDash("App")  # replaces dash.Dash
-    logger.debug("created DashApp")
-    df = px.data.iris()  # iris is a pandas DataFrame
-    fig = px.scatter(df, x="sepal_width", y="sepal_length")
-    logger.debug("created fig")
-    app.layout = html.Div(
-        [
-            dcc.RadioItems(
-                id="dropdown-color",
-                options=[
-                    {"label": c, "value": c.lower()} for c in ["Red", "Green", "Blue"]
-                ],
-                value="red",
-            ),
-            html.Div(id="output-color"),
-            dcc.RadioItems(
-                id="dropdown-size",
-                options=[
-                    {"label": i, "value": j}
-                    for i, j in [("L", "large"), ("M", "medium"), ("S", "small")]
-                ],
-                value="medium",
-            ),
-            html.Div(id="output-size"),
-            html.Div([dcc.Graph(figure=fig)], id="graph"),
-        ]
-    )
-
-    logger.debug("created layout")
-
-    @app.callback(
-        dash.dependencies.Output("output-color", "children"),
-        [dash.dependencies.Input("dropdown-color", "value")],
-    )
-    def callback_color(dropdown_value):
-        return "The selected color is %s." % dropdown_value
-
-    @app.callback(
-        dash.dependencies.Output("output-size", "children"),
-        [
-            dash.dependencies.Input("dropdown-color", "value"),
-            dash.dependencies.Input("dropdown-size", "value"),
-        ],
-    )
-    def callback_size(dropdown_color, dropdown_size):
-        return "The chosen T-shirt is a %s %s one." % (
-            dropdown_size,
-            dropdown_color,
-        )
-
-    logger.debug("created callbacks")
-    return app
-
-
-def overall_app():
-    from .data import RegistryDataFrame
-    from .models import VisualisationConfig
-    from rdrf.models.definition.models import Registry
-    from datetime import datetime, timedelta
-
-    t1 = datetime.now()
-
-    registry = Registry.objects.get()
-
-    vis_config = VisualisationConfig.objects.get(registry=registry, code="overall")
-
-    rdf = RegistryDataFrame(registry, vis_config.config)
-
-    tof = TypesOfFormCompleted(types_forms_completed_df, date_start, date_end)
-
-    app = DjangoDash("App", external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-    app.layout = html.Div(
-        [
-            html.H1(f"Patient Reported Outcomes {registry.name}"),
-            chart("Types of Forms Completed", "tof", tof.pie),
-        ]
-    )
-
-    t2 = datetime.now()
-    secs = (t2 - t1).total_seconds()
-    logger.debug(f"overall app generated in {secs} seconds")
-
-    return app, secs
-
-
 def create_graphic(vis_config, data):
     if vis_config.code == "pcf":
         return PatientsWhoCompletedForms(vis_config.config, data).graphic
@@ -126,6 +38,7 @@ def create_graphic(vis_config, data):
     elif vis_config.code == "cpr":
         return ChangesInPatientResponses(vis_config.config, data).graphic
     else:
+        logger.error(f"dashboard error - unknown visualisation {vis_config.code}")
         raise Exception(f"Unknown code: {vis_config.code}")
 
 
