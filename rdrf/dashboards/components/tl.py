0626b8bf2f1da63362f468a99200a4d88f0631a4
Lee Render <id2359@yahoo.com>
2023-02-02 16:03:12 +0800

    rdrf #2333 config just has list of fields

--- rdrf/dashboards/components/tl.py
+++ rdrf/dashboards/components/tl.py
@@ -84,15 +84,12 @@ def get_display(field, value):
         return d
 
 
-def get_popup_info(group_name, field, value, display):
+def get_popup_info(field, display):
     return f"{field}: {display}"
 
 
-def get_fields():
-    from dashboards.models import VisualisationBaseDataConfig
-
-    c = VisualisationBaseDataConfig.objects.get()
-    return [code for code in c.config["fields"] if code.startswith("EORTCQLQC30")]
+def get_fields(config):
+    return config["fields"]
 
 
 def get_field_label(cde_code):
@@ -115,49 +112,37 @@ def get_popover_target(target_id, body):
 
 class TrafficLights(BaseGraphic):
     def get_graphic(self):
+        self.fields = get_fields(self.config)
         data = self._get_table_data()
         return self.get_table(data)
 
     def get_table(self, table_data):
         seq_names = [html.Th(x) for x in table_data["SEQ_NAME"]]
-        table_header = [html.Thead(html.Tr([html.Th("Scale Group"), *seq_names]))]
-
-        groups_config = self.config["groups"]
-        groups_dict = {g["name"]: g["fields"] for g in groups_config}
-
+        table_header = [html.Thead(html.Tr([html.Th("Field"), *seq_names]))]
         table_rows = []
 
-        for group_name, fields in groups_dict.items():
-            fields2 = get_fields()
+        for field in self.fields:
+            field_values = table_data[field]
+            image_id = f"image_{field}_"
 
-            for field in fields2:
-                field_values = table_data[field]
-                image_id = f"image_{field}_"
-                logger.debug(f"image id = {image_id}")
-
-                table_row = html.Tr(
-                    [
-                        html.Td(html.Small(get_field_label(field))),
-                        *[
-                            html.Td(
-                                [
-                                    get_image(value, image_id + "_" + str(index)),
-                                    get_popover_target(
-                                        image_id + "_" + str(index),
-                                        get_popup_info(
-                                            group_name,
-                                            field,
-                                            value,
-                                            get_display(field, value),
-                                        ),
-                                    ),
-                                ],
-                            )
-                            for index, value in enumerate(field_values)
-                        ],
-                    ]
-                )
-                table_rows.append(table_row)
+            table_row = html.Tr(
+                [
+                    html.Td(html.Small(get_field_label(field))),
+                    *[
+                        html.Td(
+                            [
+                                get_image(value, image_id + "_" + str(index)),
+                                get_popover_target(
+                                    image_id + "_" + str(index),
+                                    get_popup_info(field, get_display(field, value)),
+                                ),
+                            ],
+                        )
+                        for index, value in enumerate(field_values)
+                    ],
+                ]
+            )
+            table_rows.append(table_row)
 
         table_body = [html.Tbody(table_rows)]
         table = dbc.Table(table_header + table_body, className="table-striped table-sm")
@@ -165,26 +150,18 @@ class TrafficLights(BaseGraphic):
         return table
 
     def _get_table_data(self) -> pd.DataFrame:
-        groups = self.config["groups"]
         prefix = ["PID", "SEQ", "SEQ_NAME", "TYPE"]
-        cdes = []
-        for group in groups:
-            cdes.extend(group["fields"])
-
-        fields = get_fields()
-
-        df = self.data[prefix + fields]
+        cdes = self.fields
+        df = self.data[prefix + cdes]
 
         logger.debug(f"dataframe columns: {df.columns}")
 
         for cde in cdes:
             df = self._add_colour_column(cde, df)
 
-        for field in fields:
+        for field in cdes:
             display_field = field + "_display"
             df[display_field] = df[field].map(lambda v: get_display_value(field, v))
-
-        logger.debug(f"{df}")
 
         return df
 
