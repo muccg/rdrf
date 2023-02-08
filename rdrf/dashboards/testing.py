7bbfbcd56a709c001df8ba271684899216fb006d
Lee Render <id2359@yahoo.com>
2023-02-08 16:37:20 +0800

    rdrf #2286 run tests for dashboards

---
+++ rdrf/dashboards/testing.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+
+class VisTestCase(TestCase):
+    def test_scg_symptom_score(self):
+        from dashboards.score_functions import sgc_symptom_score as f
+
+        range_value = 3.0
+        EORTCQLQC30_Q09 = 1.0
+        EORTCQLQC30_Q19 = 2.0
+
+        raw_scores = [EORTCQLQC30_Q09, EORTCQLQC30_Q19]
+        raw_score = sum(raw_scores) / 2.0
+
+        correct_value = 16.67
+        result = f(raw_score, 3.0)
+
+        self.assertEquals(
+            raw_score,
+            correct_value,
+            f"symptom score: correct={correct_value} actual={result}",
+        )
