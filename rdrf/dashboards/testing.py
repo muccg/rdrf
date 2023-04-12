48f18560d9a081e8ad403e834a25b4ef94b4c096
Lee Render <id2359@yahoo.com>
2023-04-12 14:08:28 +0800

    rdrf #2449 basic test

--- rdrf/dashboards/testing.py
+++ rdrf/dashboards/testing.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from dashboards.score_functions import sgc_symptom_score as f
 
+from unittest.mock import Mock
+
 
 class VisTestCase(TestCase):
     def test_scg_symptom1(self):
@@ -36,7 +38,12 @@ class VisTestCase(TestCase):
         )
 
 
-class CRCTrafficLightTestCase(TestCase):
+class VisualisationsTestCase(TestCase):
+    databases = {"default", "clinical"}
+    fixtures = ["testing_auth", "testing_users", "testing_rdrf"]
+
+
+class CRCTrafficLightTestCase(VisualisationsTestCase):
     def test_normal_case(self):
         initial_df = self.create_dataframe()
         tl = self.create_traffic_light(initial_df)
@@ -48,9 +55,14 @@ class CRCTrafficLightTestCase(TestCase):
         self.check_followups(table_data)
 
     def create_traffic_light(self, data):
-        from dashboards.components.tl import TrafficLight
+        from dashboards.components.tl import TrafficLights
 
-        return TrafficLight(data)
+        vis_config = Mock()
+
+        data = None
+        patient = Mock()
+
+        return TrafficLights("crc test tl", vis_config, data, patient, None)
 
     def create_dataframe(self):
         return None
@@ -63,4 +75,11 @@ class CRCTrafficLightTestCase(TestCase):
                 self.assertEqual(row["SEQ"], 0, "baseline row does not have SEQ 0")
                 num_baselines += 1
 
-        self.assertEqual(num_baselines, 1, "Number of baseline rows is not 1")
+        self.assertEqual(num_baselines, 1, f"Number of baseline rows is not 1: is {num_baselines}")
+
+
+    def check_followups(self, df):
+        for index, row in df.iterrows():
+            if row["TYPE"] == "followup":
+                seq = row["SEQ"]
+                self.assertGreater(seq, 0, f"followup seq number should be greater than 0. Actual={seq}")
